6acf652e487e59e8bb6f1238647758a6827c79d9
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-07-10 09:43:20 +0100

    Add error page and fix bootstrap

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<title>Error {status} - Mercury</title>
+</svelte:head>
+
+<script lang="ts" context="module">
+	export function load({ error, status }: { error: object; status: number }) {
+		return {
+			props: {
+				status: status,
+				error: error,
+			},
+		}
+	}
+	// copied straight from kit.svelte.dev
+</script>
+
+<script lang="ts">
+	export let status: number
+	export let error: any
+</script>
+
+<h1>Error {status}</h1>
+{error["message"]}
