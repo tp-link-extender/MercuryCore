0d3779b45f2898a42431689db2e3d99f7b517864
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-07-10 09:18:21 +0100

    I rewrot it in svelt

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess(),
+  ],
+
+  kit: {
+    adapter: adapter(),
+  },
+};
