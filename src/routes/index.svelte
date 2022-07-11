057b930e15564fa084c86a7f16233a24d8837bdd
Lewin Kelly <heli@odex.cf>
2022-07-11 02:58:01 +0100

    ""Fix"" gradient

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -57,6 +57,6 @@
 		height: 420vh
 		background: hsl(305, 63%, 80%)
 
-		background-image: linear-gradient(180deg, rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 1) 25%,  rgba(0, 0, 0, 0.2) 50%,  rgba(0, 0, 0, 1) 100%), radial-gradient(ellipse at top left, rgba(13, 110, 253, 0.5), #0000 50%), radial-gradient(ellipse at top right, rgba(255, 228, 132, 0.5), #0000 50%), radial-gradient(ellipse at center right, rgba(112.520718, 44.062154, 249.437, 0.5), #0000 50%), radial-gradient(ellipse at center left, rgba(214, 51, 132, 0.5), #0000 50%)
-
+		background-image: linear-gradient(180deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 25%,  rgba(0, 0, 0, 0.2) 50%,  rgba(0, 0, 0, 1) 100%), radial-gradient(ellipse at top left, rgba(13, 110, 253, 0.5), #0000 50%), radial-gradient(ellipse at top right, rgba(255, 228, 132, 0.5), #0000 50%), radial-gradient(ellipse at center right, rgb(255,255,255), #0000 50%), radial-gradient(ellipse at center left, rgb(255,255,255), #0000 50%), radial-gradient(ellipse at center center, rgb(255,255,255), #0000 50%)
+		// do not look at the last half of this gradient worst mistake of my life
 </style>
