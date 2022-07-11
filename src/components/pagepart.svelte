23592c4484b34ee371957ab0842f1d77380ab7d2
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-07-11 04:15:04 +0100

    Remove intersection animations, doesn't work well with parallax

--- src/components/pagepart.svelte
+++ src/components/pagepart.svelte
@@ -1,34 +1,25 @@
 <script lang="ts">
 	import { fly } from "svelte/transition"
-	import IntersectionObserver from "svelte-intersection-observer"
-
-	let element: HTMLElement
-	let intersecting: boolean
 	export let right = false
-	export let middle = false
 
-	let id = middle ? "middle" : right ? "right" : "left"
+	let id = right ? "right" : "left"
 </script>
 
-<IntersectionObserver {element} bind:intersecting>
-	<div bind:this={element} class="container" {id}>
-		<div>
-			<!-- {#if intersecting} -->
-				<div id="b" transition:fly={{ x: 300, duration: 1}} class="d-flex flex-column justify-content-center align-items-center">
-					<slot />
-				</div>
-			<!-- {/if} -->
-		</div>
+<div class="container" {id}>
+	<div id="b" transition:fly={{ x: 300, duration: 1000 }} class="d-flex flex-column justify-content-center align-items-center">
+		<slot />
 	</div>
-</IntersectionObserver>
+</div>
 
 <style lang="sass">
 	#b
-		height: 50vh
-		width: 33vh
+		height: 100vh
+		width: 50vw
+		padding: 15vh
 
 	#right
-		margin-left: 66vw
-	#middle
-		margin-left: 41vw
+		margin-left: 50vw
+
+	#left
+		margin-left: 0
 </style>
