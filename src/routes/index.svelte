c14864e9b0bcd727c25ecaf870a41efd2bada0b8
Lewin Kelly <heli@odex.cf>
2022-07-11 07:53:58 +0100

    Add hybrid static + parallax landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,32 +15,79 @@
 	</div>
 
 	<Parallax sections={2} config={{ stiffness: 1, damping: 1 }}>
-		<ParallaxLayer offset={1} rate={-3}>
+		<ParallaxLayer offset={0} rate={0}>
+			<Pagepart>
+				<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			</Pagepart>
+		</ParallaxLayer>
+		<ParallaxLayer offset={0} rate={-2}>
+			<Pagepart right>
+				<h1 class="fw-bolder fw-light text-dark">Better than Tadah</h1>
+			</Pagepart>
+		</ParallaxLayer>
+		<ParallaxLayer offset={1} rate={0}>
 			<Pagepart>
 				<h1 class="fw-bolder fw-light text-dark">Lots of clients</h1>
 			</Pagepart>
-			<Pagepart>
-				<h1 class="fw-bolder fw-light text-dark">Better than Tadah</h1>
-			</Pagepart>
-			<Pagepart>
-				<h1 class="fw-bolder fw-light text-dark">Exactly 0% Kinery</h1>
-			</Pagepart>
 		</ParallaxLayer>
-		<ParallaxLayer offset={0} rate={1}>
-			<Pagepart right>
-				<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
-			</Pagepart>
-			<Pagepart right>
-				<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
-			</Pagepart>
+		<ParallaxLayer offset={1} rate={-2}>
 			<Pagepart right>
 				<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
 			</Pagepart>
 		</ParallaxLayer>
 	</Parallax>
+	<Pagepart fullwidth>
+		<div>
+			<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			<h1 class="fw-bolder fw-light text-dark text-center">Exactly 0% Kinery.</h1>
+		</div>
+		<div class="px-5"></div>
+		<div>
+			<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			<h1 class="fw-bolder fw-light text-dark text-center">That's 100% less Kinery than Tadah.</h1>
+		</div>
+	</Pagepart>
+
+
+	<Parallax sections={2} config={{ stiffness: 1, damping: 1 }}>
+		<ParallaxLayer offset={0} rate={0}>
+			<Pagepart>
+				<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			</Pagepart>
+		</ParallaxLayer>
+		<ParallaxLayer offset={0} rate={-2}>
+			<Pagepart right>
+				<h1 class="fw-bolder fw-light text-dark">Approximately 60,000 better than Polygon</h1>
+			</Pagepart>
+		</ParallaxLayer>
+		<ParallaxLayer offset={1} rate={0}>
+			<Pagepart>
+				<h1 class="fw-bolder fw-light text-light">Better website, too</h1>
+			</Pagepart>
+		</ParallaxLayer>
+		<ParallaxLayer offset={1} rate={-2}>
+			<Pagepart right>
+				<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			</Pagepart>
+		</ParallaxLayer>
+	</Parallax>
+	<Pagepart fullwidth>
+		<div>
+			<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			<h1 class="fw-bolder fw-light text-light text-center">Includes Web3 Technology.</h1>
+		</div>
+		<div class="px-5"></div>
+		<div>
+			<img src="/devices.png" alt="Devices playing Mercury. Rendered with models from deviceframes.com" />
+			<h1 class="fw-bolder fw-light text-light text-center">It's on the BalochChain.</h1>
+		</div>
+	</Pagepart>
 </main>
 
 <style lang="sass">
+	h1
+		width: 100%
+		text-align: center
 	img
 		width: 100%
 
@@ -50,6 +97,6 @@
 	main
 		background: hsl(305, 63%, 80%)
 
-		background-image: linear-gradient(180deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 25%,  rgba(0, 0, 0, 0.2) 50%,  rgba(0, 0, 0, 1) 120%), radial-gradient(ellipse at top left, rgba(13, 110, 253, 0.5), #0000 50%), radial-gradient(ellipse at top right, rgba(255, 228, 132, 0.5), #0000 50%), radial-gradient(ellipse at center right, rgb(255,255,255), #0000 50%), radial-gradient(ellipse at center left, rgb(255,255,255), #0000 50%), radial-gradient(ellipse at center center, rgb(255,255,255), #0000 50%)
+		background-image: linear-gradient(180deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 10%,  rgba(0, 0, 0, 0.2) 50%,  rgba(0, 0, 0, 1) 120%), radial-gradient(ellipse at top left, rgba(13, 110, 253, 0.5), #0000 50%), radial-gradient(ellipse at top right, rgba(255, 228, 132, 0.5), #0000 50%), radial-gradient(ellipse at center right, rgb(255,255,255), #0000 50%), radial-gradient(ellipse at center left, rgb(255,255,255), #0000 50%), radial-gradient(ellipse at center center, rgb(255,255,255), #0000 50%)
 		// do not look at the last half of this gradient worst mistake of my life
 </style>
