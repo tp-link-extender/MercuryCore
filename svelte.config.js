341aa9a8345f5c95832dd78b033e7b55e2e6434a
Lewin Kelly <heli@odex.cf>
2022-07-13 23:34:20 +0100

    Allow site to be compiled down to static html

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-static"
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,5 +9,8 @@ export default {
 
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			default: true,
+		},
 	},
 }
