166db49f5733ae6fd40b3913b828a8362b0a02e2
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-07-24 06:04:33 +0100

    Add login page checks for username/password length

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -5,17 +5,29 @@
 <h1 class="text-center light-text">Login</h1>
 
 <div class="container mt-5">
-	<form class="m-auto">
+	<form class="m-auto" action="/api/login" method="post">
+		<!-- Didn't work when page and endpoint were under the same url, "cannot prerender pages that have endpoints with mutative methods" -->
+		
 		<div class="mb-3">
-			<input type="text" class="form-control" placeholder="Username" />
+			<input name="username" type="text" class="form-control" placeholder="Username" />
 		</div>
 		<div class="mb-3">
-			<input type="password" class="form-control" placeholder="Password" />
+			<input name="password" type="password" class="form-control" placeholder="Password" />
 		</div>
 		<div class="mb-3 form-check">
-			<input type="checkbox" class="form-check-input" id="rememberMe" />
+			<input name="remember" type="checkbox" class="form-check-input" id="rememberMe" />
 			<label class="form-check-label light-text" for="rememberMe">Remember me</label>
 		</div>
+
+		<div id="errors" class="col-12 mb-3 text-danger">
+			<p id="error">An unexpected error occured</p>
+			<p id="ushort">Username must be more than 3 characters</p>
+			<p id="ulong">Username must be less than 30 characters</p>
+			<p id="pshort">Password must be at least 16 characters</p>
+			<p id="plong">Password must be less than 6969 characters</p>
+		</div>
+		<!-- Because I couldn't get it working from the endpoint -->
+
 		<button type="submit" class="container-fluid btn btn-primary">Log in</button>
 	</form>
 </div>
@@ -30,4 +42,9 @@
 	input
 		background: var(--accent)
 		border: none
+
+	#errors p
+		display: none
+		&:target
+			display: block
 </style>
