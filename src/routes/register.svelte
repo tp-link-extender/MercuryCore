dcf4763c78d496cd077f44b2c6ee77dfa93f6ae9
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-07-24 06:03:19 +0100

    Add register page (no functionality yet)

---
+++ src/routes/register.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+	<title>Login - Mercury</title>
+</svelte:head>
+
+<h1 class="text-center light-text">Register</h1>
+
+<div class="container mt-5">
+	<form class="m-auto">
+		<div class="mb-3">
+			<input type="text" class="form-control" placeholder="Username" />
+		</div>
+		<div class="mb-3">
+			<input type="password" class="form-control" placeholder="Password" />
+		</div>
+		<div class="mb-3">
+			<input type="password" class="form-control" placeholder="Registration Key" />
+		</div>
+		<div class="mb-3 form-check">
+			<input type="checkbox" class="form-check-input" id="rememberMe" />
+			<label class="form-check-label light-text" for="rememberMe">Remember me</label>
+		</div>
+		<button type="submit" class="container-fluid btn btn-primary">Register</button>
+	</form>
+</div>
+
+<style lang="sass">
+	.container
+		background: var(--accent2)
+		max-width: 30rem
+		border-radius: 1rem
+		padding: 1.25rem
+
+	input
+		background: var(--accent)
+		border: none
+</style>
