d9796f9d2dadcbdcee450e3a1c7e27af400d2d23
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2022-11-19 14:54:03 +0000

    Responsive navbar

--- src/routes/(main)/login/+page.svelte
+++ src/routes/(main)/login/+page.svelte
@@ -7,53 +7,46 @@
 <svelte:head>
 	<title>Login - Mercury</title>
 </svelte:head>
-
-<h1 class="text-center light-text">Login</h1>
-
 <div class="container mt-5">
-	<form
-		class="m-auto"
-		method="POST"
-		use:enhance={() => {
-			// Allows form to work without reloading if javascript is available
-			return async ({ result }) => {
-				if (result.type == "redirect") {
-					window.location.href = result.location
-					// By default, use:enhance uses goto() to redirect to the homepage,
-					// this would be bad as the user's auth token would not be updated
-					// and we wouldn't be able to load their homepage info
+	<div class="row">
+		<div class="col">
+
+		</div>
+		<div class="col">
+			<form
+			class="m-auto"
+			method="POST"
+			use:enhance={() => {
+				// Allows form to work without reloading if javascript is available
+				return async ({ result }) => {
+					if (result.type == "redirect") {
+						window.location.href = result.location
+						// By default, use:enhance uses goto() to redirect to the homepage,
+						// this would be bad as the user's auth token would not be updated
+						// and we wouldn't be able to load their homepage info
+					}
+					await applyAction(result)
 				}
-				await applyAction(result)
-			}
-		}}
-	>
-		<div class="mb-3">
-			<input name="username" type="text" class="form-control" placeholder="Username" />
+			}}
+		>
+			<div class="mb-3">
+				<input name="username" type="text" class="form-control" placeholder="Username" />
+			</div>
+			<div class="mb-3">
+				<input name="password" type="password" class="form-control" placeholder="Password" />
+			</div>
+			<div class="mb-3 form-check">
+				<input name="remember" type="checkbox" class="form-check-input" id="rememberMe" />
+				<label class="form-check-label light-text" for="rememberMe">Remember me</label>
+			</div>
+	
+			{#if form?.msg}
+				<p class="col-12 mb-3 text-danger">{form.msg}</p>
+			{/if}
+	
+			<button type="submit" class="container-fluid btn btn-primary">Log in</button>
+		</form>
 		</div>
-		<div class="mb-3">
-			<input name="password" type="password" class="form-control" placeholder="Password" />
-		</div>
-		<div class="mb-3 form-check">
-			<input name="remember" type="checkbox" class="form-check-input" id="rememberMe" />
-			<label class="form-check-label light-text" for="rememberMe">Remember me</label>
-		</div>
-
-		{#if form?.msg}
-			<p class="col-12 mb-3 text-danger">{form.msg}</p>
-		{/if}
-
-		<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-	</form>
+	</div>
 </div>
 
-<style lang="sass">
-	.container
-		background: var(--accent2)
-		max-width: 30rem
-		border-radius: 1rem
-		padding: 1.25rem
-
-	input
-		background: var(--accent)
-		border: none
-</style>
