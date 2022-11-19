9212134ffeffbb0b340822acfdcddb73aeecdfee
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2022-11-19 17:15:29 +0000

    Update navbar.svelte

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,78 +1,80 @@
 <script lang="ts">
 	import { getUser } from "@lucia-auth/sveltekit/client"
 	import { signOut } from "@lucia-auth/sveltekit/client"
-
+	
 	const user = getUser()
-</script>
-
-<nav class="navbar navbar-expand-lg navbar-dark navbar-fixed-top position-fixed">
+ </script>
+ <nav class="navbar navbar-expand-lg navbar-dark navbar-fixed-top position-fixed">
 	<div class="container">
-		<a class="navbar-brand light-text" href="/">Mercury</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarText">
+	   <a class="navbar-brand light-text" href="/">Mercury</a>
+	   <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarExample-expand-lg" aria-controls="offcanvasNavbarExample-expand-lg">
+		<span class="navbar-toggler-icon" data-bs-target="#offcanvasNavbarExample-expand-lg"></span>
+	</button>
+	   <div class="offcanvas offcanvas-start bg-dark" data-bs-hideresize="true" tabindex="-1" id="offcanvasNavbarExample-expand-lg" aria-labelledby="offcanvasNavbarExample-expand-lg">
+		<div class="offcanvas-header">
+			<h5 class="offcanvas-title" id="offcanvasLabel" style="color:white;">Mercury</h5>
+			<button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+		</div>  
+		<div class="offcanvas-body">
 		<div class="d-flex">
-			{#if $user}
-				<a type="button" href="/home" class="btn my-2 my-sm-0 light-text">Home</a>
-			{/if}
-			<!-- <a type="button" href="/discover" class="btn my-2 my-sm-0 light-text">Discover</a>
-			<a type="button" href="/catalog" class="btn my-2 my-sm-0 light-text">Catalog</a>
-			<a type="button" href="/create" class="btn my-2 my-sm-0 light-text">Create</a> -->
-		</div>
-		<div class="navbar-nav ms-auto">
-			{#if !$user}
-			  <li class="nav-item">
+			 {#if $user}
+			 <a type="button" href="/home" class="btn my-2 my-sm-0 light-text">Home</a>
+			 {/if}
+			 <!-- <a type="button" href="/discover" class="btn my-2 my-sm-0 light-text">Discover</a>
+				<a type="button" href="/catalog" class="btn my-2 my-sm-0 light-text">Catalog</a>
+				<a type="button" href="/create" class="btn my-2 my-sm-0 light-text">Create</a> -->
+		  </div>
+		  <div class="navbar-nav ms-auto">
+			 {#if !$user}
+			 <li class="nav-item">
 				<a type="button" href="/login" class="btn light-text">Login</a>
-			  </li>
-			  <li class="nav-item">
+			 </li>
+			 <li class="nav-item">
 				<a type="button" href="/register" class="btn btn-success my-2 my-sm-0">Register</a>
-			  </li>
-			{:else}
-				<a id="username" href="/{$user?.username}" class="d-flex">
-					<div id="pfp" class="mx-2">
-						<img src={$user?.image} alt="You" />
-					</div>
-					<p class="light-text my-auto fs-6 me-4">
-						{$user?.displayname || $user?.username}
-					</p>
-				</a>
-				<button
-					on:click={async () => {
-						await signOut()
-						window.location.reload()
-						// I have no clue if this is a good way to do this
-						// I tried it, and it worked
-						// TODO: make it not rely on js
-					}}
-					class="btn btn-success my-2 my-sm-0">Logout</button
+			 </li>
+			 {:else}
+			 <a id="username" href="/{$user?.username}" class="d-flex">
+				<div id="pfp" class="mx-2">
+				   <img src={$user?.image} alt="You" />
+				</div>
+				<p class="light-text my-auto fs-6 me-4">
+				   {$user?.displayname || $user?.username}
+				</p>
+			 </a>
+			 <button
+			 on:click={async () => {
+			 await signOut()
+			 window.location.reload()
+			 // I have no clue if this is a good way to do this
+			 // I tried it, and it worked
+			 // TODO: make it not rely on js
+			 }}
+			 class="btn btn-success my-2 my-sm-0">Logout</button
 				>
-			{/if}
+			 {/if}
+		  </div>
 		</div>
-		</div>
+	   </div>
 	</div>
-</nav>
-
-<style lang="sass">
+ </nav>
+ <style lang="sass">
 	nav
 		z-index: 9
 		width: 100vw
 		max-height: 10vh
 		backdrop-filter: blur(8px)
-		a
-			margin-right: 0.5rem
-		img
-			border-radius: 50%
-
+	a
+		margin-right: 0.5rem
+	img
+		border-radius: 50%
 	#pfp
 		background: var(--accent2)
 		border-radius: 50%
 		width: 2.5rem
 		height: 2.5rem
-		img
-			width: 2.5rem
-			height: 2.5rem
-	
+	img
+		width: 2.5rem
+		height: 2.5rem
 	#username
 		text-decoration: none
-</style>
+ </style>
