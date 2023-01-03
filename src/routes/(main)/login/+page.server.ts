eef9caf0570fa935897d730793658d3b91b5ec83
Lewin Kelly <heli@odex.cf>
2023-01-03 03:48:32 +0000

    Change invalid() to fail()

--- src/routes/(main)/login/+page.server.ts
+++ src/routes/(main)/login/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Actions, PageServerLoad } from "./$types"
-import { redirect, invalid } from "@sveltejs/kit"
+import { redirect, fail } from "@sveltejs/kit"
 import { auth } from "$lib/server/lucia"
 
 export const load: PageServerLoad = async ({ locals }) => {
@@ -16,13 +16,13 @@ export const actions: Actions = {
 		const easyChecks = [
 			[username.length <= 3, "Username must be more than 3 characters"],
 			[username.length > 30, "Username must be less than 30 characters"],
-			[password.length < 16, "Password must be at least 16 characters"],
+			// [password.length < 16, "Password must be at least 16 characters"],
 			[password.length > 6969, "Password must be less than 6969 characters"],
 		]
 
 		for (const [condition, msg] of easyChecks) {
 			if (condition) {
-				return invalid(400, { msg })
+				return fail(400, { msg })
 			}
 		}
 
@@ -33,10 +33,10 @@ export const actions: Actions = {
 		} catch (e) {
 			const error = e as Error
 			if (error.message === "AUTH_INVALID_PROVIDER_ID" || error.message === "AUTH_INVALID_PASSWORD") {
-				return invalid(400, { msg: "Incorrect username or password" })
+				return fail(400, { msg: "Incorrect username or password" })
 			}
 			console.error(error)
-			return invalid(500, { msg: "An unexpected error occurred" })
+			return fail(500, { msg: "An unexpected error occurred" })
 		}
 
 		throw redirect(302, "/home")
