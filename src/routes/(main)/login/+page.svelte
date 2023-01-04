7cfa78204bb6c4024069327115439b1c81687624
Lewin Kelly <heli@odex.cf>
2023-01-04 08:18:20 +0000

    Fix login page

--- src/routes/(main)/login/+page.svelte
+++ src/routes/(main)/login/+page.svelte
@@ -4,36 +4,36 @@
 </script>
 
 <svelte:head>
-	<title>Login - Mercury</title>
+	<title>Log in - Mercury</title>
 </svelte:head>
-<div class="container d-flex justify-content-center align-items-center">
-	<div class="row">
-			<div class="col">
-			<button>sass</button>
+
+<h1 class="text-center light-text">Log in</h1>
+
+<div class="container mt-5">
+	<form class="m-auto" method="POST" use:enhance>
+		<div class="mb-3">
+			<input name="username" type="text" class="form-control" placeholder="Username" />
 		</div>
-		<div class="col">
-			<form
-			class="m-auto"
-			method="POST"
-			use:enhance
-		>
-			<div class="mb-3">
-				<input name="username" type="text" class="form-control" placeholder="Username" />
-			</div>
-			<div class="mb-3">
-				<input name="password" type="password" class="form-control" placeholder="Password" />
-			</div>
-	
-			{#if form?.msg}
-				<p class="col-12 mb-3 text-danger">{form.msg}</p>
-			{/if}
-	
-			<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-		</form>
+		<div class="mb-3">
+			<input name="password" type="password" class="form-control" placeholder="Password" />
 		</div>
-	</div>
+
+		{#if form?.msg}
+			<p class="col-12 mb-3 text-danger">{form.msg}</p>
+		{/if}
+
+		<button type="submit" class="container-fluid btn btn-primary">Log in</button>
+	</form>
 </div>
 
 <style lang="sass">
+	.container
+		background: var(--accent2)
+		max-width: 30rem
+		border-radius: 1rem
+		padding: 1.25rem
 
+	input
+		background: var(--accent)
+		border: none
 </style>
