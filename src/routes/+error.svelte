2a201d0c902923931e0d5eb331eda8d4a40d453c
Lewin Kelly <heli@odex.cf>
2023-01-07 00:04:44 +0000

    Restructure pages to remove footer from login and register

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script lang="ts" context="module">
+	import { page } from "$app/stores"
+	import Footer from "$lib/components/Footer.svelte"
+</script>
+
+<svelte:head>
+	<title>Error {$page.status} - Mercury</title>
+</svelte:head>
+
+<main>
+	<div id="all">
+		<div class="container d-flex flex-column justify-content-center align-items-center light-text">
+			<h1 class="light-text">Error {$page.status}</h1>
+			{$page.error?.message}
+		</div>
+	</div>
+</main>
+
+<Footer />
+
+<style lang="sass">
+	main
+		padding-bottom: 5vh
+		padding-top: 10vh
+		flex: 1 0 auto
+
+	#all
+		padding: 25vh 0
+		div
+			background: var(--accent2)
+			width: fit-content
+			padding: 2rem 5rem
+			border-radius: 1rem
+</style>
