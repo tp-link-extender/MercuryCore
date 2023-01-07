a9df7bf815f862121c9b01cf30fe03957c94d95f
Lewin Kelly <heli@odex.cf>
2023-01-07 00:43:04 +0000

    Fixes to experimental login page layout

--- src/routes/(nofooter)/login/+page.svelte
+++ src/routes/(nofooter)/login/+page.svelte
@@ -1,81 +1,84 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { enhance } from "$app/forms"
 	export let form: any
-</script>
+</script> -->
 
 <svelte:head>
 	<title>Log in - Mercury</title>
 </svelte:head>
 
-<div class="row bg-dark">
-	<div class="col px-0">
-		<div class="container px-0">
-			s
-			<!--
-			<form class="form-group" method="POST" use:enhance>
-				<fieldset>
-					<div class="mb-3">
-						{#if form?.area == "username"}
-							<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
-						{:else}
-							<input name="username" type="text" class="form-control" placeholder="Username" />
+<div id="all">
+	<div class="row bg-dark">
+		<div class="col px-0">
+			<div class="container px-0">
+				s
+				<!--
+				<form class="form-group" method="POST" use:enhance>
+					<fieldset>
+						<div class="mb-3">
+							{#if form?.area == "username"}
+								<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
+							{:else}
+								<input name="username" type="text" class="form-control" placeholder="Username" />
+							{/if}
+						</div>
+						<div class="mb-3">
+							{#if form?.area == "password"}
+								<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
+							{:else}
+								<input name="password" type="password" class="form-control" placeholder="Password" />
+							{/if}
+						</div>
+			
+						{#if form?.msg}
+							<p class="col-12 mb-3 text-danger">{form.msg}</p>
 						{/if}
-					</div>
-					<div class="mb-3">
-						{#if form?.area == "password"}
-							<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
-						{:else}
-							<input name="password" type="password" class="form-control" placeholder="Password" />
-						{/if}
-					</div>
-		
-					{#if form?.msg}
-						<p class="col-12 mb-3 text-danger">{form.msg}</p>
-					{/if}
-		
-					<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-				</fieldset>
-			</form>
-		-->
+			
+						<button type="submit" class="container-fluid btn btn-primary">Log in</button>
+					</fieldset>
+				</form>
+			-->
+			</div>
 		</div>
-	</div>
-	<div class="col px-0">
-		<div class="container px-0">
-			s
-			<!--
-			<form class="form-group" method="POST" use:enhance>
-				<fieldset>
-					<div class="mb-3">
-						{#if form?.area == "username"}
-							<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
-						{:else}
-							<input name="username" type="text" class="form-control" placeholder="Username" />
+		<div class="col px-0">
+			<div class="container px-0">
+				s
+				<!--
+				<form class="form-group" method="POST" use:enhance>
+					<fieldset>
+						<div class="mb-3">
+							{#if form?.area == "username"}
+								<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
+							{:else}
+								<input name="username" type="text" class="form-control" placeholder="Username" />
+							{/if}
+						</div>
+						<div class="mb-3">
+							{#if form?.area == "password"}
+								<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
+							{:else}
+								<input name="password" type="password" class="form-control" placeholder="Password" />
+							{/if}
+						</div>
+			
+						{#if form?.msg}
+							<p class="col-12 mb-3 text-danger">{form.msg}</p>
 						{/if}
-					</div>
-					<div class="mb-3">
-						{#if form?.area == "password"}
-							<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
-						{:else}
-							<input name="password" type="password" class="form-control" placeholder="Password" />
-						{/if}
-					</div>
-		
-					{#if form?.msg}
-						<p class="col-12 mb-3 text-danger">{form.msg}</p>
-					{/if}
-		
-					<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-				</fieldset>
-			</form>
-		-->
+			
+						<button type="submit" class="container-fluid btn btn-primary">Log in</button>
+					</fieldset>
+				</form>
+			-->
+			</div>
 		</div>
 	</div>
 </div>
 
-
-
 <style lang="sass">
-	input
-		background: var(--accent)
-		border-color: var(--accent3)
+	#all
+		overflow: hidden
+		
+	// input
+	// 	background: var(--accent)
+	// 	border-color: var(--accent3)
 </style>
