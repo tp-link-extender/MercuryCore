d0d7191cfa06e85dfcb7f9f78c216da5723d9268
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-01-07 00:36:59 +0000

    ssss

--- src/routes/(nofooter)/login/+page.svelte
+++ src/routes/(nofooter)/login/+page.svelte
@@ -7,42 +7,74 @@
 	<title>Log in - Mercury</title>
 </svelte:head>
 
-<h1 class="text-center light-text">Log in</h1>
-
-<div class="container mt-5">
-	<form class="m-auto form-group" method="POST" use:enhance>
-		<fieldset>
-			<div class="mb-3">
-				{#if form?.area == "username"}
-					<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
-				{:else}
-					<input name="username" type="text" class="form-control" placeholder="Username" />
-				{/if}
-			</div>
-			<div class="mb-3">
-				{#if form?.area == "password"}
-					<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
-				{:else}
-					<input name="password" type="password" class="form-control" placeholder="Password" />
-				{/if}
-			</div>
-
-			{#if form?.msg}
-				<p class="col-12 mb-3 text-danger">{form.msg}</p>
-			{/if}
-
-			<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-		</fieldset>
-	</form>
+<div class="row bg-dark">
+	<div class="col px-0">
+		<div class="container px-0">
+			s
+			<!--
+			<form class="form-group" method="POST" use:enhance>
+				<fieldset>
+					<div class="mb-3">
+						{#if form?.area == "username"}
+							<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
+						{:else}
+							<input name="username" type="text" class="form-control" placeholder="Username" />
+						{/if}
+					</div>
+					<div class="mb-3">
+						{#if form?.area == "password"}
+							<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
+						{:else}
+							<input name="password" type="password" class="form-control" placeholder="Password" />
+						{/if}
+					</div>
+		
+					{#if form?.msg}
+						<p class="col-12 mb-3 text-danger">{form.msg}</p>
+					{/if}
+		
+					<button type="submit" class="container-fluid btn btn-primary">Log in</button>
+				</fieldset>
+			</form>
+		-->
+		</div>
+	</div>
+	<div class="col px-0">
+		<div class="container px-0">
+			s
+			<!--
+			<form class="form-group" method="POST" use:enhance>
+				<fieldset>
+					<div class="mb-3">
+						{#if form?.area == "username"}
+							<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
+						{:else}
+							<input name="username" type="text" class="form-control" placeholder="Username" />
+						{/if}
+					</div>
+					<div class="mb-3">
+						{#if form?.area == "password"}
+							<input name="password" type="password" class="form-control is-invalid" placeholder="Password" />
+						{:else}
+							<input name="password" type="password" class="form-control" placeholder="Password" />
+						{/if}
+					</div>
+		
+					{#if form?.msg}
+						<p class="col-12 mb-3 text-danger">{form.msg}</p>
+					{/if}
+		
+					<button type="submit" class="container-fluid btn btn-primary">Log in</button>
+				</fieldset>
+			</form>
+		-->
+		</div>
+	</div>
 </div>
 
-<style lang="sass">
-	.container
-		background: var(--accent2)
-		max-width: 30rem
-		border-radius: 1rem
-		padding: 1.25rem 
 
+
+<style lang="sass">
 	input
 		background: var(--accent)
 		border-color: var(--accent3)
