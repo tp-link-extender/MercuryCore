615d7972f9956587edf3437cc8b37f30ea765ae3
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-09 13:36:16 +0000

    Clean up experimental login form

--- src/routes/(nofooter)/login/+page.svelte
+++ src/routes/(nofooter)/login/+page.svelte
@@ -1,60 +1,19 @@
-<!-- <script lang="ts">
-	import { enhance } from "$app/forms"
-	export let form: any
-</script> -->
-
 <svelte:head>
 	<title>Log in - Mercury</title>
 </svelte:head>
 
-<div class="row">
-	<div class="col bg-dark px-0">
-		<div class="container px-0">
-			s
-			<!--
-			<form class="form-group" method="POST" use:enhance>
-				<fieldset>
-					<div class="mb-3">
-						{#if form?.area == "username"}
-							<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
-						{:else}
-							<input name="username" type="text" class="form-control" placeholder="Username" />
-						{/if}
-			
-						<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-					</fieldset>
-				</form>
-			-->
-			</div>
+<div id="all" class="overflow-hidden">
+	<div class="row">
+		<div class="col">
+				bruh
 		</div>
-	</div>
-	<div class="col bg-dark px-0">
-		<div class="container px-0">
-			s
-			<!--
-			<form class="form-group" method="POST" use:enhance>
-				<fieldset>
-					<div class="mb-3">
-						{#if form?.area == "username"}
-							<input name="username" type="text" class="form-control is-invalid" placeholder="Username" />
-						{:else}
-							<input name="username" type="text" class="form-control" placeholder="Username" />
-						{/if}
-			
-						<button type="submit" class="container-fluid btn btn-primary">Log in</button>
-					</fieldset>
-				</form>
-			-->
-			</div>
+		<div class="col bg-dark">
+				bruh
 		</div>
 	</div>
 </div>
 
 <style lang="sass">
-	#all
-		overflow: hidden
-		
-	// input
-	// 	background: var(--accent)
-	// 	border-color: var(--accent3)
+	.row
+		height: 90vh
 </style>
