15ddfde8bd519e4dcebe6890c3a2a298f9abd88a
Lewin Kelly <heli@odex.cf>
2023-01-15 03:00:59 +0000

    Remove unnecessary code

--- src/routes/(main)/discover/+page.server.ts
+++ src/routes/(main)/discover/+page.server.ts
@@ -1,52 +1,9 @@
 import type { PageServerLoad } from "./$types"
-import { error, redirect } from "@sveltejs/kit"
 import { PrismaClient } from "@prisma/client"
-import { createClient, Graph } from "redis"
 
 const prisma = new PrismaClient()
 
-export const load: PageServerLoad = async ({ locals }) => {
-	const session = await locals.validateUser()
-	if (!session.session) throw redirect(302, "/login")
-
-	const client = createClient({ url: import.meta.env.REDIS_URL })
-	client.on("error", () => {
-		throw error(500, "Redis error")
-	})
-	await client.connect()
-	const graph = new Graph(client, "friends")
-
-	const friendsQuery = await graph.roQuery(
-		`
-		MATCH (:User { name: $user1 }) -[r:friends]-> (u:User)
-		RETURN u.name as name
-		`,
-		{
-			params: {
-				user1: session.user.username,
-			},
-		}
-	)
-
-	let friends: any[] = []
-
-	for (let i of friendsQuery.data || ([] as any)) {
-		if (i.name)
-			friends.push(
-				await prisma.user.findUnique({
-					where: {
-						username: i?.name,
-					},
-					select: {
-						username: true,
-						displayname: true,
-						image: true,
-						status: true,
-					},
-				})
-			)
-	}
-
+export const load: PageServerLoad = async () => {
 	const getPlaces = await prisma.place.findMany({
 		select: {
 			name: true,
@@ -56,6 +13,5 @@ export const load: PageServerLoad = async ({ locals }) => {
 	})
 	return {
 		places: getPlaces || [],
-		friends: friends,
 	}
 }
