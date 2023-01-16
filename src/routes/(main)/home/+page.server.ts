049986c21c8bbfd3c2d1874f955bedeee664547c
Lewin Kelly <heli@odex.cf>
2023-01-16 18:50:00 +0000

    Fix redis urls again

--- src/routes/(main)/home/+page.server.ts
+++ src/routes/(main)/home/+page.server.ts
@@ -9,10 +9,10 @@ export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.validateUser()
 	if (!session.session) throw redirect(302, "/login")
 
-	const client = createClient({ url: import.meta.env.REDIS_URL })
+	const client = createClient({ url: "redis://localhost:6479" })
 
 	client.on("error", e => {
-		console.log(e)
+		console.log("Redis error", e)
 		throw error(500, "Redis error")
 	})
 	await client.connect()
