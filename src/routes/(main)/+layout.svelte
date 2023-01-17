66180305aa537681c98150b85c702d2745d0f96f
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-17 19:31:58 +0000

    Fixes for landing page scrolling on mobile

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -3,17 +3,24 @@
 	import Footer from "$lib/components/Footer.svelte"
 </script>
 
-<Navbar />
-
-<main>
-	<slot />
-</main>
-
-<Footer />
+<div id="all">
+	<Navbar />
+	
+	<main>
+		<slot />
+	</main>
+	
+	<Footer />
+</div>
 
 <style lang="sass">
 	main
 		padding-bottom: 5vh
 		padding-top: 10vh
 		flex: 1 0 auto
+		
+	#all
+		display: flex
+		flex-direction: column
+		height: 100vh
 </style>
