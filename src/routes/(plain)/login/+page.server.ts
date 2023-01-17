42ec3dc76843da9ef123c64c03734099127d476b
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-01-17 18:51:39 +0000

    Login reg fixes

--- src/routes/(plain)/login/+page.server.ts
+++ src/routes/(plain)/login/+page.server.ts
@@ -28,10 +28,10 @@ export const actions: Actions = {
 		} catch (e) {
 			const error = e as Error
 			if (error.message === "AUTH_INVALID_PROVIDER_ID" || error.message === "AUTH_INVALID_PASSWORD") {
-				return fail(400, { msg: "Incorrect username or password" })
+				return fail(400, { area: "username", msg: "Incorrect username or password" })
 			}
 			console.error(error)
-			return fail(500, { msg: "An unexpected error occurred" })
+			return fail(500, { area: "unexp", msg: "An unexpected error occurred" })
 		}
 
 		throw redirect(302, "/home")
