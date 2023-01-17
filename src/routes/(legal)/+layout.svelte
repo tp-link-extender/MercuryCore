7769cc6d7b0095dd1920717386569203a9f89675
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-17 20:43:36 +0000

    Fix formatting on pages

--- src/routes/(legal)/+layout.svelte
+++ src/routes/(legal)/+layout.svelte
@@ -3,6 +3,8 @@
 	import Footer from "$lib/components/Footer.svelte"
 </script>
 
+<div id="bg" class="position-fixed w-100 h-100 z-n1" />
+
 <div id="all">
 	<Navbar />
 	
@@ -14,15 +16,16 @@
 </div>
 
 <style lang="sass">
+	main
+		padding-bottom: 5vh
+		padding-top: 10vh
+		flex: 1 0 auto
+		
+	#all
+		display: flex
+		flex-direction: column
+		height: 100vh
 
-    main
-	    padding-bottom: 5vh
-        padding-top: 10vh
-        flex: 1 0 auto
-
-    #all 
-        display: flex
-        flex-direction: column
-        height: 100vh
-        background: linear-gradient(-20deg, var(--footer) 50%, var(--mainaccent) 250%)
+	#bg
+		background: linear-gradient(-20deg, #0000 50%, var(--mainaccent) 250%)
 </style>
