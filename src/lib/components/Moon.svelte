3b496b6bb109745838a746ad759b2a9d15e9554e
kickturn <62524115+kickturn@users.noreply.github.com>
2023-01-21 21:04:08 -0600

    disable camera panning

--- src/lib/components/Moon.svelte
+++ src/lib/components/Moon.svelte
@@ -22,7 +22,7 @@
 
 <Canvas>
 	<PerspectiveCamera position={item.camPos}>
-		<OrbitControls autoRotate autoRotateSpeed={Math.random() > 0.5 ? 10 : -10} rotateSpeed={0.1} panSpeed={0.1} enableZoom={false} />
+		<OrbitControls autoRotate autoRotateSpeed={Math.random() > 0.5 ? 10 : -10} rotateSpeed={0.1} enablePan={false} panSpeed={0.1} enableZoom={false} />
 	</PerspectiveCamera>
 
 	<DirectionalLight color="white" position={{ x: -15, y: 45, z: 20 }} />
