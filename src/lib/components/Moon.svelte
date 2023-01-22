7aa12475f0682014ed2e5f67167318f4a96a1068
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-22 01:59:20 +0000

    Simplify moon code

--- src/lib/components/Moon.svelte
+++ src/lib/components/Moon.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { Canvas, DirectionalLight, HemisphereLight, OrbitControls, PerspectiveCamera, Three, useThrelte } from "@threlte/core"
+	import { Canvas, DirectionalLight, HemisphereLight, OrbitControls, PerspectiveCamera } from "@threlte/core"
 	import { GLTF } from "@threlte/extras"
 
 	const items = [
 		{
-			url: "/landing/moon.gltf",
+			name: "moon",
 			camPos: { x: 100, y: 100, z: 100 },
 		},
 		{
-			url: "/landing/mercury.gltf",
+			name: "mercury",
 			camPos: { x: 100, y: 20, z: 100 },
 		},
 		{
-			url: "/landing/rock.gltf",
+			name: "rock",
 			camPos: { x: 100, y: 100, z: 100 },
 		},
 	]
@@ -22,11 +22,11 @@
 
 <Canvas>
 	<PerspectiveCamera position={item.camPos}>
-		<OrbitControls autoRotate autoRotateSpeed={10} rotateSpeed={0.1} panSpeed={0.1} enableZoom={false} />
+		<OrbitControls autoRotate autoRotateSpeed={Math.random() > 0.5 ? 10 : -10} rotateSpeed={0.1} panSpeed={0.1} enableZoom={false} />
 	</PerspectiveCamera>
 
 	<DirectionalLight color="white" position={{ x: -15, y: 45, z: 20 }} />
 	<HemisphereLight skyColor="white" groundColor="#ac844c" intensity={0.4} />
 
-	<GLTF url={item.url} />
+	<GLTF url="/landing/{item.name}.gltf" />
 </Canvas>
