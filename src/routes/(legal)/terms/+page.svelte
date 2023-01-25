3a356dc9cc68bb63e4f1100155cd766b5b9b8c96
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-25 04:13:34 +0000

    Add basic Terms and Privacy policy

--- src/routes/(legal)/terms/+page.svelte
+++ src/routes/(legal)/terms/+page.svelte
@@ -5,20 +5,94 @@
 <h1 class="text-center light-text">Terms of Service</h1>
 
 <div class="container mt-5">
-	<p class="light-text">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet non curabitur gravida arcu ac tortor. Tellus molestie nunc non blandit. Metus vulputate eu scelerisque felis imperdiet proin fermentum. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Posuere urna nec tincidunt praesent semper feugiat nibh. Fermentum leo vel orci porta non pulvinar neque laoreet. Massa vitae tortor condimentum lacinia. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Nunc lobortis mattis aliquam faucibus. Ullamcorper eget nulla facilisi etiam. Nunc lobortis mattis aliquam faucibus purus in massa tempor nec.<br />
-		<br />
-		Et odio pellentesque diam volutpat. Adipiscing bibendum est ultricies integer quis auctor. Dui sapien eget mi proin sed libero. Integer vitae justo eget magna fermentum iaculis eu. Est velit egestas dui id ornare arcu. Sed vulputate odio ut enim blandit. Elit sed vulputate mi sit amet mauris commodo quis. Magnis dis parturient montes nascetur. Sed viverra ipsum nunc aliquet bibendum enim. Tristique et egestas quis ipsum. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Cum sociis natoque penatibus et magnis dis parturient montes. Enim lobortis scelerisque fermentum dui faucibus in ornare. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Vitae purus faucibus ornare suspendisse sed nisi. Amet justo donec enim diam vulputate ut pharetra. Adipiscing tristique risus nec feugiat in fermentum. Viverra maecenas accumsan lacus vel facilisis volutpat. Orci a scelerisque purus semper eget duis at.<br />
-		<br />
-		Orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Tristique magna sit amet purus gravida quis blandit. Mauris nunc congue nisi vitae suscipit tellus mauris. Maecenas accumsan lacus vel facilisis volutpat est velit. Leo integer malesuada nunc vel risus commodo viverra. Dignissim enim sit amet venenatis urna cursus eget. Amet justo donec enim diam vulputate ut pharetra sit amet. Ridiculus mus mauris vitae ultricies leo integer. Senectus et netus et malesuada fames ac turpis egestas. Eget mauris pharetra et ultrices neque ornare aenean euismod. Nulla at volutpat diam ut venenatis tellus. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Enim ut tellus elementum sagittis. Eget aliquet nibh praesent tristique magna sit.<br />
-		<br />
-		Vestibulum morbi blandit cursus risus at. Urna nunc id cursus metus aliquam eleifend mi in nulla. Nibh venenatis cras sed felis. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Sodales ut etiam sit amet nisl purus. Tortor posuere ac ut consequat semper viverra nam libero justo. Mauris sit amet massa vitae tortor condimentum. Faucibus turpis in eu mi bibendum neque. Dictum varius duis at consectetur lorem donec massa sapien faucibus. Iaculis urna id volutpat lacus laoreet. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus.<br />
-		<br />
-		Ut tortor pretium viverra suspendisse potenti. Ac ut consequat semper viverra. Enim sit amet venenatis urna cursus eget. Egestas maecenas pharetra convallis posuere morbi leo. Magna sit amet purus gravida. Nunc lobortis mattis aliquam faucibus purus in. Velit ut tortor pretium viverra. Auctor elit sed vulputate mi. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Eget gravida cum sociis natoque penatibus et.
-	</p>
+	<div class="light-text">
+		<p>
+			This Terms of Service Agreement (“agreement”) is an agreement made between you, or if you represent a legal entity, that legal entity (“you”) and Mercury, the owners of the Mercury website
+			and platform.
+		</p>
+		<p>
+			This agreement is a binding agreement between you and Mercury, and shall set forth the terms and conditions under which you can use Mercury. For the purposes of this agreement, this shall
+			mean the Mercury website, the Mercury client, and any parts or copies of them.
+		</p>
+		<p class="fw-bold">
+			Any services provided by Mercury are made available “as is” without warranty of any kind, express or implied, including but not limited to any warranties of merchantability, fitness for a
+			particular purpose and noninfringement. Mercury does not and cannot guarantee the results you may obtain by using Mercury.
+		</p>
+		<p class="fw-bold">
+			In no event will Mercury be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of, or in connection with the software
+			or the use or other dealings in the software.
+		</p>
+		<p>
+			Mercury hereby grants you a non-transferable, non-exclusive license to use Mercury or any of their services, provided that you agree to, comply with, and accept the entirety of the
+			following:
+		</p>
+
+		<ol>
+			<li>You enter and participate on the Mercury platform at your own risk.</li>
+			<li>You are 13 years of age or older, or have obtained parental consent to use Mercury.</li>
+			<li>
+				You are fully responsible for your own statements and materials that you upload to Mercury and any consequences of your statements and materials. You agree that you will not take any action directed towards attempting
+				to hold us responsible for any such materials or statements.
+			</li>
+			<li>
+				Any opinions or views expressed by users on Mercury are their own. We do not endorse, support, or otherwise give any credence or reason for reliance on any such statements or opinions.
+			</li>
+			<li>
+				Mercury is not required to monitor communications on the platform, and neither we nor any Moderators or Administrators are responsible for any material that any users upload, but we
+				may do so at our sole discretion.
+			</li>
+			<li>We reserve the right to remove or expel any party from participation and access to Mercury for any time or for any reason.</li>
+			<li>By accessing Mercury, you agree that you will not commit any of the following acts:</li>
+
+			<ol type="a" class="my-3">
+				<li>Using Mercury for any illegal purpose under applicable law.</li>
+				<li>Placing any material in Mercury that you do not have a right to upload, link to, post, or otherwise transmit under applicable law.</li>
+				<li>
+					Placing any material in Mercury that contains a false statement about any person/legal entity, infringes upon the privacy rights of any other person/legal entity, or threatens,
+					harrases, abuses, defames or embarrasses any other person/legal entity without explicit permission from them.
+				</li>
+				<li>Placing any obscene, pornographic, sexually explicit or violent materials in Mercury.</li>
+				<li>Repeatedly uploading unsolicited advertising, marketing materials, or sales/promotional materials to Mercury.</li>
+				<li>Placing any material in the Mercury that is disruptive or defamatory to Mercury or other users of the Mercury service including Moderators or Administrators.</li>
+			</ol>
+
+			<li>
+				By uploading any material to Mercury, you hereby grant any viewers of the material a perpetual, non-revocable, royalty-free license to view, download, store, and reproduce such material.
+			</li>
+			<li>Any users, Moderators, or Administrators of Mercury reserve the right to stay anonymous and unnamed if they wish to.</li>
+			<li>
+				You may use Mercury or any of their services only as outlined in this agreement. Any use of Mercury or any of their services that is not expressly allowed in this Agreement will
+				automatically terminate your rights to use Mercury under this agreement. If your right to use Mercury or any of their services is terminated, you shall immediately stop using Mercury.
+			</li>
+			<li>
+				This agreement contains the entire agreement between the Parties. No variation of any of the terms or conditions of this agreement may be made unless such variation is explicitly
+				agreed in writing and signed by both you and Mercury.
+			</li>
+		</ol>
+
+		<p>This agreement is governed by and constructed in accordance with the applicable laws of Scotland and the United Kingdom, and shall be subject to the non-exclusive jurisdiction of the courts of Scotland and Great Britain.</p>
+		<p>
+			If any term of this agreement is adjudged to be invalid or unenforceable, such term shall in no way affect any other term of this agreement or the application of such term in any other
+			circumstance or the validity or enforceability of this Agreement. The Parties will negotiate in good faith about a term that will replace the invalid or unenforceable term.
+		</p>
+		<p>
+			Please check these terms and conditions regularly. We may update them at any time without notice. You must agree to any changes if you continue to use Mercury after the terms and
+			conditions have been updated.
+		</p>
+		<p>
+			If you believe you know of someone/a group of people who have broken these terms or if you want more information, please contact our support team at <a href="mailto:support@banland.xyz"
+				>support@banland.xyz</a
+			>.
+		</p>
+		<em>Last updated: 25 January 2023</em>
+	</div>
 </div>
 
 <style lang="sass">
-	.container
-		width: 50rem
+	@media only screen and (min-width: 576px)
+		.container
+			width: 50rem
+
+	a
+		color: var(--mainaccent)
 </style>
