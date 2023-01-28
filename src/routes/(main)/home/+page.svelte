d9de9fe31e7d0723e90532b15a5960696827a8f4
Lewin Kelly <heli@odex.cf>
2023-01-28 08:14:38 +0000

    Improve navbar and add news section to homepage

--- src/routes/(main)/home/+page.svelte
+++ src/routes/(main)/home/+page.svelte
@@ -13,6 +13,21 @@
 
 	const greets = [`Hi, ${$user?.displayname}!`, `Hello, ${$user?.displayname}!`]
 
+	const news = [
+		{ time: new Date(), title: "Mercury is now life!", content: "Mercury is now live! We have a lot of features in store for you, so stay tuned!" },
+		{ time: new Date(), title: "Mercury is now life!", content: "Yes" },
+		{ time: new Date(), title: "Mercury is now life!", content: "Mercury is now live! We have a lot of features in store for you, so stay tuned!" },
+		{ time: new Date(), title: "Mercury now has Free Rocks", content: "Click here for Free Rocks!" },
+		{ time: new Date(), title: "Mercury", content: "Pls can i have invite ke" },
+		{
+			time: new Date(),
+			title: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+			content: "Mercury is now live! We have a lot of features in store for you, so stay tunedeoeeeeeeeeeeeeeeeeeeeeee! eeeeeeeeeeeeeeeeeeeeeeeeeee eeeee",
+		},
+		{ time: new Date(), title: "Mercury is now life!", content: "Mercury is now live! We have a lot of features in store for you, so stay tuned!" },
+		{ time: new Date(), title: "Mercury is now love!", content: "Yes" },
+	]
+
 	export let data: any
 	export let form: any
 </script>
@@ -23,7 +38,7 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col col-12 col-xl-5 col-md-6 col-sm-12">
+		<div class="col col-12 col-xxl-4 col-xl-5 col-md-6 col-sm-12">
 			<div class="top d-flex px-2">
 				<div class="pfp rounded-circle align-middle">
 					<img src={$user?.image} alt="You" class="rounded-circle img-fluid rounded-top-0" />
@@ -32,8 +47,8 @@
 					{greets[Math.floor(Math.random() * greets.length)]}
 				</h1>
 			</div>
-			<div id="feed" class="card mt-4 overflow-auto">
-				<div class="card-body light-text col">
+			<div id="feed" class="card mt-4">
+				<div class="card-body light-text overflow-auto">
 					<p>Post your status - your friends and followers can view how you're doing!</p>
 					<form method="POST" use:enhance>
 						<div class="input-group">
@@ -67,56 +82,91 @@
 			</div>
 		</div>
 
-		<div class="col col-9 col-xl-7 col-md-6 col-sm-12">
-			<div class="mt-4">
+		<div class="col col-12 col-xxl-4 col-xl-7 col-md-6">
+			<div class="col2">
 				{#if data.friends.length > 0}
 					<h2 class="h4 light-text">Friends</h2>
-					<div id="friends" class="d-flex">
-						{#each data.friends as friend}
-							<a class="friend px-2 mb-2 text-center light-text" href="/user/{friend.id}">
-								<div class="position-relative mb-2">
-									<div class="image-background rounded-circle">
-										<img src={friend.image} alt={friend.displayname} class="h-100 rounded-circle img-fluid rounded-top-0" />
+					<div class="home-row d-flex">
+						{#each Array(10) as _}
+							{#each data.friends as friend}
+								<a class="px-2 mb-2 text-center light-text text-decoration-none" href="/user/{friend.id}">
+									<div class="position-relative mb-2">
+										<div class="image-background rounded-circle">
+											<img src={friend.image} alt={friend.displayname} class="h-100 rounded-circle img-fluid rounded-top-0" />
+										</div>
+										{#if friend.status}
+											<span class="position-absolute bottom-0 end-0 badge rounded-circle {statusColours[friend.status]}">
+												<span class="visually-hidden">{friend.status}</span>
+											</span>
+										{/if}
 									</div>
-									{#if friend.status}
-										<span class="position-absolute bottom-0 end-0 badge rounded-circle {statusColours[friend.status]}">
-											<span class="visually-hidden">{friend.status}</span>
-										</span>
-									{/if}
-								</div>
-								{friend.displayname}
-							</a>
+									{friend.displayname}
+								</a>
+							{/each}
 						{/each}
 					</div>
 				{/if}
 			</div>
 			<div class="mt-5">
 				<h2 class="h4 light-text">Resume playing</h2>
-				<div class="row m-0 p-0 rounded-0">
-					{#each data.places || [] as place}
-						<div class="col col-6 col-sm-4 col-md-4 col-xl-3 text-center">
-							<Place {place} />
-						</div>
-					{/each}
+				<div class="home-row d-flex">
+					<div class="home-row d-flex">
+						{#each Array(20) as _}
+							{#each data.places || [] as place}
+								<div class="px-2 mb-2">
+									<div class="place">
+										<Place {place} />
+									</div>
+								</div>
+							{/each}
+						{/each}
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col col-xxl-4">
+			<div class="col2">
+				<h2 class="h4 light-text">News</h2>
+				<div id="news" class="card">
+					<div class="card-body row overflow-auto">
+						{#each news as thing}
+							<div class="p-1 col-xxl-6 col-lg-4 col-sm-6">
+								<div class="card light-text h-100">
+									<div class="card-body p-2">
+										<div class="mb-2 light-text">
+											<div class="fw-bold text-center text-truncate">{thing.title}</div>
+											<div id="date" class="ms-auto fw-italic text-center">{thing.time.toLocaleString()}</div>
+										</div>
+										<div id="gradient" class="position-absolute bottom-0 rounded-2" />
+										<p id="content" class="mb-0 p-1">
+											{thing.content}
+										</p>
+									</div>
+								</div>
+							</div>
+						{/each}
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<br /><br />
 </div>
 
 <style lang="sass">
 	.container
-		font-size: 0.9rem
+		max-width: 120rem
 
 	.top
-		width: fit-content
+		width: 100vw
 		.pfp
 			background: var(--accent)
 			width: 6rem
 			height: 6rem
 		h1
 			margin: auto 2rem
+
+	.col2
+		margin-top: 7rem
 
 	input
 		background: var(--accent)
@@ -125,9 +175,23 @@
 	.card
 		background: var(--accent)
 
-	#feed
+	#news
+		#date
+			min-width: 5rem
+		#content
+			max-height: 5rem
+			overflow: hidden
+		#gradient
+			left: 0
+			right: 0
+			height: 8rem
+			background: linear-gradient(0deg, var(--accent) 10%, rgba(0,0,0,0) 100%)
+
+	#feed, #news
 		background: var(--darker)
-		max-height: 50vh
+		max-height: 55vh
+
+	#feed
 		#user
 			align-items: center
 			.pfp
@@ -135,15 +199,16 @@
 				img
 					width: 2rem
 
-	#friends
+	.home-row
 		overflow-x: auto
-		.friend 
-			text-decoration: none
-			.badge
-				padding: 0.75rem
-			.image-background
-				background: var(--accent)
-				width: 7rem
-				height: 7rem
-				margin: auto
+
+		.badge
+			padding: 0.75rem
+		.place
+			width: 8rem
+			margin: auto
+		.image-background
+			background: var(--accent)
+			width: 7rem
+			margin: auto
 </style>
