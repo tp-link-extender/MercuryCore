f5977c191030300688911c4c2265f518ba6bd9fe
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-28 00:53:40 +0000

    Simplify code for friends, followers, and following routes

--- src/routes/(main)/user/[userid]/[f]/+page.server.ts
+++ src/routes/(main)/user/[userid]/[f]/+page.server.ts
@@ -3,10 +3,33 @@ import { prisma } from "$lib/server/prisma"
 import { roQuery } from "$lib/server/redis"
 import { error } from "@sveltejs/kit"
 
+const types = ["friends", "followers", "following"]
+
+const usersQueries: any = {
+	friends: `
+		MATCH (:User { name: $user }) -[r:friends]-> (u:User)
+		RETURN u.name AS name`,
+	followers: `
+		MATCH (:User { name: $user }) <-[r:follows]- (u:User)
+		RETURN u.name AS name`,
+	following: `
+		MATCH (:User { name: $user }) -[r:follows]-> (u:User)
+		RETURN u.name AS name`,
+}
+
+const numberQueries: any = {
+	friends: "RETURN SIZE((:User { name: $user }) -[:friends]-> ())",
+	followers: "RETURN SIZE((:User { name: $user }) <-[:follows]- ())",
+	following: "RETURN SIZE((:User { name: $user }) -[:follows]-> ())",
+}
+
 export const load: PageServerLoad = async ({ params }) => {
 	console.time("user")
 	if (!/^\d+$/.test(params.userid)) throw error(400, `Invalid user id: ${params.userid}`)
 	const id = parseInt(params.userid)
+
+	if (params.f && !types.includes(params.f)) throw error(400, `Not found: /user/${params.userid}/${params.f}`)
+	const type = params.f
 
 	const user = await prisma.user.findUnique({
 		where: {
@@ -27,10 +50,7 @@ export const load: PageServerLoad = async ({ params }) => {
 
 		async function Users() {
 			const usersQuery = await roQuery(
-				`
-				MATCH (:User { name: $user }) -[r:follows]-> (u:User)
-				RETURN u.name AS name
-				`,
+				usersQueries[type],
 				query,
 				false,
 				true
@@ -61,11 +81,12 @@ export const load: PageServerLoad = async ({ params }) => {
 
 		console.timeEnd("user")
 		return {
+			type,
 			username: user.username,
 			displayname: user.displayname,
 			img: user.image,
 			users: Users(),
-			number: roQuery("RETURN SIZE((:User { name: $user }) -[:follows]-> ())", query, true),
+			number: roQuery(numberQueries[type], query, true),
 		}
 	} else {
 		throw error(404, `Not found`)
