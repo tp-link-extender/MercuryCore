9e5446808dab417531bd08d10a489ebb39a4eb61
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-28 01:53:25 +0000

    Disable access to (main) routes for logged-out users

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -2,6 +2,7 @@ import type { LayoutServerLoad } from "./$types"
 import { redirect } from "@sveltejs/kit"
 
 export const load: LayoutServerLoad = async ({ locals }) => {
+	console.log("yeah")
 	const session = await locals.validate()
-	if (session) throw redirect(302, "/home")
+	if (!session) throw redirect(302, "/login")
 }
