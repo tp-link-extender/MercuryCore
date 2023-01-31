363af75fa26046a21b7c6641110db3a9b109b2ff
Lewin Kelly <heli@odex.cf>
2023-01-31 16:06:50 +0000

    Fix prisma add_keys migration
    (i think)

--- prisma/migrations/20230131132414_add_keys/migration.sql
+++ prisma/migrations/20230131132414_add_keys/migration.sql
@@ -1,40 +1,19 @@
-/*
-  Warnings:
+-- custom migration
 
-  - You are about to drop the column `expires` on the `session` table. All the data in the column will be lost.
-  - You are about to drop the column `hashed_password` on the `user` table. All the data in the column will be lost.
-  - You are about to drop the column `provider_id` on the `user` table. All the data in the column will be lost.
-  - Added the required column `active_expires` to the `session` table without a default value. This is not possible if the table is not empty.
-  - Made the column `displayname` on table `user` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- DropIndex
-DROP INDEX "user_provider_id_key";
-
--- AlterTable
-ALTER TABLE "session" DROP COLUMN "expires",
-ADD COLUMN     "active_expires" BIGINT NOT NULL;
-
--- AlterTable
-ALTER TABLE "user" DROP COLUMN "hashed_password",
-DROP COLUMN "provider_id",
-ALTER COLUMN "displayname" SET NOT NULL;
-
--- CreateTable
-CREATE TABLE "key" (
-    "id" TEXT NOT NULL,
-    "hashed_password" TEXT,
-    "user_id" INTEGER NOT NULL,
-    "primary" BOOLEAN NOT NULL,
-
-    CONSTRAINT "key_pkey" PRIMARY KEY ("id")
+CREATE TABLE public.key (
+  id TEXT PRIMARY KEY,
+  user_id INTEGER REFERENCES public.user(id) NOT NULL,
+  "primary" BOOLEAN NOT NULL,
+  hashed_password TEXT
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "key_id_key" ON "key"("id");
+INSERT INTO public.key (id, user_id, "primary", hashed_password)
+SELECT provider_id, id, TRUE, hashed_password
+FROM public.user;
 
--- CreateIndex
-CREATE INDEX "key_user_id_idx" ON "key"("user_id");
+ALTER TABLE public.user
+DROP COLUMN provider_id,
+DROP COLUMN hashed_password;
 
--- AddForeignKey
-ALTER TABLE "key" ADD CONSTRAINT "key_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE public.session
+RENAME COLUMN expires TO active_expires;
