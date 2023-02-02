b53100271014d251e55e6cfec46c29365bde015f
Lewin Kelly <heli@odex.cf>
2023-02-02 21:00:11 +0000

    Fix undefined user ids

--- src/routes/(main)/place/[place]/+page.svelte
+++ src/routes/(main)/place/[place]/+page.svelte
@@ -52,7 +52,7 @@
 				<div class="card-body">
 					<h2 class="light-text">{data.name}</h2>
 					<p class="light-text mt-2 mb-0">
-						<b>By</b> <a href="/user/{data.owner.id}" class="text-decoration-none">@{data.owner.displayname}</a>
+						<b>By</b> <a href="/user/{data.owner.number}" class="text-decoration-none">@{data.owner.displayname}</a>
 					</p>
 					<p class="light-text mb-0">Gears: <i class="fa-regular fa-circle-xmark" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tooltip on top" /></p>
 					<span class="badge text-bg-success mb-1">Online</span>
