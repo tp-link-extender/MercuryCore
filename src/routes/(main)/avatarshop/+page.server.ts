fb405ebfb3aa8b3eb76dd19c999c4cc5106d8b69
Lewin Kelly <heli@odex.cf>
2023-02-14 05:57:09 +0000

    Add inventory page

--- src/routes/(main)/avatarshop/+page.server.ts
+++ src/routes/(main)/avatarshop/+page.server.ts
@@ -6,6 +6,7 @@ export const load: PageServerLoad = async () => {
 		items: findItems({
 			select: {
 				name: true,
+				price: true,
 				id: true,
 			},
 		}),
---
+++ src/routes/(main)/inventory/+page.server.ts
@@ -0,0 +1,21 @@
+import type { PageServerLoad  } from "./$types"
+import { prisma } from "$lib/server/prisma"
+
+export const load: PageServerLoad = async ({ locals }) => {
+	console.time("inventory")
+	const session = await locals.validateUser()
+
+	const user = await prisma.user.findUnique({
+		where: {
+			number: session.user.number,
+		},
+		select: {
+			itemsOwned: true,
+		},
+	})
+
+	console.timeEnd("inventory")
+	return {
+		items: user?.itemsOwned
+	}
+}
