5b23d68cace01c44d7c0a7cda88f8f003227ba55
Lewin Kelly <heli@odex.cf>
2023-02-15 04:42:50 +0000

    Improve transactions page

--- src/routes/(main)/transactions/+page.server.ts
+++ src/routes/(main)/transactions/+page.server.ts
@@ -6,15 +6,20 @@ export const load: PageServerLoad = async () => {
 		transactions: prisma.transaction.findMany({
 			select: {
 				id: true,
+				time: true,
 				amountSent: true,
 				taxRate: true,
 				sender: {
 					select: {
+						image: true,
+						number: true,
 						displayname: true,
 					},
 				},
 				receiver: {
 					select: {
+						image: true,
+						number: true,
 						displayname: true,
 					},
 				},
