04ecdc3acb35699cfee5336b380cc5eea0aa7f34
Lewin Kelly <heli@odex.cf>
2023-02-19 05:36:54 +0000

    Clean up some functions

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,8 @@
 import { client } from "$lib/server/redis"
 import { handleServerSession } from "@lucia-auth/sveltekit"
 
-export const load = handleServerSession(async () => {
-	return {
-		bannerText: client.get("bannerText"),
-		bannerColour: client.get("bannerColour"),
-		bannerTextLight: client.get("bannerTextLight") // truthy or falsy string
-	}
-})
+export const load = handleServerSession(async () => ({
+	bannerText: client.get("bannerText"),
+	bannerColour: client.get("bannerColour"),
+	bannerTextLight: client.get("bannerTextLight"), // truthy or falsy string
+}))
