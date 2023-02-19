a5733dfac398b53fb36176e65fcc7653e3051912
Lewin Kelly <heli@odex.cf>
2023-02-19 11:38:28 +0000

    Fix group styling

--- src/lib/components/Group.svelte
+++ src/lib/components/Group.svelte
@@ -3,28 +3,21 @@
 </script>
 
 <a class="card text-center light-text text-decoration-none h6 rounded-4 m-0" href="/groups/{group.name}">
-	<div class="p-3">
-		<p class="mb-1">
-			{group.name}
-		</p>
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<div class="mt-auto mb-1">
-			<div class="float-end">
-				<span>
-					<i class="fa fa-user opacity-75" />
-					{group.members}
-				</span>
-			</div>
+	<div class="p-3 d-flex flex-column h-100">
+		{group.name}
+		<div class="mt-auto mb-1 float-end">
+			<span class="float-end">
+				<i class="fa fa-user opacity-75" />
+				{group.members}
+			</span>
 		</div>
 	</div>
 </a>
 
 <style lang="sass">
 	.card
+		width: 11rem
+		height: 11rem
 		border: none
 		background: var(--darker)
 	a
