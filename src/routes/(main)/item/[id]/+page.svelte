e37ba9680d3858eab9bdafb7fb3ac154d3d0ba1d
Lewin Kelly <heli@odex.cf>
2023-02-20 15:10:55 +0000

    Add owners to item page

--- src/routes/(main)/item/[id]/+page.svelte
+++ src/routes/(main)/item/[id]/+page.svelte
@@ -118,24 +118,17 @@
 			<p class="light-text">what go here</p>
 		</div>
 		<div class="tab-pane fade" id="pills-game" role="tabpanel" aria-labelledby="pills-game-tab" tabindex={0}>
-			<h4 class="light-text">Server List</h4>
-			<div class="card mb-2">
-				<div class="card-body">
-					<div class="row">
-						<div class="col col-2">
-							<p class="light-text mb-2">Currently Playing: 6/20</p>
-							<button id="join" class="btn btn-sm btn-success">Join Server</button>
+			<div class="row">
+				{#each data.owners as owner}
+					<a href="/user/{owner.number}" class="d-flex text-decoration-none py-2 col col-lg-3 col-md-4 col-sm-6">
+						<div class="me-3 rounded-circle pfp">
+							<img src={owner?.image} alt={owner.displayname} class="rounded-circle rounded-top-0" />
 						</div>
-						<div class="col">
-							<img src={$user?.image} id="pfp" alt="You" height="75" width="75" class="rounded-circle img-fluid rounded-top-0 ml-2" />
-							<img src={$user?.image} id="pfp" alt="You" height="75" width="75" class="rounded-circle img-fluid rounded-top-0 ml-2" />
-							<img src={$user?.image} id="pfp" alt="You" height="75" width="75" class="rounded-circle img-fluid rounded-top-0 ml-2" />
-							<img src={$user?.image} id="pfp" alt="You" height="75" width="75" class="rounded-circle img-fluid rounded-top-0 ml-2" />
-							<img src={$user?.image} id="pfp" alt="You" height="75" width="75" class="rounded-circle img-fluid rounded-top-0 ml-2" />
-							<img src={$user?.image} id="pfp" alt="You" height="75" width="75" class="rounded-circle img-fluid rounded-top-0 ml-2" />
-						</div>
-					</div>
-				</div>
+						<p class="light-text my-auto h5 me-4 text-truncate">
+							{owner.displayname}
+						</p>
+					</a>
+				{/each}
 			</div>
 		</div>
 	</div>
@@ -183,6 +176,11 @@
 			border-width: 0px 0px 2px 0px
 			border-color: var(--bs-blue)
 
-	#pfp
-		background: var(--accent2)
+	.pfp
+		background: var(--accent)
+		width: 3.5rem
+		height: 3.5rem
+		img
+			width: 3.5rem
+			height: 3.5rem
 </style>
