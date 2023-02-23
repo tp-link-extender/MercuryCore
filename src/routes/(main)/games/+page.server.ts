12067b3cade611f89f3b64bdd366abba0324873c
Lewin Kelly <heli@odex.cf>
2023-02-23 19:11:26 +0000

    Add instant search to games, avatar shop, and groups pages

--- src/routes/(main)/games/+page.server.ts
+++ src/routes/(main)/games/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from "./$types"
+import type { PageServerLoad, Actions } from "./$types"
 import { findPlaces } from "$lib/server/prisma"
 
 export const load: PageServerLoad = async () => ({
@@ -10,3 +10,24 @@ export const load: PageServerLoad = async () => ({
 		},
 	}),
 })
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		const filter = (await request.formData()).get("query")?.toString()
+		return {
+			places: await findPlaces({
+				where: {
+					name: {
+						contains: filter,
+						mode: "insensitive",
+					}
+				},
+				select: {
+					name: true,
+					slug: true,
+					image: true,
+				},
+			})
+		}
+	},
+}
