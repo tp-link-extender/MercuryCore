491f9014e1584e55aed34ab1cda0c5cf42036d33
Lewin Kelly <heli@odex.cf>
2023-02-26 16:32:17 +0000

    Redirect /asset to /asset/assetfetch

---
+++ src/routes/(main)/(rbxclient)/asset/assetfetch/+server.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from "./$types"
+import { SignData } from "$lib/server/sign"
+import { error, redirect } from "@sveltejs/kit"
+import md5 from "crypto-js/md5"
+import fs from "fs"
+
+export const GET: RequestHandler = async ({ url, setHeaders }) => {
+	const ID = url.searchParams.get("id")
+	if (!ID || !/^\d+$/.test(ID)) throw error(400, "Invalid Request")
+
+	if (fs.existsSync(`./corescripts/${ID}.lua`)) {
+		let file = fs.readFileSync(`./corescripts/${ID}.lua`)
+
+		setHeaders({
+			"Content-Type": "binary/octet-stream",
+			"Content-Disposition": `attachment; filename="${md5(file).toString()}"`,
+		})
+
+		let file2 = file.toString().replaceAll("roblox.com/asset", "banland.xyz/asset")
+
+		if (ID != "38037265") file2 = SignData(file2, parseInt(ID))
+
+		return new Response(file2)
+	}
+
+	throw redirect(302, `https://assetdelivery.roblox.com/v1/asset/?${ID}`)
+}
