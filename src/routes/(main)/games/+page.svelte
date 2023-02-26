846a6886db2e6e8c5171b7822c93460289009d03
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-02-26 16:35:05 +0000

    Update +page.svelte

--- src/routes/(main)/games/+page.svelte
+++ src/routes/(main)/games/+page.svelte
@@ -44,7 +44,7 @@
 		<h1 class="col light-text">
 			Games
 			<a href="/games/create" class="btn btn-primary ms-4">Create</a>
-			<a href="/games/create" class="btn btn-primary ms-4">Create</a>
+			<a href="mercury-player:1+launchmode:ide" class="btn btn-danger ms-4">Join Testing Localhost Server</a>
 		</h1>
 		<div class="col-8">
 			<form use:enhance method="POST" action="/search" class="row">
