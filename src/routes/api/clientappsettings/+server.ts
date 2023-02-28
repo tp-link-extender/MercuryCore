cc123967d08392a0d40c5d52d31eb0e6f0417abc
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-02-28 20:09:14 +0000

    Update +server.ts

--- src/routes/api/clientappsettings/+server.ts
+++ src/routes/api/clientappsettings/+server.ts
@@ -1,5 +1,53 @@
 import type { RequestHandler } from "./$types"
+import { json } from "@sveltejs/kit"
 
 export const GET: RequestHandler = async () => {
-	return new Response()
+	return json({
+		DFFlagUseKeyframeHumanoidAnimations: "True",
+		FFlagUseKeyframeHumanoidAnimations: "True",
+		FFlagRenderNewMaterials: "True",
+		FFlagRenderAnisotropy: "True",
+		FFlagEnableNPCServerAnimation: "True",
+		FFlagFireStoppedAnimSignal: "True",
+		FFlagPhysics60HZ: "True",
+		FFlagFRMUse60FPSLockstepTable: "True",
+		FFlagFRMAdjustForMultiCore: "True",
+		FFlagFRMRecomputeDistanceFrameDelay: "True",
+		DFFlagHeartBeatCanRunTwiceFor30Hz: "True",
+		FFlagFixFirstPersonToolLag: "True",
+		FFlagUseNewCameraZoomPath: "True",
+		FFlagUseNewCameraOcclusionZoom: "True",
+		FFlagRenderSoftParticles: "True",
+		FFlagRenderClearLightGridOnBind: "True",
+		FFlagRenderFastClusterMergeBuffers: "True",
+		FFlagRenderLightGridEnabled: "True",
+		FFlagRenderLightGridSIMD: "True",
+		FFlagRenderLightGridShadowsSmooth: "True",
+		FFlagSSAOEnable: "True",
+
+		FFlagUserHttpAPIVisible: "True",
+		FFlagModuleScriptsVisible: "True",
+		FFlagSurfaceGuiVisible: "True",
+		FFlagCreateServerScriptServiceInStudio: "True",
+		FFlagCreateServerStorageInStudio: "True",
+		FFlagCreateReplicatedStorageInStudio: "True",
+
+		FFlagStudioInitializeViewOnPaint: "True",
+		FFlagStudioZoomExtentsExplorerFixEnabled: "True",
+		FFlagStudioPropertiesRespectCollisionToggle: "True",
+		FFlagStudioScriptBlockAutocomplete: "True",
+		FFlagStudioRightClickFixesEnabled: "True",
+
+		FFlagQtFindInExplorer: "True",
+		FFlagQtRightClickContextMenu: "True",
+		FFlagQtTooltipObjectDescriptions: "True",
+
+		FFlagRibbonBarEnabled: "True",
+		FFlagAllowBrowsableAppearInExplorer: "True",
+		FFlagBetterGuiObjectInsertDefaults: "True",
+
+		FFlagTrimExtraSlashesAfterRobloxDomain: "False",
+		FFlagStudioIntellesenseEnabled: "False",
+		FFlagStudioShowToolboxByDefault: "False",
+	})
 }
