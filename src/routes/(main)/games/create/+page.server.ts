b7c2d9ad240d17372f6128f701716d150afe3d2c
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-03-02 23:11:00 +0000

    PRVIAT (HGELLO

--- src/routes/(main)/games/create/+page.server.ts
+++ src/routes/(main)/games/create/+page.server.ts
@@ -10,11 +10,16 @@ export const actions: Actions = {
 		const data = await request.formData()
 		const name = data.get("name")?.toString() || ""
 		const description = data.get("description")?.toString()
+		const serverIP = data.get("serverIP")?.toString()
+		const serverPort = parseInt(data.get("serverPort")?.toString() || "")
+		const maxPlayers = parseInt(data.get("maxPlayers")?.toString() || "")
+		const privateServer = Boolean(data.get("privateServer"))
 
 		const slug = name.toLowerCase()
+		
 
-		if (!name || !description) return fail(400, { msg: "Missing fields" })
-		if (name.length < 3 || name.length > 50 || description.length > 1000) return fail(400, { msg: "Invalid fields" })
+		if (!name || !description || !serverIP || !serverPort || !maxPlayers) return fail(400, { msg: "Missing fields" })
+		if (name.length < 3 || name.length > 50 || description.length > 1000 || serverPort > 65535 || serverPort < 53640 || maxPlayers > 99 || maxPlayers < 1 || !/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?|^((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(serverIP)) return fail(400, { msg: "Invalid fields" })
 
 		if (
 			await prisma.place.findUnique({
@@ -36,6 +41,7 @@ export const actions: Actions = {
 						description,
 						serverIP,
 						serverPort,
+						privateServer,
 						maxPlayers,
 						image: `/place/placeholderIcon${Math.floor(Math.random() * 3) + 1}.png`,
 						ownerUsername: user.username,
