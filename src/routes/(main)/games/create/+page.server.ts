281431430b4abd374d7f493e3f7cbb1802231aac
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-03-03 17:06:47 +0000

    Update game settings

--- src/routes/(main)/games/create/+page.server.ts
+++ src/routes/(main)/games/create/+page.server.ts
@@ -18,6 +18,13 @@ export const actions: Actions = {
 		if (!name || !description || !serverIP || !serverPort || !maxPlayers) return fail(400, { msg: "Missing fields" })
 		if (name.length < 3 || name.length > 50 || description.length > 1000 || serverPort > 65535 || serverPort < 53640 || maxPlayers > 99 || maxPlayers < 1 || !/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?|^((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(serverIP)) return fail(400, { msg: "Invalid fields" })
 
+		const gameCount = await prisma.user.findUnique({
+			where: {id: user.userId},
+			select: {_count: { select: {places: true} }}
+		})
+
+		if (gameCount && gameCount?._count.places >= 2) return fail(400, { msg: "You may only have 2 places at most" })
+
 		let place: any
 		try {
 			await prisma.$transaction(async tx => {
