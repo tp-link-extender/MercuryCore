5af414dcb4dadac212460f28ff0742addf0c396a
Lewin Kelly <heli@odex.cf>
2023-03-05 23:05:13 +0000

    Fix formatting and set no-cache headers on characterfetch

--- src/routes/(rbxclient)/asset/characterfetch/+server.ts
+++ src/routes/(rbxclient)/asset/characterfetch/+server.ts
@@ -1,12 +1,17 @@
-import type { RequestHandler } from './$types';
-import { error } from '@sveltejs/kit';
+import type { RequestHandler } from "./$types"
+import { error } from "@sveltejs/kit"
 
-export const GET: RequestHandler = async ({url}) => {
-    const userId = url.searchParams.get("userID")
+export const GET: RequestHandler = async ({ url, setHeaders }) => {
+	const userId = url.searchParams.get("userID")
 
-    if(!userId || !/^\d+$/.test(userId)) throw error(400, "Invalid Request")
+	if (!userId || !/^\d+$/.test(userId)) throw error(400, "Invalid Request")
 
-    let charApp = `https://banland.xyz/Asset/BodyColors.ashx?id=${userId}`
+	let charApp = `https://banland.xyz/Asset/BodyColors.ashx?id=${userId}`
 
-    return new Response(charApp);
-};
+	setHeaders({
+		Pragma: "no-cache",
+		"Cache-Control": "no-cache",
+	})
+
+	return new Response(charApp)
+}
