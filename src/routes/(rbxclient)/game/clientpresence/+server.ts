8ab86af24ee0be4408080f41fb1f6619692785ad
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-03-11 18:42:55 +0000

    Client presence

--- src/routes/(rbxclient)/game/clientpresence/+server.ts
+++ src/routes/(rbxclient)/game/clientpresence/+server.ts
@@ -1,5 +1,21 @@
-import type { RequestHandler } from './$types';
+import { error } from "@sveltejs/kit"
+import { prisma } from "$lib/server/prisma"
 
-export const GET: RequestHandler = async () => {
-    return new Response("OK");
-};
+export async function GET({ url, request, setHeaders }) {
+	const ticket = url.searchParams.get("ticket") as string
+
+	if (!ticket) throw error(400, "Invalid Request")
+	if (request.headers.get("user-agent") != "Roblox/WinInet") throw error(400, "Invalid Request")
+
+	await prisma.gameSessions.update({
+		where: { ticket: ticket },
+		data: { ping: Math.floor(Date.now() / 1000) },
+	})
+
+	setHeaders({
+		Pragma: "no-cache",
+		"Cache-Control": "no-cache",
+	})
+
+	return new Response("OK")
+}
