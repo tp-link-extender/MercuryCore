877ce15895e41bc8d2fe149e87e47ac34e3d2e98
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-03-12 19:58:34 +0000

    Banner system

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,17 @@
+import { prisma } from "$lib/server/prisma"
 import { client } from "$lib/server/redis"
 import { handleServerSession } from "@lucia-auth/sveltekit"
 
 export const load = handleServerSession(async () => ({
 	// Some globals for pages, loaded from Redis
-	bannerText: client.get("bannerText"),
-	bannerColour: client.get("bannerColour"),
-	bannerTextLight: client.get("bannerTextLight"), // truthy or falsy string
+
+	banners: prisma.announcements.findMany({
+		select: {
+			body: true,
+			bgColour: true,
+			active: true,
+			textLight: true,
+		}
+	})
+	
 }))
