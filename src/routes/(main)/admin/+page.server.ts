4cc007b51bd6344d600a7ce427df4d11ab958918
Lewin Kelly <heli@odex.cf>
2023-03-14 19:09:45 +0000

    Add disk space and memory usage to admin panel in Statistics section

--- src/routes/(main)/admin/+page.server.ts
+++ src/routes/(main)/admin/+page.server.ts
@@ -1,7 +1,10 @@
+import os from "os"
 import { authoriseAllAdmin } from "$lib/server/lucia"
+import checkDiskSpace from "check-disk-space"
 
 // Make sure a user is an administrator before loading the page.
 export async function load({ locals }) {
 	await authoriseAllAdmin(locals)
-}
 
+	return checkDiskSpace(os.homedir()) // because top level await doesnt work in svelte
+}
