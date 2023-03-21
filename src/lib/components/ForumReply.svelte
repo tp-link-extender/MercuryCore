3e5417f94e9a2024f76fb96a76405ab7e8bec23f
Lewin Kelly <heli@odex.cf>
2023-03-21 22:59:16 +0000

    Improve styling of forum replies

--- src/lib/components/ForumReply.svelte
+++ src/lib/components/ForumReply.svelte
@@ -8,10 +8,11 @@
 	export let reply: any
 	export let num: number
 	export let baseDepth: Writable<number>
-	export let depth: number = $baseDepth
+	export let depth = $baseDepth
 	export let replyingTo: Writable<string>
 	export let postAuthorName: string
 	export let forumCategory: string
+	const forumCategoryLower = forumCategory.toLowerCase()
 	export let postId: string
 	export let repliesCollapsed: Writable<any>
 	export let topLevel = false
@@ -29,69 +30,26 @@
 	}
 </script>
 
-{#if reply && reply.author}
-	{#if depth == $baseDepth && !topLevel}
+{#if depth == $baseDepth && !topLevel}
+	<a href="/forum/{forumCategoryLower}/{postId}" class="text-decoration-none">
+		<i class="fa fa-arrow-left me-2" />
+		Parent post
+	</a>
+	{#if reply.parentReplyId}
+		<br />
 		<a
-			href="/forum/{forumCategory.toLowerCase()}/{postId}"
+			href="/forum/{forumCategoryLower}/{postId}/{reply.parentReplyId}?depth={depth -
+				1}"
 			class="text-decoration-none">
-			<i class="fa fa-arrow-left me-2" />
-			Parent post
+			<i class="fa fa-arrow-up me-2" />
+			Parent reply
 		</a>
-		{#if reply.parentReplyId}
-			<br />
-			<a
-				href="/forum/{forumCategory.toLowerCase()}/{postId}/{reply.parentReplyId}?depth={depth -
-					1}"
-				class="text-decoration-none">
-				<i class="fa fa-arrow-up me-2" />
-				Parent reply
-			</a>
-		{/if}
 	{/if}
-	<div in:fade|global={{ num }} class="d-flex mt-2 w-100">
-		<form
-			use:enhance
-			class="me-2 p-1 pt-2 ps-0 sidebar2 d-flex h-100"
-			method="POST"
-			action="/forum/{forumCategory}?/like">
-			<input type="hidden" name="replyId" value={reply.id} />
-			<div class="row mb-2">
-				<div>
-					<button
-						name="action"
-						value={reply.likes ? "unlike" : "like"}
-						class="smallbutton p-0 btn btn-sm">
-						{#if reply.likes}
-							<i class="fa fa-thumbs-up text-success" />
-						{:else}
-							<i class="fa-regular fa-thumbs-up" />
-						{/if}
-					</button>
-				</div>
-				<span
-					class="my-1 text-center {reply.likes
-						? 'text-success fw-bold'
-						: reply.dislikes
-						? 'text-danger fw-bold'
-						: ''}">
-					{reply.likeCount - reply.dislikeCount}
-				</span>
-				<div>
-					<button
-						name="action"
-						value={reply.dislikes ? "undislike" : "dislike"}
-						class="smallbutton p-0 btn btn-sm">
-						{#if reply.dislikes}
-							<i class="fa fa-thumbs-down text-danger" />
-						{:else}
-							<i class="fa-regular fa-thumbs-down" />
-						{/if}
-					</button>
-				</div>
-			</div>
-		</form>
+{/if}
 
-		<div class="w-100">
+{#if reply && reply.author}
+	<div class:mt-2={!$repliesCollapsed?.[reply.id]} class="d-flex">
+		<span class="d-flex flex-column">
 			<a
 				href="/user/{reply.author.number}"
 				class="user d-flex text-decoration-none pt-2">
@@ -101,104 +59,161 @@
 						alt={reply.author.username}
 						class="rounded-circle rounded-top-0" />
 				</span>
-				<span
-					class="fw-bold ms-3 {reply.author.username == postAuthorName
-						? 'text-primary'
-						: 'light-text '}">
-					{reply.author.username}
-					{#if reply.author.username == postAuthorName}
-						<i class="fa fa-microphone ms-2" />
-					{/if}
-				</span>
-				<small class="light-text ps-4">
-					{reply.posted.toLocaleString()}
-				</small>
 			</a>
-			<p class="my-2">
-				{reply.content}
-			</p>
-			{#if $replyingTo != reply.id}
-				<button
-					on:click={() => replyingTo.set(reply.id)}
-					class="p-0 btn btn-sm grey-text px-1">
-					<i class="fa-regular fa-message pe-2" />
-					Reply
-				</button>
-				<Report
-					user={reply.author.username}
-					url="/forum/{forumCategory}/{reply.id}"
-					reverse />
-			{/if}
-			{#if $replyingTo == reply.id}
-				<div class="mb-2 card reply">
-					<div class="card-body p-3 pt-1 pb-0">
-						<form
-							use:enhance
-							on:submit={() => replyingTo.set("")}
-							class="mb-4"
-							method="POST"
-							action="/forum/{forumCategory}/{reply.id}">
-							<input
-								type="hidden"
-								name="replyId"
-								value={reply.id} />
-							<label
-								for="content"
-								class="form-label light-text mt-2">
-								Post a Reply
-							</label>
-							<fieldset>
-								<textarea
-									bind:value={content}
-									class="form-control valid mb-2"
-									required
-									minlength="5"
-									maxlength="1000"
-									name="content"
-									placeholder="What are your thoughts?"
-									rows="4" />
-								<button type="submit" class="btn btn-success">
-									<i class="fa-regular fa-message me-2" />
-									Reply
-								</button>
+			<button
+				on:click={collapse(reply.id)}
+				id="collapse"
+				class="p-0 border-0 h-100 mt-3" />
+		</span>
+
+		{#if $repliesCollapsed?.[reply.id]}
+			<button
+				on:click={collapse(reply.id)}
+				id="collapse2"
+				class="m-2 ms-3 p-0 mt-0">
+				<small>
+					{reply.author.username}: {reply.content}
+				</small>
+			</button>
+		{:else}
+			<div in:fade|global={{ num }} class="w-100">
+				<div class="d-flex w-100">
+					<div class="w-100">
+						<a
+							href="/user/{reply.author.number}"
+							id="userlink"
+							class="user d-flex text-decoration-none pt-2 ms-3">
+							<span
+								class="fw-bold {reply.author.username ==
+								postAuthorName
+									? 'text-primary'
+									: 'light-text '}">
+								{reply.author.username}
+								{#if reply.author.username == postAuthorName}
+									<i class="fa fa-microphone ms-2" />
+								{/if}
+							</span>
+							<small class="light-text ps-4">
+								{reply.posted.toLocaleString()}
+							</small>
+						</a>
+						<p class="my-2">
+							{reply.content}
+						</p>
+						{#if $replyingTo != reply.id}
+							<form
+								use:enhance
+								class="d-inline me-2"
+								method="POST"
+								action="/forum/{forumCategory}?/like">
+								<input
+									type="hidden"
+									name="replyId"
+									value={reply.id} />
+
 								<button
-									on:click={() => replyingTo.set("")}
-									class="btn btn-dark grey-text ms-1">
-									<i class="fa fa-cancel me-2" />
-									Cancel
+									name="action"
+									value={reply.likes ? "unlike" : "like"}
+									class="smallbutton p-0 btn btn-sm">
+									{#if reply.likes}
+										<i
+											class="fa fa-thumbs-up text-success" />
+									{:else}
+										<i class="fa-regular fa-thumbs-up" />
+									{/if}
 								</button>
-							</fieldset>
-						</form>
+								<span
+									class="my-1 text-center {reply.likes
+										? 'text-success fw-bold'
+										: reply.dislikes
+										? 'text-danger fw-bold'
+										: ''}">
+									{reply.likeCount - reply.dislikeCount}
+								</span>
+								<button
+									name="action"
+									value={reply.dislikes
+										? "undislike"
+										: "dislike"}
+									class="smallbutton p-0 btn btn-sm">
+									{#if reply.dislikes}
+										<i
+											class="fa fa-thumbs-down text-danger" />
+									{:else}
+										<i class="fa-regular fa-thumbs-down" />
+									{/if}
+								</button>
+							</form>
+							<button
+								on:click={() => replyingTo.set(reply.id)}
+								class="p-0 btn btn-sm grey-text px-1">
+								<i class="fa-regular fa-message pe-2" />
+								Reply
+							</button>
+							<Report
+								user={reply.author.username}
+								url="/forum/{forumCategory}/{reply.id}"
+								reverse />
+						{:else}
+							<div class="mb-2 card reply">
+								<div class="card-body p-3 pt-1 pb-0">
+									<form
+										use:enhance
+										on:submit={() => replyingTo.set("")}
+										class="mb-4"
+										method="POST"
+										action="/forum/{forumCategory}/{reply.id}">
+										<input
+											type="hidden"
+											name="replyId"
+											value={reply.id} />
+										<label
+											for="content"
+											class="form-label light-text mt-2">
+											Post a Reply
+										</label>
+										<fieldset>
+											<textarea
+												bind:value={content}
+												class="form-control valid mb-2"
+												required
+												minlength="5"
+												maxlength="1000"
+												name="content"
+												placeholder="What are your thoughts?"
+												rows="4" />
+											<button
+												type="submit"
+												class="btn btn-success">
+												<i
+													class="fa-regular fa-message me-2" />
+												Reply
+											</button>
+											<button
+												on:click={() =>
+													replyingTo.set("")}
+												class="btn btn-dark grey-text ms-1">
+												<i class="fa fa-cancel me-2" />
+												Cancel
+											</button>
+										</fieldset>
+									</form>
+								</div>
+							</div>
+						{/if}
 					</div>
 				</div>
-			{/if}
-		</div>
-	</div>
 
-	{#if depth > $baseDepth + 8}
-		<a
-			href="/forum/{forumCategory.toLowerCase()}/{postId}/{reply.id}?depth={depth}"
-			class="text-decoration-none">
-			<i class="fa fa-arrow-down me-2" />
-			More replies
-		</a>
-	{/if}
+				{#if depth > $baseDepth + 8}
+					<a
+						href="/forum/{forumCategoryLower}/{postId}/{reply.id}?depth={depth}"
+						class="text-decoration-none">
+						<i class="fa fa-arrow-down me-2" />
+						More replies
+					</a>
+				{/if}
 
-	{#each reply.replies as reply2}
-		<div class="d-flex">
-			<button
-				on:click={collapse(reply2.id)}
-				id="collapse"
-				class="mx-1 p-0 border-0" />
-			{#if $repliesCollapsed?.[reply2.id]}
-				<button
-					on:click={collapse(reply2.id)}
-					id="collapse2"
-					class="m-2">
-					Expand replies
-				</button>
-			{:else}
-				<div class="w-100">
+				{#each reply.replies as reply2}
 					<!-- Get READY for some RECURSION!!! -->
 					<svelte:self
 						reply={reply2}
@@ -210,33 +225,35 @@
 						{repliesCollapsed}
 						depth={depth + 1}
 						{baseDepth} />
-				</div>
-			{/if}
-		</div>
-	{/each}
+				{/each}
+			</div>
+		{/if}
+	</div>
 {/if}
 
 <style lang="sass">
-	.sidebar2
-		width: 2rem
-
 	#collapse, #collapse2
 		transition: all 0.2s ease-out
 	
 	#collapse
-		width: 14px
+		width: auto
 		background: var(--accent2)
-		border-left: 3px solid var(--background) !important
-		border-right: 9px solid var(--background) !important
+		border-left: 9px solid var(--background) !important
+		border-right: 13px solid var(--background) !important
 		&:hover
 			background: var(--grey-text)
 
 	#collapse2
 		border: none
 		background: none
+
 		color: var(--accent3)
 		&:hover
 			color: var(--grey-text)
+
+		small
+			max-width: 10rem
+			text-overflow: ellipsis
 
 	.card
 		max-width: 75%
@@ -256,11 +273,14 @@
 	p
 		word-break: break-word
 		
+	#userlink
+		margin-top: 1px
+
 	.user
 		align-items: center 
-		.pfp
-			background: var(--accent2)
-			img
-				max-width: 2rem
-				width: 2rem
+	.pfp
+		background: var(--accent2)
+		img
+			max-width: 1.5rem
+			width: 1.5rem
 </style>
