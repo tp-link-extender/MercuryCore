d0a7b101b778af3ca808a8e04868234b4e666114
Lewin Kelly <heli@odex.cf>
2023-03-24 22:44:41 +0000

    Bump bootstrap js version to 5.3.0-alpha2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,7 +31,7 @@
 
 	<script
 		defer
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
 		crossorigin="anonymous"></script>
 </svelte:head>
