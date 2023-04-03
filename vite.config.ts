5428c153a83118e9307137b5c2276ee8b4eacf2b
Lewin Kelly <heli@odex.cf>
2023-04-03 19:07:37 +0100

    Add vite-plugin-warmup for faster server start

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite"
+import { warmup } from "vite-plugin-warmup"
 
 export default {
-	plugins: [sveltekit()],
+	plugins: [
+		warmup({
+			clientFiles: ["./src/**/*.svelte"],
+		}),
+		sveltekit(),
+	],
 
 	ssr: {
 		noExternal: ["three", "troika-three-text"],
