4c02520f923dbf4adaa5c3e066dbe8336d5b6575
Lewin Kelly <heli@odex.cf>
2023-04-16 23:39:38 +0100

    Formatting fixes and add more data to statistics page

--- src/routes/(legal)/statistics/+page.svelte
+++ src/routes/(legal)/statistics/+page.svelte
@@ -28,7 +28,7 @@
 			<td class="ps-4">{data.items}</td>
 		</tr>
 		<tr>
-			<td>Currency</td>
+			<td><i class="fa fa-gem" /></td>
 			<td class="ps-4">{data.currency._sum.currency}</td>
 		</tr>
 		<tr>
@@ -39,18 +39,39 @@
 			<td>Friendships</td>
 			<td class="ps-4">{data.friendships}</td>
 		</tr>
+		<tr>
+			<td>Followerships</td>
+			<td class="ps-4">{data.followerships}</td>
+		</tr>
+		<tr>
+			<td>Status posts</td>
+			<td class="ps-4">{data.statusPosts}</td>
+		</tr>
+		<tr>
+			<td>Forum posts</td>
+			<td class="ps-4">{data.forumPosts}</td>
+		</tr>
+		<tr>
+			<td>Forum replies</td>
+			<td class="ps-4">{data.forumReplies}</td>
+		</tr>
 	</table>
-	<br />
-	<br />
-	<h2 class="light-text mb-3">Average number of:</h2>
+	<h2 class="light-text mt-4 mb-3">Average number of:</h2>
 	<table>
 		<tr>
-			<td>Currency per user</td>
-			<td class="ps-4">{data.currency._avg.currency}</td>
+			<td>
+				<i class="fa fa-gem me-2" />
+				per user
+			</td>
+			<td class="ps-4">{data.currency._avg.currency?.toFixed(2)}</td>
 		</tr>
 		<tr>
 			<td>Friends per user</td>
-			<td class="ps-4">{data.friendships / data.users}</td>
+			<td class="ps-4">{(data.friendships / data.users).toFixed(2)}</td>
+		</tr>
+		<tr>
+			<td>Followers per user</td>
+			<td class="ps-4">{(data.followerships / data.users).toFixed(2)}</td>
 		</tr>
 	</table>
 </div>
