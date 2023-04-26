0fa4d295350c8dc8ba8d54c9615dcd47d26f5631
Lewin Kelly <heli@odex.cf>
2023-04-26 15:24:59 +0100

    Port visit corescript to yuescript

--- corescripts/processed/visit.lua
+++ corescripts/processed/visit.lua
@@ -1,112 +1,88 @@
--- Prepended to Edit.lua and Visit.lua and Studio.lua and PlaySolo.lua--
-
+print("[Mercury]: Loaded Visit corescript")
 if true then
-	pcall(function()
-		game:SetPlaceID(_PLACE_ID)
+pcall(function()
+		return game:SetPlaceID(_PLACE_ID)
 	end)
-else
-	if 0 > 0 then
-		pcall(function()
-			game:SetPlaceID(_PLACE_ID)
-		end)
-	end
 end
-
-visit = game:GetService "Visit"
-
-local message = Instance.new "Message"
+local visit = game:GetService("Visit")
+local message = Instance.new("Message")
 message.Parent = workspace
 message.archivable = false
-
-game:GetService("ScriptInformationProvider"):SetAssetUrl "http://banland.xyz/Asset/"
+game:GetService("ScriptInformationProvider"):SetAssetUrl("http://banland.xyz/Asset/")
 game:GetService("ContentProvider"):SetThreadPool(16)
 pcall(function()
-	game:GetService("InsertService")
-		:SetFreeModelUrl "http://www.roblox.com/Game/Tools/InsertAsset.ashx?type=fm&q=%s&pg=%d&rs=%d"
-end) -- Used for free model search (insert tool)
+	return game:GetService("InsertService"):SetFreeModelUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?type=fm&q=%s&pg=%d&rs=%d")
+end)
 pcall(function()
-	game:GetService("InsertService")
-		:SetFreeDecalUrl "http://www.roblox.com/Game/Tools/InsertAsset.ashx?type=fd&q=%s&pg=%d&rs=%d"
-end) -- Used for free decal search (insert tool)
-
+	return game:GetService("InsertService"):SetFreeDecalUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?type=fd&q=%s&pg=%d&rs=%d")
+end)
 settings().Diagnostics:LegacyScriptMode()
-
-game:GetService("InsertService"):SetBaseSetsUrl "http://www.roblox.com/Game/Tools/InsertAsset.ashx?nsets=10&type=base"
-game:GetService("InsertService")
-	:SetUserSetsUrl "http://www.roblox.com/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=%d"
-game:GetService("InsertService"):SetCollectionUrl "http://www.roblox.com/Game/Tools/InsertAsset.ashx?sid=%d"
-game:GetService("InsertService"):SetAssetUrl "http://banland.xyz/Asset/?id=%d"
-game:GetService("InsertService"):SetAssetVersionUrl "http://www.roblox.com/Asset/?assetversionid=%d"
-
+do
+	local _with_0 = game:GetService("InsertService")
+	_with_0:SetBaseSetsUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?nsets=10&type=base")
+	_with_0:SetUserSetsUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=%d")
+	_with_0:SetCollectionUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?sid=%d")
+	_with_0:SetAssetUrl("http://banland.xyz/Asset/?id=%d")
+	_with_0:SetAssetVersionUrl("http://www.roblox.com/Asset/?assetversionid=%d")
+end
+do
+	local _with_0 = game:GetService("SocialService")
 pcall(function()
-	game:GetService("SocialService")
-		:SetFriendUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsFriendsWith&playerid=%d&userid=%d"
+		return _with_0:SetFriendUrl("http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsFriendsWith&playerid=%d&userid=%d")
+	end)
+pcall(function()
+		return _with_0:SetBestFriendUrl("http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsBestFriendsWith&playerid=%d&userid=%d")
+	end)
+pcall(function()
+		return _with_0:SetGroupUrl("http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsInGroup&playerid=%d&groupid=%d")
+	end)
+pcall(function()
+		return _with_0:SetGroupRankUrl("http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=%d&groupid=%d")
+	end)
+pcall(function()
+		return _with_0:SetGroupRoleUrl("http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&playerid=%d&groupid=%d")
+	end)
+end
+pcall(function()
+	return game:GetService("GamePassService"):SetPlayerHasPassUrl("http://banland.xyz/Game/GamePass/GamePassHandler.ashx?Action=HasPass&UserID=%d&PassID=%d")
 end)
 pcall(function()
-	game:GetService("SocialService")
-		:SetBestFriendUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsBestFriendsWith&playerid=%d&userid=%d"
+	return game:SetCreatorID(0, Enum.CreatorType.User)
 end)
 pcall(function()
-	game:GetService("SocialService")
-		:SetGroupUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsInGroup&playerid=%d&groupid=%d"
+	return game:SetScreenshotInfo("")
 end)
 pcall(function()
-	game:GetService("SocialService")
-		:SetGroupRankUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=%d&groupid=%d"
+	return game:SetVideoInfo("")
 end)
-pcall(function()
-	game:GetService("SocialService")
-		:SetGroupRoleUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&playerid=%d&groupid=%d"
-end)
-pcall(function()
-	game:GetService("GamePassService")
-		:SetPlayerHasPassUrl "http://banland.xyz/Game/GamePass/GamePassHandler.ashx?Action=HasPass&UserID=%d&PassID=%d"
-end)
-pcall(function()
-	game:SetCreatorID(0, Enum.CreatorType.User)
-end)
-
-pcall(function()
-	game:SetScreenshotInfo ""
-end)
-pcall(function()
-	game:SetVideoInfo ""
-end)
-
 pcall(function()
 	settings().Rendering.EnableFRM = true
 end)
 pcall(function()
 	settings()["Task Scheduler"].PriorityMethod = Enum.PriorityMethod.AccumulatedError
 end)
-
 game:GetService("ChangeHistoryService"):SetEnabled(false)
 pcall(function()
-	game:GetService("Players")
-		:SetBuildUserPermissionsUrl "http://banland.xyz//Game/BuildActionPermissionCheck.ashx?assetId=0&userId=%d&isSolo=true"
+	return game:GetService("Players"):SetBuildUserPermissionsUrl("http://banland.xyz//Game/BuildActionPermissionCheck.ashx?assetId=0&userId=%d&isSolo=true")
 end)
-
 workspace:SetPhysicsThrottleEnabled(true)
-
 local addedBuildTools = false
-local screenGui = game:GetService("CoreGui"):FindFirstChild "RobloxGui"
-
-function doVisit()
+local screenGui = game:GetService("CoreGui"):FindFirstChild("RobloxGui")
+local doVisit
+doVisit = function()
 	message.Text = "Loading Game"
 	if false then
-		game:Load ""
-		pcall(function()
-			visit:SetUploadUrl ""
+		game:Load("")
+pcall(function()
+			return visit:SetUploadUrl("")
 		end)
 	else
-		pcall(function()
-			visit:SetUploadUrl ""
+pcall(function()
+			return visit:SetUploadUrl("")
 		end)
 	end
-
 	message.Text = "Running"
 	game:GetService("RunService"):Run()
-
 	message.Text = "Creating Player"
 	if false then
 		player = game:GetService("Players"):CreateLocalPlayer(1)
@@ -115,60 +91,55 @@ function doVisit()
 		player = game:GetService("Players"):CreateLocalPlayer(0)
 	end
 	player.CharacterAppearance = ""
-	local propExists, canAutoLoadChar = false
+	local propExists = false
+	local canAutoLoadChar = false
 	propExists = pcall(function()
 		canAutoLoadChar = game.Players.CharacterAutoLoads
 	end)
-
 	if (propExists and canAutoLoadChar) or not propExists then
 		player:LoadCharacter()
 	end
-
 	message.Text = "Setting GUI"
 	player:SetSuperSafeChat(true)
-	pcall(function()
-		player:SetMembershipType(Enum.MembershipType.None)
+pcall(function()
+		return player:SetMembershipType(Enum.MembershipType.None)
 	end)
-	pcall(function()
-		player:SetAccountAge(0)
+pcall(function()
+		return player:SetAccountAge(0)
 	end)
-
 	if false then
 		message.Text = "Setting Ping"
 		visit:SetPing("http://banland.xyz/Game/ClientPresence.ashx?version=old&PlaceID=_PLACE_ID", 300)
-
 		message.Text = "Sending Stats"
-		game:HttpGet ""
+		return game:HttpGet("")
 	end
 end
-
-success, err = pcall(doVisit)
-
+local success, err = pcall(doVisit)
 if not addedBuildTools then
-	local playerName = Instance.new "StringValue"
-	playerName.Name = "PlayerName"
-	playerName.Value = player.Name
-	playerName.RobloxLocked = true
-	playerName.Parent = screenGui
-
-	pcall(function()
-		game:GetService("ScriptContext"):AddCoreScript(59431535, screenGui, "BuildToolsScript")
+	do
+		local _with_0 = Instance.new("StringValue")
+		_with_0.Name = "PlayerName"
+		_with_0.Value = player.Name
+		_with_0.RobloxLocked = true
+		_with_0.Parent = screenGui
+	end
+pcall(function()
+		return game:GetService("ScriptContext"):AddCoreScript(59431535, screenGui, "BuildToolsScript")
 	end)
 	addedBuildTools = true
 end
-
 if success then
 	message.Parent = nil
 else
 	print(err)
 	if false then
-		pcall(function()
-			visit:SetUploadUrl ""
+pcall(function()
+			return visit:SetUploadUrl("")
 		end)
 	end
 	wait(5)
-	message.Text = "Error on visit: " .. err
+	message.Text = "Error on visit: " .. tostring(err)
 	if false then
-		game:HttpPost("http://banland.xyz/Error/Lua.ashx?", "Visit.lua: " .. err)
+		return game:HttpPost("http://banland.xyz/Error/Lua.ashx?", "Visit.lua: " .. tostring(err))
 	end
 end
