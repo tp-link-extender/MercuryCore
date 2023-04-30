4781e25326e2f45d50429e1f8faad0e81e03347b
Lewin Kelly <heli@odex.cf>
2023-04-30 11:54:00 +0100

    Improve styling of table borders and admin panel

--- src/routes/(main)/admin/+page.svelte
+++ src/routes/(main)/admin/+page.svelte
@@ -77,338 +77,282 @@
 				</li>
 			</ul>
 		</div>
-		<div class="col-lg-10 col-md-9">
-			<div id="myTabContent" class="tab-content">
-				{#if user?.permissionLevel == 5}
-					<div
-						class="tab-pane fade active show"
-						id="administration"
-						role="tabpanel">
-						<div class="row g-3">
-							<div class="col-lg-3">
-								<a href="/admin/banners" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i class="fas fa-bullhorn" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Banners
-											</h5>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div class="col-lg-3">
-								<a href="/admin/accounts" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i class="far fa-user" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Accounts
-											</h5>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div class="col-lg-3">
-								<a href="/admin/audit" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i class="fas fa-book" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Audit Logs
-											</h5>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div class="col-lg-3">
-								<a href="/admin/invites" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i class="fas fa-key" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Invites
-											</h5>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div class="col-lg-3">
-								<a href="#" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i class="fas fa-dice" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Coin Flip
-											</h5>
-										</div>
-									</div>
-								</a>
-							</div>
-						</div>
-					</div>
-				{/if}
+		<div id="myTabContent" class="tab-content col-lg-10 col-md-9">
+			{#if user?.permissionLevel == 5}
 				<div
-					class="tab-pane fade {user?.permissionLevel == 5
-						? ''
-						: 'active show'}"
-					id="moderation"
+					class="tab-pane fade active show"
+					id="administration"
 					role="tabpanel">
-					<div class="row g-3">
-						<div class="col-lg-3">
-							<a href="/admin/moderation" class="shadow-hover">
-								<div class="card bg-a3 text-center light-text">
-									<div class="card-body bg-a rounded-1 p-4">
-										<h1>
-											<i class="fas fa-user-slash" />
-										</h1>
-										<h5 class="fw-normal mt-3">
-											Moderate User
-										</h5>
-									</div>
+					<div class="row">
+						<a href="/admin/banners" class="col-lg-3 p-1" >
+							<div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="fas fa-bullhorn" />
+									</h1>
+									<h5 class="fw-normal mt-3">Banners</h5>
 								</div>
-							</a>
-						</div>
-						<div class="col-lg-3">
-							<a href="#" class="shadow-hover">
-								<div class="card bg-a3 text-center light-text">
-									<div class="card-body bg-a rounded-1 p-4">
-										<h1>
-											<i class="far fa-flag" />
-										</h1>
-										<h5 class="fw-normal mt-3">
-											Report Abuse
-										</h5>
-									</div>
+							</div>
+						</a>
+
+						<a href="/admin/accounts" class="col-lg-3 p-1" >
+							<div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="far fa-user" />
+									</h1>
+									<h5 class="fw-normal mt-3">Accounts</h5>
 								</div>
-							</a>
-						</div>
-						<div class="col-lg-3">
-							<a href="#" class="shadow-hover">
-								<div class="card bg-a3 text-center light-text">
-									<div class="card-body bg-a rounded-1 p-4">
-										<h1>
-											<i
-												class="fas fa-file-circle-check" />
-										</h1>
-										<h5 class="fw-normal mt-3">
-											Asset Approval
-										</h5>
-									</div>
+							</div>
+						</a>
+
+						<a href="/admin/audit" class="col-lg-3 p-1" >
+							<div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="fas fa-book" />
+									</h1>
+									<h5 class="fw-normal mt-3">Audit Logs</h5>
 								</div>
-							</a>
-						</div>
+							</div>
+						</a>
+
+						<a href="/admin/invites" class="col-lg-3 p-1" >
+							<div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="fas fa-key" />
+									</h1>
+									<h5 class="fw-normal mt-3">Invites</h5>
+								</div>
+							</div>
+						</a>
+
+						<!-- <a href="#" class="col-lg-3 p-1" 
+							><div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="fas fa-dice" />
+									</h1>
+									<h5 class="fw-normal mt-3">Coin Flip</h5>
+								</div>
+							</div>
+						</a> -->
 					</div>
 				</div>
-				<div class="tab-pane fade" id="economy" role="tabpanel">
-					<div class="row g-3">
-						{#if user?.permissionLevel == 5}
-							<div class="col-lg-3">
-								<a href="/admin/stipend" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i class="far fa-clock" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Daily Stipend
-											</h5>
-										</div>
-									</div>
-								</a>
+			{/if}
+			<div
+				class="tab-pane fade {user?.permissionLevel == 5
+					? ''
+					: 'active show'}"
+				id="moderation"
+				role="tabpanel">
+				<div class="row g-3">
+					<a href="/admin/moderation" class="col-lg-3 p-1" >
+						<div class="px-0 card bg-a3 text-center light-text">
+							<div class="card-body bg-a rounded-1 p-4">
+								<h1>
+									<i class="fas fa-user-slash" />
+								</h1>
+								<h5 class="fw-normal mt-3">Moderate User</h5>
 							</div>
-						{/if}
-						<div class="col-lg-3">
-							<a href="#" class="shadow-hover">
-								<div class="card bg-a3 text-center light-text">
-									<div class="card-body bg-a rounded-1 p-4">
-										<h1><i class="far fa-gem" /></h1>
-										<h5 class="fw-normal mt-3">
-											Award Currency
-										</h5>
-									</div>
-								</div>
-							</a>
 						</div>
-						{#if user?.permissionLevel == 5 || user?.permissionLevel == 3}
-							<div class="col-lg-3">
-								<a href="#" class="shadow-hover">
-									<div
-										class="card bg-a3 text-center light-text">
-										<div
-											class="card-body bg-a rounded-1 p-4">
-											<h1>
-												<i
-													class="fas fa-file-circle-plus" />
-											</h1>
-											<h5 class="fw-normal mt-3">
-												Create New Asset
-											</h5>
-										</div>
-									</div>
-								</a>
+					</a>
+
+					<!-- <a href="#" class="col-lg-3 p-1"
+						><div class="px-0 card bg-a3 text-center light-text">
+							<div class="card-body bg-a rounded-1 p-4">
+								<h1>
+									<i class="far fa-flag" />
+								</h1>
+								<h5 class="fw-normal mt-3">Report Abuse</h5>
 							</div>
-						{/if}
-						<div class="col-lg-3">
-							<a href="/admin/transactions" class="shadow-hover">
-								<div class="card bg-a3 text-center light-text">
-									<div class="card-body bg-a rounded-1 p-4">
-										<h1>
-											<i
-												class="fas fa-money-bill-transfer" />
-										</h1>
-										<h5 class="fw-normal mt-3">
-											Transactions
-										</h5>
-									</div>
-								</div>
-							</a>
 						</div>
-					</div>
+						 </a> -->
+
+					<!-- <a href="#" class="col-lg-3 p-1"
+						><div class="px-0 card bg-a3 text-center light-text">
+							<div class="card-body bg-a rounded-1 p-4">
+								<h1>
+									<i class="fas fa-file-circle-check" />
+								</h1>
+								<h5 class="fw-normal mt-3">Asset Approval</h5>
+							</div>
+						</div>
+						</a> -->
 				</div>
-				<div class="tab-pane fade" id="statistics" role="tabpanel">
-					<div class="row g-3">
-						<div class="col-lg-7 col-md-7">
-							<div class="card bg-a3 text-black mb-3">
-								<div class="card-body bg-a rounded-1">
+			</div>
+			<div class="tab-pane fade" id="economy" role="tabpanel">
+				<div class="row g-3">
+					{#if user?.permissionLevel == 5}
+						<a href="/admin/stipend" class="col-lg-3 p-1" >
+							<div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="far fa-clock" />
+									</h1>
+									<h5 class="fw-normal mt-3">
+										Daily Stipend
+									</h5>
+								</div>
+							</div>
+						</a>
+					{/if}
+
+					<!-- <a href="#" class="col-lg-3 p-1"
+						><div class="px-0 card bg-a3 text-center light-text">
+							<div class="card-body bg-a rounded-1 p-4">
+								<h1><i class="far fa-gem" /></h1>
+								<h5 class="fw-normal mt-3">Award Currency</h5>
+							</div>
+						</div>
+						</a> -->
+					<!-- {#if user?.permissionLevel == 5 || user?.permissionLevel == 3}
+						<a href="#" class="col-lg-3 p-1"
+							><div class="px-0 card bg-a3 text-center light-text">
+								<div class="card-body bg-a rounded-1 p-4">
+									<h1>
+										<i class="fas fa-file-circle-plus" />
+									</h1>
+									<h5 class="fw-normal mt-3">
+										Create New Asset
+									</h5>
+								</div>
+							</div>
+							</a>
+					{/if} -->
+
+					<a href="/admin/transactions" class="col-lg-3 p-1" >
+						<div class="px-0 card bg-a3 text-center light-text">
+							<div class="card-body bg-a rounded-1 p-4">
+								<h1>
+									<i class="fas fa-money-bill-transfer" />
+								</h1>
+								<h5 class="fw-normal mt-3">Transactions</h5>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="tab-pane fade" id="statistics" role="tabpanel">
+				<div class="row g-3">
+					<div class="col-lg-7 col-md-7">
+						<div class="card bg-a3 text-black mb-3">
+							<div class="card-body bg-a rounded-1">
+								<h3 class="light-text">
+									<i class="fas fa-memory" />
+									{(
+										(data.totalmem - data.freemem) /
+										1024 ** 3
+									).toFixed(2)} / {(
+										data.totalmem /
+										1024 ** 3
+									).toFixed(2)} GB
+								</h3>
+								<span class="light-text">
+									{Math.round(
+										(data.totalmem - data.freemem) /
+											1024 ** 2
+									)} MB is being used
+								</span>
+								<div class="progress bg-a mt-2">
+									<div
+										class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+										role="progressbar"
+										aria-valuenow={data.totalmem -
+											data.freemem}
+										aria-valuemin={0}
+										aria-valuemax={data.totalmem}
+										style="width: {((data.totalmem -
+											data.freemem) /
+											data.totalmem) *
+											100}%;" />
+								</div>
+							</div>
+						</div>
+						<div class="card bg-a3 text-black mb-3">
+							<div class="card-body bg-a rounded-1">
+								{#await data.stream.disk}
+									<h3 class="light-text">Loading...</h3>
+								{:then disk}
 									<h3 class="light-text">
-										<i class="fas fa-memory" />
+										<i class="fas fa-hard-drive me-2" />
 										{(
-											(data.totalmem - data.freemem) /
+											(disk.size - disk.free) /
 											1024 ** 3
 										).toFixed(2)} / {(
-											data.totalmem /
+											disk.size /
 											1024 ** 3
 										).toFixed(2)} GB
 									</h3>
 									<span class="light-text">
 										{Math.round(
-											(data.totalmem - data.freemem) /
-												1024 ** 2
+											(disk.size - disk.free) / 1024 ** 2
 										)} MB is being used
 									</span>
 									<div class="progress bg-a mt-2">
 										<div
-											class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+											class="progress-bar progress-bar-striped progress-bar-animated"
 											role="progressbar"
-											aria-valuenow={data.totalmem -
-												data.freemem}
+											aria-valuenow={disk.size -
+												disk.free}
 											aria-valuemin={0}
-											aria-valuemax={data.totalmem}
-											style="width: {((data.totalmem -
-												data.freemem) /
-												data.totalmem) *
+											aria-valuemax={disk.size}
+											style="width: {((disk.size -
+												disk.free) /
+												disk.size) *
 												100}%;" />
 									</div>
-								</div>
-							</div>
-							<div class="card bg-a3 text-black mb-3">
-								<div class="card-body bg-a rounded-1">
-									{#await data.stream.disk}
-										<h3 class="light-text">Loading...</h3>
-									{:then disk}
-										<h3 class="light-text">
-											<i class="fas fa-hard-drive me-2" />
-											{(
-												(disk.size - disk.free) /
-												1024 ** 3
-											).toFixed(2)} / {(
-												disk.size /
-												1024 ** 3
-											).toFixed(2)} GB
-										</h3>
-										<span class="light-text">
-											{Math.round(
-												(disk.size - disk.free) /
-													1024 ** 2
-											)} MB is being used
-										</span>
-										<div class="progress bg-a mt-2">
-											<div
-												class="progress-bar progress-bar-striped progress-bar-animated"
-												role="progressbar"
-												aria-valuenow={disk.size -
-													disk.free}
-												aria-valuemin={0}
-												aria-valuemax={disk.size}
-												style="width: {((disk.size -
-													disk.free) /
-													disk.size) *
-													100}%;" />
-										</div>
-									{/await}
-								</div>
+								{/await}
 							</div>
 						</div>
-						<div class="col-lg-5 col-md-5">
-							<div class="card bg-a3 text-black mb-3">
-								<div class="card-body bg-a rounded-1">
-									<h3 class="light-text">
-										<i class="far fa-user me-2" />
-										Users
-									</h3>
-									<span class="light-text">
-										<b class="text-primary">0 users</b>
-										are currently online
-									</span>
-								</div>
+					</div>
+					<div class="col-lg-5 col-md-5">
+						<div class="card bg-a3 text-black mb-3">
+							<div class="card-body bg-a rounded-1">
+								<h3 class="light-text">
+									<i class="far fa-user me-2" />
+									Users
+								</h3>
+								<span class="light-text">
+									<b class="text-primary">0 users</b>
+									are currently online
+								</span>
 							</div>
-							<div class="card bg-a3 text-black mb-3">
-								<div class="card-body bg-a rounded-1">
-									<h3 class="light-text">
-										<i class="far fa-file me-2" />
-										Assets
-									</h3>
-									<span class="light-text">
-										<i
-											class="fas text-warning fa-file-circle-minus me-2" />
-										<b class="light-text">0 assets</b>
-										are currently pending
-									</span>
-									<br />
-									<span class="light-text">
-										<i
-											class="fas text-success fa-file-circle-check me-2" />
-										<b class="light-text">0 assets</b>
-										have been approved
-									</span>
-									<br />
-									<span class="light-text">
-										<i
-											class="fas text-danger fa-file-circle-xmark me-2" />
-										<b class="light-text">0 assets</b>
-										have been disapproved
-									</span>
-									<br />
-									<span class="light-text">
-										<i
-											class="fas text-info fa-folder-closed me-2" />
-										<b class="light-text">0 assets</b>
-										in total
-									</span>
-								</div>
+						</div>
+						<div class="card bg-a3 text-black mb-3">
+							<div class="card-body bg-a rounded-1">
+								<h3 class="light-text">
+									<i class="far fa-file me-2" />
+									Assets
+								</h3>
+								<span class="light-text">
+									<i
+										class="fas text-warning fa-file-circle-minus me-2" />
+									<b class="light-text">0 assets</b>
+									are currently pending
+								</span>
+								<br />
+								<span class="light-text">
+									<i
+										class="fas text-success fa-file-circle-check me-2" />
+									<b class="light-text">0 assets</b>
+									have been approved
+								</span>
+								<br />
+								<span class="light-text">
+									<i
+										class="fas text-danger fa-file-circle-xmark me-2" />
+									<b class="light-text">0 assets</b>
+									have been disapproved
+								</span>
+								<br />
+								<span class="light-text">
+									<i
+										class="fas text-info fa-folder-closed me-2" />
+									<b class="light-text">0 assets</b>
+									in total
+								</span>
 							</div>
 						</div>
 					</div>
@@ -422,38 +366,28 @@
 	h2
 		border-color: var(--accent3) !important
 
+	a .card-body
+		transition: background-color 0.2s
+		&:hover
+			background-color: var(--background) !important
+
 	.card
 		border-width: 2px
 		border-color: var(--accent3)
 
-	.shadow-hover,
-	.shadow-hover:link 
-		color: #636c72
-		text-decoration: none
-		font-size: 14px
-
-	.shadow-hover > .card
-		border-radius: 0.2rem
-		box-shadow: none
-		transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)
-
-	.shadow-hover:not(.no-hover-shadow-hover) > .card:active,
-	.shadow-hover:not(.no-hover-shadow-hover) > .card:hover 
-		box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1), 0 1px 4px 2px rgba(0, 0, 0, 0.1)
-
 	.nav-tabs .nav-item.show .nav-link,
 	.nav-tabs .nav-link.active 
-		color: rgb(255, 255, 255)
+		color: white
 		background-color: rgb(13, 109, 252)
 		border-color: var(--bs-nav-tabs-link-active-border-color)
 		border-radius: 0.375rem
-        
+
 	.nav-tabs .nav-link 
 		margin-bottom: calc(0 * var(--bs-nav-tabs-border-width))
 		background: 0 0
 		border: var(--bs-nav-tabs-border-width) solid transparent
 		border-radius: 0.375rem
-        
+
 	.nav-link
 		border-radius: 0
 		color: var(--light-text)
