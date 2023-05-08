d41efd3457a45d2400423cfa565be928dde120b9
Lewin Kelly <heli@odex.cf>
2023-05-08 12:42:33 +0100

    Improve avatar image server and renderer

--- avatar/main.ts
+++ avatar/main.ts
@@ -1,70 +1,70 @@
 import * as THREE from "three"
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js"
-import { DecalGeometry } from "three/addons/geometries/DecalGeometry.js"
+import { DecalGeometry } from "three/addons/geometries/DecalGeometry"
 
 const brickToHex = {
-	1: "F2F3F3",
-	5: "D7C59A",
-	9: "E8BAC8",
-	11: "80BBDC",
-	18: "CC8E69",
-	21: "C4281C",
-	23: "0D69AC",
-	24: "F5CD30",
-	26: "1B2A35",
-	28: "287F47",
-	29: "A1C48C",
-	37: "4B974B",
-	38: "A05F35",
-	101: "DA867A",
-	102: "6E99CA",
-	104: "6B327C",
-	105: "E29B40",
-	106: "DA8541",
-	107: "008F9C",
-	119: "A4BD47",
-	125: "EAB892",
-	135: "74869D",
-	141: "27462D",
-	151: "789082",
-	153: "957977",
-	192: "694028",
-	194: "A3A2A5",
-	199: "635F62",
-	208: "E5E4DF",
-	217: "7C5C46",
-	226: "FDEA8D",
-	1001: "F8F8F8",
-	1002: "CDCDCD",
-	1003: "111111",
-	1004: "FF0000",
-	1006: "B480FF",
-	1007: "A34B4B",
-	1008: "C1BE42",
-	1009: "FFFF00",
-	1010: "0000FF",
-	1011: "002060",
-	1012: "2154B9",
-	1013: "04AFEC",
-	1014: "AA5500",
-	1015: "AA00AA",
-	1016: "FF66CC",
-	1017: "FFAF00",
-	1018: "12EED4",
-	1019: "00FFFF",
-	1020: "00FF00",
-	1021: "3A7D15",
-	1022: "7F8E64",
-	1023: "8C5B9F",
-	1024: "AFDDFF",
-	1025: "FFC9C9",
-	1026: "B1A7FF",
-	1027: "9FF3E9",
-	1028: "CCFFCC",
-	1029: "FFFFCC",
-	1030: "FFCC99",
-	1031: "6225D1",
-	1032: "FF00BF",
+	1: 0xf2f3f3,
+	5: 0xd7c59a,
+	9: 0xe8bac8,
+	11: 0x80bbdc,
+	18: 0xcc8e69,
+	21: 0xc4281c,
+	23: 0x0d69ac,
+	24: 0xf5cd30,
+	26: 0x1b2a35,
+	28: 0x287f47,
+	29: 0xa1c48c,
+	37: 0x4b974b,
+	38: 0xa05f35,
+	101: 0xda867a,
+	102: 0x6e99ca,
+	104: 0x6b327c,
+	105: 0xe29b40,
+	106: 0xda8541,
+	107: 0x008f9c,
+	119: 0xa4bd47,
+	125: 0xeab892,
+	135: 0x74869d,
+	141: 0x27462d,
+	151: 0x789082,
+	153: 0x957977,
+	192: 0x694028,
+	194: 0xa3a2a5,
+	199: 0x635f62,
+	208: 0xe5e4df,
+	217: 0x7c5c46,
+	226: 0xfdea8d,
+	1001: 0xf8f8f8,
+	1002: 0xcdcdcd,
+	1003: 0x111111,
+	1004: 0xff0000,
+	1006: 0xb480ff,
+	1007: 0xa34b4b,
+	1008: 0xc1be42,
+	1009: 0xffff00,
+	1010: 0x0000ff,
+	1011: 0x002060,
+	1012: 0x2154b9,
+	1013: 0x04afec,
+	1014: 0xaa5500,
+	1015: 0xaa00aa,
+	1016: 0xff66cc,
+	1017: 0xffaf00,
+	1018: 0x12eed4,
+	1019: 0x00ffff,
+	1020: 0x00ff00,
+	1021: 0x3a7d15,
+	1022: 0x7f8e64,
+	1023: 0x8c5b9f,
+	1024: 0xafddff,
+	1025: 0xffc9c9,
+	1026: 0xb1a7ff,
+	1027: 0x9ff3e9,
+	1028: 0xccffcc,
+	1029: 0xffffcc,
+	1030: 0xffcc99,
+	1031: 0x6225d1,
+	1032: 0xff00bf,
 }
 
 let bodyColours = {
@@ -93,14 +93,14 @@ scene.add(new THREE.HemisphereLight(0xffffff, 0))
 objLoader.load("./head.obj", (root: any) => {
 	root = root.children[0]
 	root.position.set(0, 1.5, 0)
-	root.material.color.set(parseInt("0x" + brickToHex[bodyColours.Head]))
+	root.material.color.set(brickToHex[bodyColours.Head])
 
 	const decalMesh = new THREE.Mesh(
 		new DecalGeometry(
 			root,
-			new THREE.Vector3(0, 0.06, 1.5),
+			new THREE.Vector3(0, 0.06, 1),
 			new THREE.Euler(0, 0, 0),
-			new THREE.Vector3(1.25, 1.25, 2)
+			new THREE.Vector3(1.25, 1.25, 1)
 		),
 		new THREE.MeshBasicMaterial({
 			map: new THREE.TextureLoader().load("./smile.webp"),
@@ -118,47 +118,42 @@ objLoader.load("./head.obj", (root: any) => {
 objLoader.load("./limb.obj", (root: any) => {
 	root = root.children[0]
 	root.position.set(1.5, 0, 0)
-	root.material.color.set(parseInt("0x" + brickToHex[bodyColours.LeftArm]))
+	root.material.color.set(brickToHex[bodyColours.LeftArm])
 	scene.add(root)
 })
 objLoader.load("./limb.obj", (root: any) => {
 	root = root.children[0]
 	root.position.set(-1.5, 0, 0)
-	root.material.color.set(parseInt("0x" + brickToHex[bodyColours.RightArm]))
+	root.material.color.set(brickToHex[bodyColours.RightArm])
 	scene.add(root)
 })
 // objLoader.load("./limb.obj", (root: any) => {
 // 	root = root.children[0]
 // 	root.position.set(0.5, -2, 0)
-// 	root.material.color.set(parseInt("0x" + brickToHex[bodyColours.LeftLeg]))
+// 	root.material.color.set(brickToHex[bodyColours.LeftLeg])
 // 	scene.add(root)
 // })
 // objLoader.load("./limb.obj", (root: any) => {
-// 	root = root.children[0]
+//	root = root.children[0]
 // 	root.position.set(-0.5, -2, 0)
-// 	root.material.color.set(parseInt("0x" + brickToHex[bodyColours.RightLeg]))
+// 	root.material.color.set(brickToHex[bodyColours.RightLeg])
 // 	scene.add(root)
 // })
 
 objLoader.load("./torso.obj", (root: any) => {
 	root = root.children[0]
-	root.position.set(0.07, 0, 0)
-	root.material.color.set(parseInt("0x" + brickToHex[bodyColours.Torso]))
+	root.material.color.set(brickToHex[bodyColours.Torso])
 	scene.add(root)
 })
 
-document.body.appendChild(renderer.domElement)
+camera.position.set(0, 1.5, 4)
+camera.lookAt(0, 1.5, 0)
+scene.add(camera)
 
-const boom = new THREE.Group()
-boom.add(camera)
-scene.add(boom)
-camera.position.set(0, 1.5, 4) // this sets the boom's length
-camera.lookAt(0, 1.5, 0) // camera looks at the boom's zero
+document.body.appendChild(renderer.domElement)
 
 function animate() {
 	requestAnimationFrame(animate)
-	// boom.rotation.y += 0.02
-	// boom.rotation.x += 0.02
 	renderer.render(scene, camera)
 }
 animate()
