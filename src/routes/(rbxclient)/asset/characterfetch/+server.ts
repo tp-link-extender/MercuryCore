5bbc102de146ec96a8000a1d7ae9587232d0ae3a
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2023-05-18 20:28:34 +0100

    Update +server.ts

--- src/routes/(rbxclient)/asset/characterfetch/+server.ts
+++ src/routes/(rbxclient)/asset/characterfetch/+server.ts
@@ -12,5 +12,5 @@ export function GET({ url, setHeaders }) {
 		"Cache-Control": "no-cache",
 	})
 
-	return new Response(charApp)
+	return new Response(charApp + ";http://banland.xyz/asset/?id=20573078")
 }
