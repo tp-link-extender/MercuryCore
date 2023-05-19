3a8f3a573682057d7e0a6033f5da859db4c3163a
Lewin Kelly <heli@odex.cf>
2023-05-19 18:12:44 +0100

    Replace all tab navs across the site with new tabs component

--- src/routes/(main)/admin/banners/+page.svelte
+++ src/routes/(main)/admin/banners/+page.svelte
@@ -3,6 +3,7 @@
 	import { superForm } from "sveltekit-superforms/client"
 	import { writable } from "svelte/store"
 	import Modal from "$lib/components/Modal.svelte"
+	import { Tab, TabNav, TabData } from "$lib/components/Tabs"
 	import fade from "$lib/fade"
 
 	export let data
@@ -34,6 +35,8 @@
 		$form.bannerBody = body.trim()
 		newBannerBody = body.trim()
 	}
+
+	let tabData = TabData(data.url, ["Create Banner", "Banner List"])
 </script>
 
 <svelte:head>
@@ -47,210 +50,182 @@
 		Back to panel
 	</a>
 	<div class="row mt-4">
-		<div class="col-lg-2 col-md-3 mb-4">
-			<ul class="nav nav-tabs flex-column border-0">
-				<li class="nav-item" role="presentation">
-					<a
-						class="nav-link active"
-						data-bs-toggle="tab"
-						href="#makebanner"
-						aria-selected="true"
-						role="tab">
-						Create Banner
-					</a>
-				</li>
-				<li class="nav-item" role="presentation">
-					<a
-						class="nav-link"
-						data-bs-toggle="tab"
-						href="#viewbanners"
-						aria-selected="false"
-						role="tab"
-						tabindex="-1">
-						Banner List
-					</a>
-				</li>
-			</ul>
+		<div class="col-lg-2 col-md-3 mb-4 pe-0">
+			<TabNav bind:tabData tabs />
 		</div>
 		<div class="col-lg-10 col-md-9">
-			<div id="myTabContent" class="tab-content">
-				<div
-					class="tab-pane fade active show"
-					id="makebanner"
-					role="tabpanel">
-					<form use:enhance method="POST">
-						<fieldset>
-							<div class="row">
-								<label
-									for="bannerText"
-									class="col-md-3 col-form-label text-md-right light-text">
-									Banner text
-								</label>
-								<div class="col-md-8">
-									<textarea
-										bind:value={$form.bannerBody}
-										{...$constraints.bannerBody}
-										name="bannerText"
-										id="bannerText"
-										class="form-control {$errors.bannerBody
-											? 'is-in'
-											: ''}valid" />
-									<small class="light-text">
-										3-100 characters
-									</small>
-									<p class="col-12 mb-3 text-danger">
-										{$errors.bannerBody || ""}
-									</p>
-								</div>
+			<Tab {tabData} pos={1}>
+				<form use:enhance method="POST">
+					<fieldset>
+						<div class="row">
+							<label
+								for="bannerText"
+								class="col-md-3 col-form-label text-md-right light-text">
+								Banner text
+							</label>
+							<div class="col-md-8">
+								<textarea
+									bind:value={$form.bannerBody}
+									{...$constraints.bannerBody}
+									name="bannerText"
+									id="bannerText"
+									class="form-control {$errors.bannerBody
+										? 'is-in'
+										: ''}valid" />
+								<small class="light-text">
+									3-100 characters
+								</small>
+								<p class="col-12 mb-3 text-danger">
+									{$errors.bannerBody || ""}
+								</p>
 							</div>
-							<br />
-							<div class="row">
-								<label
-									for="bannerColour"
-									class="col-md-3 col-form-label text-md-right light-text">
-									Banner colour
-								</label>
-								<div class="col-md-2">
-									<input
-										bind:value={$form.bannerColour}
-										{...$constraints.bannerColour}
-										type="color"
-										name="bannerColour"
-										id="bannerColour"
-										class="{$errors.bannerColour
-											? 'is-in'
-											: ''}valid" />
-									<p class="col-12 mb-3 text-danger">
-										{$errors.bannerColour || ""}
-									</p>
-								</div>
+						</div>
+						<br />
+						<div class="row">
+							<label
+								for="bannerColour"
+								class="col-md-3 col-form-label text-md-right light-text">
+								Banner colour
+							</label>
+							<div class="col-md-2">
+								<input
+									bind:value={$form.bannerColour}
+									{...$constraints.bannerColour}
+									type="color"
+									name="bannerColour"
+									id="bannerColour"
+									class="{$errors.bannerColour
+										? 'is-in'
+										: ''}valid" />
+								<p class="col-12 mb-3 text-danger">
+									{$errors.bannerColour || ""}
+								</p>
 							</div>
-							<br />
-							<div class="row">
-								<label
-									for="bannerTextLight"
-									class="col-md-3 col-form-label text-md-right light-text">
-									Light text
-								</label>
-								<div class="col-md-2">
-									<input
-										bind:checked={$form.bannerTextLight}
-										value="true"
-										type="checkbox"
-										name="bannerTextLight"
-										id="bannerTextLight"
-										class="valid form-check-input" />
-								</div>
+						</div>
+						<br />
+						<div class="row">
+							<label
+								for="bannerTextLight"
+								class="col-md-3 col-form-label text-md-right light-text">
+								Light text
+							</label>
+							<div class="col-md-2">
+								<input
+									bind:checked={$form.bannerTextLight}
+									value="true"
+									type="checkbox"
+									name="bannerTextLight"
+									id="bannerTextLight"
+									class="valid form-check-input" />
 							</div>
-							<button
-								name="action"
-								value="create"
-								class="btn btn-success mt-3">
-								{#if $delayed}
-									Working...
-								{:else}
-									Create
-								{/if}
-							</button>
-						</fieldset>
-					</form>
+						</div>
+						<button
+							name="action"
+							value="create"
+							class="btn btn-success mt-3">
+							{#if $delayed}
+								Working...
+							{:else}
+								Create
+							{/if}
+						</button>
+					</fieldset>
+				</form>
 
-					<p
-						class:text-success={$page.status == 200}
-						class:text-danger={$page.status >= 400}>
-						{$message || ""}
-					</p>
-				</div>
-				<div class="tab-pane fade" id="viewbanners" role="tabpanel">
-					<table class="table table-responsive">
-						<thead class="light-text">
+				<p
+					class:text-success={$page.status == 200}
+					class:text-danger={$page.status >= 400}>
+					{$message || ""}
+				</p>
+			</Tab>
+
+			<Tab {tabData} pos={2}>
+				<table class="table table-responsive">
+					<thead class="light-text">
+						<tr>
+							<th scope="col">Options</th>
+							<th scope="col">Active</th>
+							<th scope="col">Body</th>
+							<th scope="col">Color</th>
+							<th scope="col">Text Color</th>
+							<th scope="col">Creator</th>
+						</tr>
+					</thead>
+					<tbody class="light-text">
+						{#each data.banners as banner}
 							<tr>
-								<th scope="col">Options</th>
-								<th scope="col">Active</th>
-								<th scope="col">Body</th>
-								<th scope="col">Color</th>
-								<th scope="col">Text Color</th>
-								<th scope="col">Creator</th>
-							</tr>
-						</thead>
-						<tbody class="light-text">
-							{#each data.banners as banner}
-								<tr>
-									<td>
-										<form use:enhance method="POST">
-											<input
-												type="hidden"
-												name="id"
-												value={banner.id} />
-											<button
-												name="action"
-												value="delete"
-												class="btn btn-sm btn-link text-decoration-none text-danger my-0">
-												<i class="fas fa-trash" />
-												Delete Banner
-											</button>
-										</form>
-										<form use:enhance method="POST">
-											<input
-												type="hidden"
-												name="action"
-												value={banner.active
-													? "hide"
-													: "show"} />
-											<input
-												type="hidden"
-												name="id"
-												value={banner.id} />
-											<button
-												type="submit"
-												class="btn btn-sm btn-link text-decoration-none text-{banner.active
-													? 'warning'
-													: 'success'} my-0">
-												<i
-													class="fas {banner.active
-														? 'fa-eye-slash'
-														: 'fa-eye'}" />
-												{banner.active
-													? "Dea"
-													: "A"}ctivate
-											</button>
-										</form>
-									</td>
-									<td>{banner.active ? "Yes" : "No"}</td>
-									<td>
-										<button
-											type="button"
-											on:click={viewBody(
-												banner.id,
-												banner.body
-											)}
-											class="btn btn-sm btn-success my-0">
-											View Body
-										</button>
-									</td>
-									<td>
+								<td>
+									<form use:enhance method="POST">
 										<input
-											type="color"
-											value={banner.bgColour}
-											disabled
-											class="valid" />
-									</td>
-									<td>
-										{banner.textLight ? "Light" : "Dark"}
-									</td>
-									<td>
-										<a
-											href="/user/{banner.user.number}"
-											class="text-decoration-none">
-											{banner.user.username}
-										</a>
-									</td>
-								</tr>
-							{/each}
-						</tbody>
-					</table>
-				</div>
-			</div>
+											type="hidden"
+											name="id"
+											value={banner.id} />
+										<button
+											name="action"
+											value="delete"
+											class="btn btn-sm btn-link text-decoration-none text-danger my-0">
+											<i class="fas fa-trash" />
+											Delete Banner
+										</button>
+									</form>
+									<form use:enhance method="POST">
+										<input
+											type="hidden"
+											name="action"
+											value={banner.active
+												? "hide"
+												: "show"} />
+										<input
+											type="hidden"
+											name="id"
+											value={banner.id} />
+										<button
+											type="submit"
+											class="btn btn-sm btn-link text-decoration-none text-{banner.active
+												? 'warning'
+												: 'success'} my-0">
+											<i
+												class="fas {banner.active
+													? 'fa-eye-slash'
+													: 'fa-eye'}" />
+											{banner.active ? "Dea" : "A"}ctivate
+										</button>
+									</form>
+								</td>
+								<td>{banner.active ? "Yes" : "No"}</td>
+								<td>
+									<button
+										type="button"
+										on:click={viewBody(
+											banner.id,
+											banner.body
+										)}
+										class="btn btn-sm btn-success my-0">
+										View Body
+									</button>
+								</td>
+								<td>
+									<input
+										type="color"
+										value={banner.bgColour}
+										disabled
+										class="valid" />
+								</td>
+								<td>
+									{banner.textLight ? "Light" : "Dark"}
+								</td>
+								<td>
+									<a
+										href="/user/{banner.user.number}"
+										class="text-decoration-none">
+										{banner.user.username}
+									</a>
+								</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</Tab>
 		</div>
 	</div>
 </div>
@@ -307,25 +282,9 @@
 {/if}
 
 <style lang="sass">
-	.nav-tabs .nav-item.show .nav-link,
-	.nav-tabs .nav-link.active 
-		color: rgb(255, 255, 255)
-		background-color: rgb(13, 109, 252)
-		border-color: var(--bs-nav-tabs-link-active-border-color)
-		border-radius: 0.375rem
-
-	.nav-tabs .nav-link 
-		margin-bottom: calc(0 * var(--bs-nav-tabs-border-width))
-		background: 0 0
-		border: var(--bs-nav-tabs-border-width) solid transparent
-		border-radius: 0.375rem
-
-	.nav-link
-		border-radius: 0
-		color: var(--light-text)
-
 	input[type="color"]
 		height: 2.5rem
+
 	input[type="checkbox"]
 		height: 1.5rem
 		width: 1.5rem
--- src/routes/(main)/admin/transactions/+page.svelte
+++ src/routes/(main)/admin/transactions/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { enhance } from "$app/forms"
 	import fade from "$lib/fade"
+	import { Tab, TabNav, TabData } from "$lib/components/Tabs"
 
 	export let data
+
+	let tabData = TabData(data.url, ["Transactions"])
 </script>
 
 <svelte:head>
@@ -17,149 +19,113 @@
 	</a>
 	<div class="row mt-4">
 		<div class="col-lg-2 col-md-3 mb-4">
-			<ul class="nav nav-tabs flex-column border-0">
-				<li class="nav-item" role="presentation">
-					<a
-						class="nav-link active"
-						data-bs-toggle="tab"
-						href="#transactions"
-						aria-selected="true"
-						role="tab">
-						Transactions
-					</a>
-				</li>
-			</ul>
+			<TabNav bind:tabData tabs />
 		</div>
 		<div class="col-lg-10 col-md-9">
-			<div id="myTabContent" class="tab-content">
-				<div
-					class="tab-pane fade active show"
-					id="transactions"
-					role="tabpanel">
-					<div class="container">
-						<table class="table table-responsive m-auto">
-							{#each data.transactions as transaction, num}
-								<tr
-									in:fade={{
-										num,
-										total: data.transactions.length,
-										max: 12,
-									}}
-									class="light-text">
-									<td class="p-0">
+			<Tab {tabData} pos={1}>
+				<div class="container">
+					<table class="table table-responsive m-auto">
+						{#each data.transactions as transaction, num}
+							<tr
+								in:fade={{
+									num,
+									total: data.transactions.length,
+									max: 12,
+								}}
+								class="light-text">
+								<td class="p-0">
+									<a
+										href="/user/{transaction.sender.number}"
+										class="d-flex text-decoration-none">
+										<div
+											class="me-2 rounded-circle pfp bg-a">
+											<img
+												src="/api/avatar/{transaction
+													.sender?.username}"
+												alt={transaction.sender
+													.username}
+												class="rounded-circle rounded-top-0" />
+										</div>
+										<p
+											class="light-text my-auto fs-6 text-truncate">
+											{transaction.sender.username}
+										</p>
+									</a>
+								</td>
+
+								<td class="p-0 d-flex justify-content-center">
+									<div class="fs-6 currency">
+										<span class="text-success">
+											<i class="fa fa-gem" />
+											{transaction.amountSent}
+										</span>
+										<i class="fa fa-arrow-right ms-1" />
+									</div>
+									<div>
+										{transaction.taxRate}% tax
+										<br />
+										<small>
+											{transaction.time.toLocaleString()}
+										</small>
+									</div>
+									<div class="fs-6 currency">
+										<i class="fa fa-arrow-right me-1" />
+										<span class="text-success">
+											<i class="fa fa-gem" />
+											{Math.round(
+												(1 -
+													transaction.taxRate / 100) *
+													transaction.amountSent
+											)}
+										</span>
+									</div>
+								</td>
+
+								<td class="p-0">
+									<a
+										href="/user/{transaction.receiver
+											.number}"
+										class="d-flex justify-content-end text-decoration-none">
+										<p class="light-text my-auto fs-6">
+											{transaction.receiver.username}
+										</p>
+										<div
+											class="ms-2 rounded-circle pfp bg-a">
+											<img
+												src="/api/avatar/{transaction
+													.receiver?.username}"
+												alt={transaction.receiver
+													.username}
+												class="rounded-circle rounded-top-0" />
+										</div>
+									</a>
+								</td>
+
+								<td class="p-0">
+									{#if transaction.note && transaction.link}
 										<a
-											href="/user/{transaction.sender
-												.number}"
-											class="d-flex text-decoration-none">
-											<div
-												class="me-2 rounded-circle pfp bg-a">
-												<img
-													src="/api/avatar/{transaction
-														.sender?.username}"
-													alt={transaction.sender
-														.username}
-													class="rounded-circle rounded-top-0" />
-											</div>
-											<p
-												class="light-text my-auto fs-6 text-truncate">
-												{transaction.sender.username}
-											</p>
+											href={transaction.link}
+											class="light-text">
+											{transaction.note}
 										</a>
-									</td>
-
-									<td
-										class="p-0 d-flex justify-content-center">
-										<div class="fs-6 currency">
-											<span class="text-success">
-												<i class="fa fa-gem" />
-												{transaction.amountSent}
-											</span>
-											<i class="fa fa-arrow-right ms-1" />
-										</div>
-										<div>
-											{transaction.taxRate}% tax
-											<br />
-											<small>
-												{transaction.time.toLocaleString()}
-											</small>
-										</div>
-										<div class="fs-6 currency">
-											<i class="fa fa-arrow-right me-1" />
-											<span class="text-success">
-												<i class="fa fa-gem" />
-												{Math.round(
-													(1 -
-														transaction.taxRate /
-															100) *
-														transaction.amountSent
-												)}
-											</span>
-										</div>
-									</td>
-
-									<td class="p-0">
-										<a
-											href="/user/{transaction.receiver
-												.number}"
-											class="d-flex justify-content-end text-decoration-none">
-											<p class="light-text my-auto fs-6">
-												{transaction.receiver.username}
-											</p>
-											<div
-												class="ms-2 rounded-circle pfp bg-a">
-												<img
-													src="/api/avatar/{transaction
-														.receiver?.username}"
-													alt={transaction.receiver
-														.username}
-													class="rounded-circle rounded-top-0" />
-											</div>
-										</a>
-									</td>
-
-									<td class="p-0">
-										{#if transaction.note && transaction.link}
-											<a
-												href={transaction.link}
-												class="light-text">
-												{transaction.note}
-											</a>
-										{:else if transaction.note}
-											<p>
-												{transaction.note}
-											</p>
-										{:else}
-											<em>No transaction note</em>
-										{/if}
-									</td>
-								</tr>
-							{/each}
-						</table>
-					</div>
+									{:else if transaction.note}
+										<p>
+											{transaction.note}
+										</p>
+									{:else}
+										<em>No transaction note</em>
+									{/if}
+								</td>
+							</tr>
+						{/each}
+					</table>
 				</div>
-			</div>
+			</Tab>
 		</div>
 	</div>
 </div>
 
 <style lang="sass">
-	.nav-tabs .nav-item.show .nav-link,
-	.nav-tabs .nav-link.active 
-		color: rgb(255, 255, 255)
-		background-color: rgb(13, 109, 252)
-		border-color: var(--bs-nav-tabs-link-active-border-color)
-		border-radius: 0.375rem
-
-	.nav-tabs .nav-link 
-		margin-bottom: calc(0 * var(--bs-nav-tabs-border-width))
-		background: 0 0
-		border: var(--bs-nav-tabs-border-width) solid transparent
-		border-radius: 0.375rem
-
-	.nav-link
-		border-radius: 0
-		color: var(--light-text)
-
 	.currency
 		margin-top: 0.5rem
 	.pfp
