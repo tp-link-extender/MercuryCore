84a51b095e31d70e3dbf03b218ceb3c598f5bb42
Lewin Kelly <heli@odex.cf>
2023-08-03 07:02:51 +0100

    Add hilarious cubes easter egg to landing page

---
+++ src/lib/components/Cubes.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	// The component that controls the 3D object  on the landing page.
+
+	import { T } from "@threlte/core"
+	import { interactivity } from "@threlte/extras"
+
+	export let columns: number[], rows: number[]
+
+	interactivity()
+</script>
+
+<T.OrthographicCamera
+	makeDefault
+	zoom={50}
+	args={[, , , , -50, 50]}
+	position={[1, 1, 1]}
+	on:create={e => e.ref.lookAt(0, 0, 0)} />
+
+<T.AmbientLight color="white" intensity={0.1} />
+<T.DirectionalLight color="white" position={[2, 5, 1]} />
+
+{#each columns as i}
+	{#each rows as j}
+		<Cube {i} {j} on:moved on:clicked />
+	{/each}
+{/each}
--- src/routes/(plain)/+page.svelte
+++ src/routes/(plain)/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Gradient } from "stripe-gradient"
+	import { Canvas } from "@threlte/core"
 
 	// Gradient must run upon page being loaded,
 	// and cannot be rendered on serverside.
@@ -14,6 +15,28 @@
 		infoPadding = 30
 		infoTextMargin = 0
 	})
+
+	const range = (n: number) =>
+			Array.from({ length: n }, (_, index) => -Math.floor(n / 2) + index),
+		columns = range(37),
+		rows = range(19)
+
+	let cubesMoved = 0,
+		cubesClicked = 0,
+		completed = false
+
+	$: cubePercentage = Math.floor(
+		(cubesMoved / (columns.length * rows.length)) * 100
+	)
+	$: cubePercentage2 = Math.min(
+		Math.floor((cubesClicked / (columns.length * rows.length)) * 200),
+		100
+	)
+
+	$: if (cubePercentage >= 100 && cubePercentage2 >= 100) {
+		completed = true
+		window.open("https://youtube.com/watch?v=cvh0nX08nRw", "_blank")
+	}
 </script>
 
 <Head
@@ -29,43 +52,91 @@
 	style="opacity: {canvasOpacity}" />
 
 <div
-	id="info"
-	class="container d-flex justify-content-center align-items-center"
-	style="padding-top: {infoPadding}vh">
-	<div id="moon" class="position-absolute" style="opacity: {canvasOpacity}">
-		<Moon />
-	</div>
-	<div
-		id="infotext"
-		class="d-flex flex-column justify-content-center align-items-center position-relative"
-		style="margin-top: {infoTextMargin}vh">
-		<h1 id="title" class="fw-bolder text-white opacity-75">Mercury 2</h1>
-		<p class="lead text-white text-center">
-			Endless possibilities. New features. Same nostalgia.
-		</p>
-		<div class="d-inline mb-3">
-			<b>
-				<a
-					type="button"
-					href="/register"
-					class="d-inline btn btn-sm btn-success text-decoration-none">
-					Register <i class="fa fa-chevron-right" />
-				</a>
-			</b>
-			<b>
-				<a
-					type="button"
-					href="/login"
-					class="d-inline btn btn-sm btn-primary text-decoration-none">
-					Login <i class="fa fa-chevron-right" />
-				</a>
-			</b>
-		</div>
-		<a href="/about" class="text-decoration-none pt-3 fs-5">
-			About us <i class="fa fa-chevron-right" />
-		</a>
-	</div>
+	id="cubes"
+	class="position-absolute h-100 w-100"
+	style="opacity: {canvasOpacity}">
+	<Canvas>
+		<Cubes
+			{columns}
+			{rows}
+			on:moved={() => cubesMoved++}
+			on:clicked={() => cubesClicked++} />
+	</Canvas>
 </div>
+
+{#if cubePercentage < 20}
+	<div
+		out:fade
+		id="info"
+		class="container d-flex justify-content-center align-items-center"
+		style="padding-top: {infoPadding}vh">
+		<div
+			id="moon"
+			class="position-absolute"
+			style="opacity: {canvasOpacity}">
+			<Moon />
+		</div>
+		<div
+			id="infotext"
+			class="d-flex flex-column justify-content-center align-items-center position-relative"
+			style="margin-top: {infoTextMargin}vh">
+			<h1 class="fw-bolder text-white opacity-75">Mercury 2</h1>
+			<p class="lead text-white text-center">
+				Endless possibilities. New features. Same nostalgia.
+			</p>
+			<div class="d-inline mb-3">
+				<b>
+					<a
+						type="button"
+						href="/register"
+						class="d-inline btn btn-sm btn-success text-decoration-none">
+						Register <i class="fa fa-chevron-right" />
+					</a>
+				</b>
+				<b>
+					<a
+						type="button"
+						href="/login"
+						class="d-inline btn btn-sm btn-primary text-decoration-none">
+						Login <i class="fa fa-chevron-right" />
+					</a>
+				</b>
+			</div>
+			<a href="/about" class="text-decoration-none pt-3 fs-5">
+				About us <i class="fa fa-chevron-right" />
+			</a>
+		</div>
+	</div>
+{:else}
+	<div
+		id="cubesMoved"
+		class="pe-none position-absolute top-50 start-50 text-center w-100">
+		{#if completed}
+			<h1 in:fade class="pe-none text-white opacity-75 fw-bolder">
+				lmao%
+			</h1>
+		{:else}
+			<h1 in:fade class="pe-none text-white opacity-75 fw-bolder">
+				{cubePercentage}%
+			</h1>
+			{#if cubePercentage2 > 0}
+				<h1 in:fade class="pe-none text-danger opacity-75 fw-bolder">
+					{cubePercentage2}%
+				</h1>
+			{/if}
+			{#if cubePercentage >= 60 && cubePercentage < 80}
+				<h2 class="pe-none text-white opacity-75 fw-bolder">
+					zoom out
+				</h2>
+			{:else if cubePercentage == 100 && cubePercentage2 < 2}
+				<h2 class="pe-none text-danger opacity-75 fw-bolder">
+					click the cubes
+				</h2>
+			{/if}
+		{/if}
+	</div>
+{/if}
+
 <div id="wavep" class="w-100 h-100 position-absolute top-0 overflow-hidden">
 	<div class="w-100 position-absolute bottom-0">
 		<div class="position-absolute" />
@@ -89,7 +160,11 @@
 
 	canvas
 	#moon
+	#cubes
 		transition opacity 0.75s ease-in
+
+	#cubesMoved
+		transform translate(-50%, -50%)
 
 	#gradientbg
 		background linear-gradient(
@@ -123,18 +198,18 @@
 
 	#info
 		background: none
-		z-index 500
 		height 75vh
 		transition padding-top 1s ease-in-out
+		z-index 500
 
-		#infotext
-			transition margin-top 1s ease-in-out
+	#infotext
+		transition margin-top 1s ease-in-out
 		
-		a
-			margin 0
+	a
+		margin 0
 
-		#title
-			font-size 4rem
+	h1
+		font-size 4rem
 
 	#moon
 		margin-bottom min(22rem, 60vh)
