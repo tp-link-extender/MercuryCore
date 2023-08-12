94cdb14f51f382c48823d2692a800b5f1a9f1bbe
Lewin Kelly <heli@odex.cf>
2023-08-12 10:28:13 +0100

    fix Moon Threlte component to prevent disappearance

--- src/lib/components/Moon.svelte
+++ src/lib/components/Moon.svelte
@@ -2,7 +2,7 @@
 	// The component that controls the 3D object
 	// on the landing and maintenance pages.
 
-	import { Canvas, T } from "@threlte/core"
+	import { T } from "@threlte/core"
 	import { GLTF, OrbitControls } from "@threlte/extras"
 
 	const items = [
@@ -31,19 +31,17 @@
 	export let item = items[Math.floor(Math.random() * items.length)]
 </script>
 
-<Canvas>
-	<T.PerspectiveCamera makeDefault position={item.camPos}>
-		<OrbitControls
-			autoRotate
-			autoRotateSpeed={Math.random() > 0.5 ? 10 : -10}
-			rotateSpeed={0.1}
-			enablePan={false}
-			panSpeed={0.1}
-			enableZoom={false} />
-	</T.PerspectiveCamera>
+<T.PerspectiveCamera makeDefault position={item.camPos}>
+	<OrbitControls
+		autoRotate
+		autoRotateSpeed={Math.random() > 0.5 ? 10 : -10}
+		rotateSpeed={0.1}
+		enablePan={false}
+		panSpeed={0.1}
+		enableZoom={false} />
+</T.PerspectiveCamera>
 
-	<T.DirectionalLight color="white" position={[15, 45, 20]} />
-	<T.HemisphereLight skyColor="white" groundColor="#7531ff" intensity={0.4} />
+<T.DirectionalLight color="white" position={[15, 45, 20]} />
+<T.HemisphereLight skyColor="white" groundColor="#7531ff" intensity={0.4} />
 
-	<GLTF url="/landing/{item.name}.glb" />
-</Canvas>
+<GLTF url="/landing/{item.name}.glb" />
