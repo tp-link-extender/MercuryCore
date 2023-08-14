024f0c05f4e0f30d656411567e063a88b970ee08
Lewin Kelly <heli@odex.cf>
2023-08-14 16:09:52 +0100

    Replace Yuescript corescripts with their original versions due to issues with Yuescript compiler

---
+++ corescripts/lua/join.lua
@@ -0,0 +1,226 @@
+print "[Mercury]: Loaded Join corescript" -- MultiplayerSharedScript.lua inserted here ------ Prepended to Join.lua --
+
+pcall(function()
+	return game:SetPlaceID(_PLACE_ID, false)
+end)
+local isTouchDevice = Game:GetService("UserInputService").TouchEnabled
+settings()["Game Options"].CollisionSoundEnabled = true
+pcall(function()
+	settings().Rendering.EnableFRM = true
+end)
+pcall(function()
+	settings().Physics.Is30FpsThrottleEnabled = false
+end)
+pcall(function()
+	settings()["Task Scheduler"].PriorityMethod = Enum.PriorityMethod.AccumulatedError
+end)
+pcall(function()
+	settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.DefaultAuto
+end)
+local threadSleepTime = ...
+if threadSleepTime == nil then
+	threadSleepTime = 15
+end
+local test = _IS_STUDIO_JOIN
+print "! Joining game '_PLACE_ID' place _PLACE_ID at _SERVER_ADDRESS"
+game:GetService("ChangeHistoryService"):SetEnabled(false)
+game:GetService("ContentProvider"):SetThreadPool(16)
+do
+	local _with_0 = game:GetService "InsertService"
+	_with_0:SetBaseSetsUrl "http://banland.xyz/game/tools/insertasset?nsets=10&type=base"
+	_with_0:SetUserSetsUrl "http://banland.xyz/game/tools/insertasset?nsets=20&type=user&userid=%d"
+	_with_0:SetCollectionUrl "http://banland.xyz/game/tools/insertasset?sid=%d"
+	_with_0:SetAssetUrl "http://banland.xyz/Asset/?id=%d"
+	_with_0:SetAssetVersionUrl "http://banland.xyz/asset/?assetversionid=%d"
+end
+do
+	local _with_0 = game:GetService "SocialService"
+	pcall(function()
+		return _with_0:SetFriendUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsFriendsWith&playerid=%d&userid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetBestFriendUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsBestFriendsWith&playerid=%d&userid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetGroupUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsInGroup&playerid=%d&groupid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetGroupRankUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=%d&groupid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetGroupRoleUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&playerid=%d&groupid=%d"
+	end)
+end
+pcall(function()
+	return game:GetService("GamePassService")
+		:SetPlayerHasPassUrl "http://banland.xyz/Game/GamePass/GamePassHandler.ashx?Action=HasPass&UserID=%d&PassID=%d"
+end)
+pcall(function()
+	return game:GetService("MarketplaceService")
+		:SetProductInfoUrl "https://banland.xyz/marketplace/productinfo?assetId=%d"
+end)
+pcall(function()
+	return game:GetService("MarketplaceService")
+		:SetPlayerOwnsAssetUrl "https://banland.xyz/ownership/hasasset?userId=%d&assetId=%d"
+end)
+pcall(function()
+	return game:SetCreatorID(_CREATOR_ID, Enum.CreatorType.User)
+end)
+pcall(function()
+	return game:GetService("Players"):SetChatStyle(Enum.ChatStyle.ClassicAndBubble)
+end)
+local waitingForCharacter = false
+pcall(function()
+	if settings().Network.MtuOverride == 0 then
+		settings().Network.MtuOverride = 1400
+	end
+end)
+client = game:GetService "NetworkClient"
+visit = game:GetService "Visit"
+setMessage = function(message)
+	return game:SetMessage((function()
+		do
+			return message
+		end
+	end)())
+end
+showErrorWindow = function(message, _, _)
+	return game:SetMessage(message)
+end
+reportError = function(err, message)
+	print("***ERROR*** " .. tostring(err))
+	if not test then
+		visit:SetUploadUrl ""
+	end
+	client:disconnect()
+	wait(4)
+	return showErrorWindow("Error: " .. tostring(err), message, "Other")
+end
+onDisconnection = function(_, lostConnection)
+	if lostConnection then
+		return showErrorWindow("You have lost the connection to the game", "LostConnection", "LostConnection")
+	else
+		return showErrorWindow("This game has shut down", "Kick", "Kick")
+	end
+end
+requestCharacter = function(replicator)
+	local connection
+	connection = player.Changed:connect(function(property)
+		if property == "Character" then
+			game:ClearMessage()
+			waitingForCharacter = false
+			return connection:disconnect()
+		end
+	end)
+	setMessage "Requesting character"
+	local success, err = pcall(function()
+		replicator:RequestCharacter()
+		setMessage "Waiting for character"
+		waitingForCharacter = true
+	end)
+	if not success then
+		reportError(err, "W4C")
+		return
+	end
+end
+onConnectionAccepted = function(url, replicator)
+	local connectResolved, waitingForMarker = true, true
+	local success, err = pcall(function()
+		if not test then
+			visit:SetPing("_PING_URL", 30)
+		end
+		do
+			game:SetMessageBrickCount()
+		end
+
+		replicator.Disconnection:connect(onDisconnection)
+		local marker = replicator:SendMarker()
+		return marker.Received:connect(function()
+			waitingForMarker = false
+			return requestCharacter(replicator)
+		end)
+	end)
+	if not success then
+		reportError(err, "ConnectionAccepted")
+		return
+	end
+	while waitingForMarker do
+		workspace:ZoomToExtents()
+		wait(0.5)
+	end
+end
+onConnectionFailed = function(_, err)
+	return showErrorWindow(
+		"Failed to connect to the Game. (ID=" .. tostring(err) .. ")",
+		"ID" .. tostring(err),
+		"Other"
+	)
+end
+onConnectionRejected = function()
+	connectionFailed:disconnect()
+	return showErrorWindow("This game is not available. Please try another", "WrongVersion", "WrongVersion")
+end
+local idled = false
+onPlayerIdled = function(time)
+	if time > 20 * 60 then
+		showErrorWindow(string.format("You were disconnected for being idle %d minutes", time / 60), "Idle", "Idle")
+		client:disconnect()
+		if not idled then
+			idled = true
+		end
+	end
+end
+pcall(function()
+	return settings().Diagnostics:LegacyScriptMode()
+end)
+local success, err = pcall(function()
+	game:SetRemoteBuildMode(true)
+	setMessage "Connecting to Server"
+	client.ConnectionAccepted:connect(onConnectionAccepted)
+	client.ConnectionRejected:connect(onConnectionRejected)
+	connectionFailed = client.ConnectionFailed:connect(onConnectionFailed)
+	client.Ticket = ""
+	playerConnectSucces, player = pcall(function()
+		return client:PlayerConnect(_USER_ID, "_SERVER_ADDRESS", _SERVER_PORT, 0, threadSleepTime)
+	end)
+	if not playerConnectSucces then
+		player = game:GetService("Players"):CreateLocalPlayer(_USER_ID)
+		client:Connect("_SERVER_ADDRESS", _SERVER_PORT, 0, threadSleepTime)
+	end
+	if not test then
+		delay(300, function() end)
+	end
+	do
+		local _with_0 = player
+		_with_0:SetSuperSafeChat(false)
+		pcall(function()
+			return _with_0:SetUnder13(false)
+		end)
+		pcall(function()
+			return _with_0:SetMembershipType(Enum.MembershipType._MEMBERSHIP_TYPE)
+		end)
+		pcall(function()
+			return _with_0:SetAccountAge(1)
+		end)
+	end
+	player.Idled:connect(onPlayerIdled)
+	pcall(function()
+		player.Name = [========[_USER_NAME]========]
+	end)
+	player.CharacterAppearance = "_CHAR_APPEARANCE"
+	if not test then
+		return visit:SetUploadUrl ""
+	end
+end)
+if not success then
+	reportError(err, "CreatePlayer")
+end
+if not test then
+	loadfile ""("", -1, 0)
+end
+pcall(function()
+	return game:SetScreenshotInfo ""
+end)
+return pcall(function()
+	return game:SetVideoInfo '<?xml version="1.0"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:yt="http://gdata.youtube.com/schemas/2007"><media:group><media:title type="plain"><![CDATA[ROBLOX Place]]></media:title><media:description type="plain"><![CDATA[ For more games visit http://banland.xyz]]></media:description><media:category scheme="http://gdata.youtube.com/schemas/2007/categories.cat">Games</media:category><media:keywords>ROBLOX, video, free game, online virtual world</media:keywords></media:group></entry>'
+end)
---
+++ corescripts/lua/studio.lua
@@ -0,0 +1,60 @@
+print "[Mercury]: Loaded Studio corescript"
+do
+	local _with_0 = game:GetService "InsertService"
+	pcall(function()
+		return _with_0:SetFreeModelUrl "http://banland.xyz/game/tools/insertasset?type=fm&q=%s&pg=%d&rs=%d"
+	end)
+	pcall(function()
+		return _with_0:SetFreeDecalUrl "http://banland.xyz/game/tools/insertasset?type=fd&q=%s&pg=%d&rs=%d"
+	end)
+	game:GetService("ScriptInformationProvider"):SetAssetUrl "http://banland.xyz/Asset/"
+	_with_0:SetBaseSetsUrl "http://banland.xyz/game/tools/insertasset?nsets=10&type=base"
+	_with_0:SetUserSetsUrl "http://banland.xyz/game/tools/insertasset?nsets=20&type=user&userid=%d"
+	_with_0:SetCollectionUrl "http://banland.xyz/game/tools/insertasset?sid=%d"
+	_with_0:SetAssetUrl "http://banland.xyz/Asset/?id=%d"
+	_with_0:SetAssetVersionUrl "http://banland.xyz/asset/?assetversionid=%d"
+	_with_0:SetTrustLevel(0)
+end
+do
+	local _with_0 = game:GetService "SocialService"
+	pcall(function()
+		return _with_0:SetFriendUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsFriendsWith&playerid=%d&userid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetBestFriendUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsBestFriendsWith&playerid=%d&userid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetGroupUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=IsInGroup&playerid=%d&groupid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetGroupRankUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=%d&groupid=%d"
+	end)
+	pcall(function()
+		return _with_0:SetGroupRoleUrl "http://banland.xyz/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&playerid=%d&groupid=%d"
+	end)
+end
+pcall(function()
+	return game:GetService("GamePassService")
+		:SetPlayerHasPassUrl "http://banland.xyz/Game/GamePass/GamePassHandler.ashx?Action=HasPass&UserID=%d&PassID=%d"
+end)
+do
+	local _with_0 = game:GetService "MarketplaceService"
+	pcall(function()
+		return _with_0:SetProductInfoUrl "https://banland.xyz/marketplace/productinfo?assetId=%d"
+	end)
+	pcall(function()
+		return _with_0:SetDevProductInfoUrl "https://banland.xyz/marketplace/productDetails?productId=%d"
+	end)
+	pcall(function()
+		return _with_0:SetPlayerOwnsAssetUrl "https://banland.xyz/ownership/hasasset?userId=%d&assetId=%d"
+	end)
+end
+local result, _ = pcall(function()
+	return game:GetService("ScriptContext"):AddStarterScript(37801172)
+end)
+if not result then
+	return pcall(function()
+		return game:GetService("ScriptContext")
+			:AddCoreScript(37801172, game:GetService("ScriptContext", "StarterScript"))
+	end)
+end
