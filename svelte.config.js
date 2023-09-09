24cd71c1cedd7ef134f36264b7aed43987473ed7
Lewin Kelly <heli@odex.cf>
2023-09-09 21:42:57 +0100

    Add Javascript-less purchase modal to marketplace item page

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@bun-community/sveltekit-adapter-bun"
+import adapter from "@sveltejs/adapter-node"
 import preprocess from "svelte-preprocess"
 import autoImport from "sveltekit-autoimport"
 import { resolve } from "path"
