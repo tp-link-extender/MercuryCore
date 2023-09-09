d49b917749fa74730b43119901de2e6094c90a48
Lewin Kelly <heli@odex.cf>
2023-09-09 02:08:57 +0100

    Revert "Add Bun adapter and update dependencies"
    This reverts commit c0aa3406cddacfd7f389dcb81225ce407fad9b25.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "svelte-adapter-bun"
+import adapter from "@bun-community/sveltekit-adapter-bun"
 import preprocess from "svelte-preprocess"
 import autoImport from "sveltekit-autoimport"
 import { resolve } from "path"
