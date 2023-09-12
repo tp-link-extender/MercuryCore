638ebe277e4141d65d190f9bfbbaefbb995d2d69
Lewin Kelly <heli@odex.cf>
2023-09-12 23:40:46 +0100

    Fix appearance of asset comment notification popups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,12 +27,14 @@
 		nprogress.done()
 	}
 
-	const notificationNotes: { [k: string]: string } = {
+	const notificationNotes = {
 		AssetApproved: "Asset approval",
 		FriendRequest: "Friend request",
 		Follower: "New follower",
 		ForumPostReply: "Reply to your post",
 		ForumReplyReply: "Reply to your reply",
+		AssetComment: "Comment on your asset",
+		AssetCommentReply: "Reply to your comment",
 		ForumMention: "Mention",
 		ForumPost: "New forum post",
 		ItemPurchase: "Item purchased",
