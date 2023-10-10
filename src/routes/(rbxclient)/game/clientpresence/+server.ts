9ef8fd5ab0089871328bd21bae52adba7bc78ed3
Lewin Kelly <heli@odex.cf>
2023-10-10 11:53:04 +0100

    Move all places to Surreal

--- src/routes/(rbxclient)/game/clientpresence/+server.ts
+++ src/routes/(rbxclient)/game/clientpresence/+server.ts
@@ -6,7 +6,10 @@ export async function GET({ url, request, setHeaders }) {
 
 	if (!ticket) throw error(400, "Invalid Request")
 	if (request.headers.get("user-agent") != "Roblox/WinInet")
-		throw error(400, "Invalid Request")
+		throw error(400, "Good one")
+
+	if (!(await surreal.select(`playing:${ticket}`))[0])
+		throw error(400, "Ticket not found")
 
 	surreal.merge(`playing:${ticket}`, {
 		ping: Math.floor(Date.now() / 1000),
