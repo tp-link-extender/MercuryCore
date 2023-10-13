b6ddd077e6a2b7b77727bc5e99acf236124124df
Lewin Kelly <heli@odex.cf>
2023-10-13 02:47:56 +0100

    Remove Prisma folder, migrations, schema, and cyphertag for Redisgraph

--- prisma/migrations/20230302195759_remove_displaynames/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `displayname` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "user" DROP COLUMN "displayname";
