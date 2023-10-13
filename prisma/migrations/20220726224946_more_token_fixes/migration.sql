9d159cd0ad867fd219b5244186813c20472050ee
Lewin Kelly <heli@odex.cf>
2023-10-13 02:47:56 +0100

    Remove Prisma folder, migrations, schema, and cyphertag for Redisgraph

--- prisma/migrations/20220726224946_more_token_fixes/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropIndex
-DROP INDEX "Token_userUsername_key";
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "displayname" DROP NOT NULL,
-ALTER COLUMN "password" DROP NOT NULL;
--- prisma/migrations/20230519203130_asset_approvals/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "ApprovalStatus" AS ENUM ('Pending', 'Approved', 'Rejected');
-
--- AlterTable
-ALTER TABLE "Asset" ADD COLUMN     "approved" "ApprovalStatus" NOT NULL DEFAULT 'Pending',
-ADD COLUMN     "moderatorUsername" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Asset" ADD CONSTRAINT "Asset_moderatorUsername_fkey" FOREIGN KEY ("moderatorUsername") REFERENCES "user"("username") ON DELETE SET NULL ON UPDATE CASCADE;
