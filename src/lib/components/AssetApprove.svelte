f733662ce148d3d9ea489eeeb1e320fa62bb1132
Lewin Kelly <heli@odex.cf>
2023-10-15 22:33:28 +0100

    Asset approval modal and other fixes

--- src/lib/components/AssetApprove.svelte
+++ src/lib/components/AssetApprove.svelte
@@ -2,48 +2,96 @@
 	// An avatar shop item component, used in the
 	// avatar shop and inventory pages.
 
-	export let asset: {
-		id: number
-		name: string
-		price: number
-		// ratio?: string | number
-	}
+	export let asset: import("../../routes/(main)/admin/asset/$types").PageData["assets"][0]
 	export let num: number
 	export let total: number
+
+	let modal: HTMLInputElement
 </script>
 
-<a
+<label
 	in:fade|global={{ num, total }}
-	href="/avatarshop/{asset.id}/{asset.name}"
-	class="col-xl-2 col-lg-3 col-md-3 col-sm-4 col-6 px-2 mb-2 text-decoration-none assetcard">
+	for="modal{asset.id}"
+	class="col-xl-2 col-lg-3 col-md-3 col-sm-4 col-6
+		text-decoration-none assetcard px-2">
 	<div class="card bg-a3">
-		<div class="card-body bg-a p-4 rounded-1">
+		<div class="card-body bg-a p-4 rounded-1 light-text">
 			<div class="text-center pb-4">
 				<img
 					src="/avatarshop/{asset.id}/{asset.name}/icon"
 					alt={asset.name} />
 			</div>
-			<p class="m-0">
-				{asset.name}
-			</p>
-            <p class="m-0">
-				by: <a href="#">{asset.name}</a>
-			</p>
+			{asset.name}
+			<span class="d-flex">
+				<strong class="pe-2">by</strong>
+				<User
+					user={asset.creator}
+					size="1.5rem"
+					thin
+					full
+					bg="background" />
+			</span>
 		</div>
 	</div>
-</a>
+</label>
+
+<input
+	type="checkbox"
+	id="modal{asset.id}"
+	class="modal-toggle"
+	bind:this={modal} />
+<div class="modal2 light-text">
+	<div class="modal-box d-flex flex-column p-4">
+		<h2 class="fs-4">Asset {asset.name}</h2>
+		{#if asset.imageAsset}
+			<div class="text-center">
+				<h3 class="fs-5">Image asset</h3>
+				<img
+					class="image"
+					src="/avatarshop/{asset.imageAsset.id}/{asset.imageAsset
+						.name}/icon"
+					alt={asset.imageAsset.name} />
+			</div>
+		{/if}
+	</div>
+	<label class="modal-backdrop" for="modal{asset.id}">Close</label>
+</div>
 
 <style lang="stylus">
 	.card
 		border-width 2px
 
 	.assetcard
+		cursor pointer
 		.card-body
 			transition 0.3s
-		&:hover
-			.card-body
+			&:hover
 				background var(--darker) !important
 
-	img
+	.modal-box
+		width 25rem
+
+	.image
+		background var(--accent)
+		background-image:
+			linear-gradient(45deg,
+				var(--darker) 25%, transparent 25%,
+				transparent 75%, var(--darker) 75%
+			),
+			linear-gradient(45deg,
+				var(--darker) 25%, transparent 25%,
+				transparent 75%, var(--darker) 75%
+			)
+		background-size 20px 20px
+		background-position 0 0, 10px 10px
+
+		height 20rem
+		width 20rem
+
+		+-sm()
+			height 15rem
+			width 15rem
+
+	label img
 		width 85%
 </style>
--- src/routes/(main)/admin/asset/+page.server.ts
+++ src/routes/(main)/admin/asset/+page.server.ts
@@ -8,6 +8,14 @@ export const load = async ({ locals }) => ({
 		price: number
 		id: number
 		type: string
+		creator: {
+			number: number
+			username: string
+		}
+		imageAsset?: {
+			id: number
+			name: string
+		}
 	}>(
 		surql`
 			SELECT
@@ -15,8 +23,12 @@ export const load = async ({ locals }) => ({
 				name,
 				price,
 				type,
-				<-owns<-user AS owners
-			FROM asset WHERE $user ∈ <-owns<-user`,
+				(SELECT number, username
+				FROM <-created<-user)[0] AS creator,
+				(SELECT meta::id(id) AS id, name
+				FROM ->imageAsset->asset)[0] AS imageAsset
+			FROM asset WHERE visibility = "Pending"
+				AND type ∈ [17, 18, 2, 11, 12, 19]`,
 		{ user: `user:${(await authorise(locals)).user.id}` },
 	),
 })
