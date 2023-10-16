35fcb5f4ff71abee79d123fb098397a327e97310
Lewin Kelly <heli@odex.cf>
2023-10-16 15:53:20 +0100

    Ping api/?statusping url while tab is open to keep online status for user up to date

--- src/routes/api/+page.server.ts
+++ src/routes/api/+page.server.ts
@@ -1,6 +1,8 @@
 // Contains various api methods that cannot be accessed in a page context,
 // usually because they are requested from a component.
 
+import surql from "$lib/surrealtag"
+import { query } from "$lib/server/surreal"
 import { auth, authorise } from "$lib/server/lucia"
 import { error, redirect } from "@sveltejs/kit"
 
@@ -18,5 +20,12 @@ export const actions = {
 		await auth.invalidateSession(session.sessionId) // invalidate session
 		locals.auth.setSession(null) // remove cookie
 		throw redirect(302, "/login")
+	},
+	statusping: async ({ locals }) => {
+		const { user } = await authorise(locals)
+
+		await query(surql`UPDATE $user SET lastOnline = time::now()`, {
+			user: `user:${user.id}`,
+		})
 	},
 }
