13edde0b53bc1bd224b707f9979eef1e6d0589fe
Lewin Kelly <heli@odex.cf>
2023-12-28 03:36:14 +0000

    Extract large json responses from api endpoints into .json files

--- src/routes/(rbxclient)/game/tools/insertasset/+server.ts
+++ src/routes/(rbxclient)/game/tools/insertasset/+server.ts
@@ -3,6 +3,6 @@ import { redirect } from "@sveltejs/kit"
 export function GET({ url }) {
 	redirect(
 		302,
-		"https://sets.pizzaboxer.xyz/Game/Tools/InsertAsset.ashx" + url.search
+		`https://sets.pizzaboxer.xyz/Game/Tools/InsertAsset.ashx${url.search}`
 	)
 }
