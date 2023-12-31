0500e861a6322352fbcc3fde31ce4e6f4f601ca6
Lewin Kelly <heli@odex.cf>
2023-12-31 19:31:36 +0000

    Improve styling of some pages

--- src/routes/(main)/groups/[name]/+page.svelte
+++ src/routes/(main)/groups/[name]/+page.svelte
@@ -35,45 +35,6 @@
 			</form>
 		</div>
 	</div>
-	<!-- {#if data.places.length > 0}
-		<div class="pt-6">
-			<h2 class="text-base">Creations</h2>
-			<div class="row m-0 p-0">
-				{#each data.places as place, num}
-					<div class="col col-4 col-sm-3 col-md-2 text-center">
-						<Place {place} {num} total={data.places.length} />
-					</div>
-				{/each}
-			</div>
-		</div>
-	{/if}
-	{#if data.feed.length > 0}
-		<h2 class="text-base pt-12">Latest feed posts</h2>
-		<div id="feed" class="light-text p-4">
-			<div class="row">
-				{#each data.feed.sort((a, b) => new Date(b.posted).getTime() - new Date(a.posted).getTime()) as status}
-					<div class="p-2 col-md-6 col-sm-12">
-						<div class="card h-full">
-							<div class="card-body pb-0">
-								<div id="user" class="flex mb-2">
-									<span class="font-bold ml-4 light-text">
-										{data.name}
-									</span>
-									<span
-										class="ms-auto italic light-text text-end">
-										{status.posted.toLocaleString()}
-									</span>
-								</div>
-								<p class="text-start">
-									{status.content}
-								</p>
-							</div>
-						</div>
-					</div>
-				{/each}
-			</div>
-		</div>
-	{/if} -->
 </div>
 
 <style lang="stylus">
