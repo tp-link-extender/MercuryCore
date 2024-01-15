b5a81ad530d37b4334a67bc91e509d196e21c974
Lewin Kelly <heli@odex.cf>
2024-01-15 20:49:29 +0000

    Use goroutines and task system to drastically improve performance

--- Client deployer/main.go
+++ Client deployer/main.go
@@ -9,6 +9,8 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"sync"
+	"time"
 
 	c "github.com/TwiN/go-color"
 )
@@ -23,9 +25,59 @@ func Log(txt string) {
 	fmt.Print(txt + strings.Repeat(" ", spaces))
 }
 
-func Done() {
-	fmt.Println(c.InBlackOverGreen(" Done! "))
+type Task struct {
+	name      string
+	timeStart int
+	timeEnd   int
 }
+
+type TaskList struct {
+	tasks []Task
+}
+
+func (t *TaskList) Update() {
+	// clear last x lines, where x is the number of tasks
+	toPrint := "\033[" + fmt.Sprint(len(t.tasks)) + "A"
+
+	// print all tasks, with [ Done! ] at the end if they are completed
+
+	for _, task := range t.tasks {
+		spaces := 50 - len(task.name)
+		toPrint += c.InBlue("Creating " + c.InUnderline(task.name) + strings.Repeat(" ", spaces))
+		if task.timeEnd > 0 {
+			toPrint += c.InBlackOverGreen(" Done! (" + fmt.Sprint(task.timeEnd-task.timeStart) + "ms) ")
+		}
+
+		toPrint += "\n"
+	}
+
+	fmt.Print(toPrint)
+}
+
+func (t *TaskList) AddTask(task string) {
+	fmt.Println()
+	t.tasks = append(t.tasks, Task{name: task, timeStart: int(time.Now().UnixNano() / int64(time.Millisecond))})
+	t.Update()
+}
+
+func (t *TaskList) AddTasks(tasks []string) {
+	fmt.Print(strings.Repeat("\n", len(tasks)))
+	for _, task := range tasks {
+		t.tasks = append(t.tasks, Task{name: task, timeStart: int(time.Now().UnixNano() / int64(time.Millisecond))})
+	}
+	t.Update()
+}
+
+func (t *TaskList) CompleteTask(completedTask string) {
+	for i, task := range t.tasks {
+		if task.name == completedTask {
+			t.tasks[i].timeEnd = int(time.Now().UnixNano() / int64(time.Millisecond))
+		}
+	}
+	t.Update()
+}
+
+var list = TaskList{tasks: []Task{}}
 
 func RandomString() string {
 	// generate a hexadecimal string of a given size
@@ -37,49 +89,54 @@ func RandomString() string {
 	return string(buf)
 }
 
-func WriteToZip(writer *zip.Writer, pathName string, entryName string, isFolder bool) {
-	if isFolder {
-		// read directory
-		files, err := os.ReadDir(pathName)
-		if err != nil {
-			Error("Could not read directory " + pathName)
-		}
+func WriteFile(writer *zip.Writer, pathName string, entryName string) {
+	// read file
+	file, err := os.Open(pathName)
+	if err != nil {
+		Error("Could not read file " + pathName)
+	}
+	defer file.Close()
 
-		// write files to zip entry
-		for _, file := range files {
-			WriteToZip(writer, path.Join(pathName, file.Name()), entryName+"/"+file.Name(), file.IsDir())
-		}
-	} else {
-		// read file
-		file, err := os.Open(pathName)
-		if err != nil {
-			Error("Could not read file " + pathName)
-		}
-		defer file.Close()
+	// create zip entry
+	entry, err := writer.Create(entryName)
+	if err != nil {
+		Error("Could not create zip entry " + entryName)
+	}
 
-		// create zip entry
-		entry, err := writer.Create(entryName)
-		if err != nil {
-			Error("Could not create zip entry " + entryName)
-		}
+	// copy file to zip entry
+	_, err = io.Copy(entry, file)
+	if err != nil {
+		fmt.Println(err)
+		Error("Could not copy file " + pathName + " to zip entry " + entryName)
+	}
+}
 
-		// copy file to zip entry
-		_, err = io.Copy(entry, file)
-		if err != nil {
-			Error("Could not copy file " + pathName + " to zip entry " + entryName)
+func WriteFolder(writer *zip.Writer, pathName string, entryName string) {
+	// read directory
+	files, err := os.ReadDir(pathName)
+	if err != nil {
+		Error("Could not read directory " + pathName)
+	}
+
+	// write files to zip entry
+	for _, file := range files {
+		if file.IsDir() {
+			WriteFolder(writer, path.Join(pathName, file.Name()), entryName+"/"+file.Name())
+		} else {
+			WriteFile(writer, path.Join(pathName, file.Name()), entryName+"/"+file.Name())
 		}
 	}
 }
 
-func zipFromArray(sources []string, destination string, baseDirectory string, isFolder bool) {
-	Log(c.InBlue("Creating " + c.InUnderline(destination)))
+func zipFromArray(wg *sync.WaitGroup, destination string, sources []string, baseDirectory string, isFolder bool) {
+	defer wg.Done()
+
 	var entryName string
 
 	zipFile, err := os.Create(destination)
 	if err != nil {
 		Error("Could not create zip file " + destination)
 	}
-	defer zipFile.Close()
 
 	archive := zip.NewWriter(zipFile)
 
@@ -93,13 +150,16 @@ func zipFromArray(sources []string, destination string, baseDirectory string, is
 
 		pathName := path.Join("staging", baseDirectory+file)
 		if entryName == "ui" {
-			WriteToZip(archive, pathName, "ui", true)
+			WriteFolder(archive, pathName, "ui")
+		} else if isFolder {
+			WriteFolder(archive, pathName, baseDirectory+file)
 		} else {
-			WriteToZip(archive, pathName, baseDirectory+file, isFolder)
+			WriteFile(archive, pathName, baseDirectory+file)
 		}
 	}
+
+	list.CompleteTask(destination)
 	archive.Close()
-	Done()
 }
 
 func main() {
@@ -139,14 +199,10 @@ func main() {
 		Error("Staging directory is empty")
 	}
 
-	fmt.Println(c.InPurple("Preparing " + c.InUnderline("Mercury.zip")))
+	// create a waitgroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
 
-	zipFromArray([]string{
-		"MercuryPlayerBeta.exe",
-		"MercuryStudioBeta.exe",
-		"ReflectionMetadata.xml",
-		"RobloxStudioRibbon.xml",
-	}, "PrepForUpload/Mercury.zip", "", false)
+	fmt.Println(c.InPurple("Preparing " + c.InUnderline("Mercury.zip")))
 
 	// get all files in the staging directory that end with .dll
 	dllFiles := []string{}
@@ -155,15 +211,22 @@ func main() {
 			dllFiles = append(dllFiles, file.Name())
 		}
 	}
-	zipFromArray(dllFiles, "PrepForUpload/Libraries.zip", "", false)
 
-	fmt.Println(c.InBlue("Creating " + c.InUnderline("redist.zip")))
+	tasks := []string{
+		"PrepForUpload/redist.zip",
+		"PrepForUpload/Mercury.zip",
+		"PrepForUpload/Libraries.zip",
+	}
 
-	zipFromArray([]string{
-		"Microsoft.VC90.CRT",
-		"Microsoft.VC90.MFC",
-		"Microsoft.VC90.OPENMP",
-	}, "PrepForUpload/redist.zip", "", true)
+	list.AddTasks(tasks)
+	wg.Add(len(tasks))
+
+	go zipFromArray(&wg, tasks[0], []string{"Microsoft.VC90.CRT", "Microsoft.VC90.MFC", "Microsoft.VC90.OPENMP"}, "", true)
+	go zipFromArray(&wg, tasks[1], []string{"MercuryPlayerBeta.exe", "MercuryStudioBeta.exe", "ReflectionMetadata.xml", "RobloxStudioRibbon.xml"}, "", false)
+	go zipFromArray(&wg, tasks[2], dllFiles, "", false)
+
+	// wait for gorooutines to finish
+	wg.Wait()
 
 	fmt.Println(" ~~~~  Deployment complete!!  ~~~~")
 }
