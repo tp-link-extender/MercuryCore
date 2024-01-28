36900b5e670dd6525ddf45fa35530dc09fc001a2
Lewin Kelly <heli@odex.cf>
2024-01-28 17:10:52 +0000

    Compile Red library from corescripts/Libraries, update types to make Red FULLY TYPE SAFE!

--- corescripts/compile.sh
+++ corescripts/compile.sh
@@ -5,6 +5,7 @@ done
 
 echo "Processing libraries..."
 darklua process -c dense.json5 ./corescripts/Libraries/Fusion/init.luau ./corescripts/processed/10000001.lua
+darklua process -c bundle.json5 ./corescripts/Libraries/Red/init.luau ./corescripts/processed/10000002.lua
 
 echo "Processing other corescripts..."
 for file in ./corescripts/luau/[a-z]*.luau; do
