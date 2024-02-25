55cdee2e85cf0d2dbef4acc5b4162da348c8705e
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2024-02-25 15:37:47 +0000

    Starter commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mercury 2 Bot
+
+This bot is used to send applications from users to administrators for invite keys
+[FURTHER INFO W.I.P]
---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import json
+import logging
+
+with open("bot.json", "r") as f:
+    config = json.load(f)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(config["token"])
