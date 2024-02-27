b08b4c2ff4be08d1f6aed802c772e3d19fa29a71
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2024-02-27 21:44:12 +0000

    dm

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import discord
 from discord import app_commands
+from discord.ext import commands
 import os
 from rich import print
+from dotenv import load_dotenv
+
+load_dotenv()
 
 token = os.getenv("token")
 
-client = discord.Client(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
-@client.event
+@bot.event
 async def on_ready():
     print(f"[green]Bot is active[/green]")
     try:
-        cmds = await client.tree.sync()
+        cmds = await bot.tree.sync()
         print(f"[green]Synced {len(cmds)} command(s)")
     except Exception as e:
         print(e)
 
-@client.tree.command(name="register")
+@bot.tree.command(name="register")
 async def register(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hello")
+    user = await interaction.user.create_dm()
+    if isinstance(interaction.channel, discord.DMChannel):
+        await interaction.response.send_message("Beginning register process.")
+    else:
+        await interaction.response.send_message("Please check your DMS!")
+    await user.send("Hi")
 
-client.run(config["token"])
+bot.run(token)
