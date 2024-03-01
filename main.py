2e9b83bfc9be4bcacca5c34d7f2fd0464ef61ebb
taskmanager <51055281+Mantaraix@users.noreply.github.com>
2024-03-01 01:28:25 +0000

    embdd

--- main.py
+++ main.py
@@ -26,7 +26,11 @@ async def register(interaction: discord.Interaction):
     if isinstance(interaction.channel, discord.DMChannel):
         await interaction.response.send_message("Beginning register process.")
     else:
-        await interaction.response.send_message("Please check your DMS!")
+        desc = ":white_check_mark: Registration instructions have been sent to your DMS!"
+        embedV = discord.Embed(color=0x472a96, description=desc)
+        embedV.set_author(name="Mercury 2 - Register", url="https://banland.xyz", icon_url="https://banland.xyz/icon192.png")
+        embedV.add_field(name="I don't see any message!", value="Make sure that you have enabled DMs from this server. If you have and you still have not recieved a message, please contact an adminstrator.")
+        await interaction.response.send_message(embed=embedV)
     await user.send("Hi")
 
 bot.run(token)
