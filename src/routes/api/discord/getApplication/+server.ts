2f2850582e7a9a04b19eca7aa2be315e349d2f07
Lewin Kelly <heli@odex.cf>
2024-03-10 14:55:14 +0000

    Add endpoint to get all pending applications for Discord bot

---
+++ src/routes/api/discord/getApplication/+server.ts
@@ -0,0 +1,29 @@
+import { squery, surql } from "$lib/server/surreal"
+import { json } from "@sveltejs/kit"
+import { verify } from "../discord"
+
+export async function GET({ url }) {
+	verify(url)
+	// Get all pending applications
+
+	const applications = await squery<{
+		response: string[]
+		created: string
+		status: string
+		reviewed: string
+		reason: string | undefined
+	}>(
+		surql`
+			SELECT
+				response,
+				created,
+				status,
+				reviewed,
+				reason
+			FROM application
+			WHERE status = "Pending"
+			ORDER BY created`
+	)
+
+	return json(applications)
+}
