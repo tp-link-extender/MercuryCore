b401aa52e68395072203a0655c1a58acfdbbadaa
Lewin Kelly <heli@odex.cf>
2024-03-31 00:46:17 +0000

    Issues with RCC asset serving turned out to be nonexistent all along

--- src/routes/(rbxclient)/asset/+server.ts
+++ src/routes/(rbxclient)/asset/+server.ts
@@ -4,14 +4,15 @@ import { error, redirect } from "@sveltejs/kit"
 import { createHash } from "node:crypto"
 import fs from "node:fs"
 
-const header = (file: Buffer | string) => ({
-	headers: {
-		"Content-Type": "binary/octet-stream",
-		"Content-Disposition": `attachment; filename="${createHash("md5")
-			.update(file)
-			.digest("hex")}"`,
-	},
-})
+const response = (file: Buffer | string) =>
+	new Response(file, {
+		headers: {
+			"Content-Type": "binary/octet-stream",
+			"Content-Disposition": `attachment; filename="${createHash("md5")
+				.update(file)
+				.digest("hex")}"`,
+		},
+	})
 
 export async function GET({ url }) {
 	const id = url.searchParams.get("id")
@@ -40,14 +41,9 @@ export async function GET({ url }) {
 
 			// The asset is visible or pending
 			// (allow pending assets to be shown through the api)
-			const file = fs.readFileSync(`data/assets/${id}`, "utf-8")
-
 			console.log(`served asset #${id}`)
 
-			return new Response(
-				fs.readFileSync(`data/assets/${id}`),
-				header(file)
-			)
+			return response(fs.readFileSync(`data/assets/${id}`))
 		}
 
 		// Try loading as a corescript
@@ -61,12 +57,12 @@ export async function GET({ url }) {
 
 		let file2 = file.replaceAll("roblox.com/asset", "banland.xyz/asset")
 
-		// Health corescript and shaggy lol
-		if (!["38037265", "20573078"].includes(id)) file2 = SignData(file2, +id)
+		// Health corescript lol
+		if (id !== "38037265") file2 = SignData(file2, +id)
 
 		console.log("served corescript", id)
 
-		new Response(file2, header(file))
+		return response(file2)
 	} catch {
 		redirect(302, `https://assetdelivery.roblox.com/v1/asset?id=${id}`)
 	}
--- src/routes/(rbxclient)/game/visit/+server.ts
+++ src/routes/(rbxclient)/game/visit/+server.ts
@@ -7,6 +7,5 @@ export const GET = () =>
 			fs
 				.readFileSync("corescripts/processed/visit.lua", "utf-8")
 				.replaceAll("_PLACE_ID", "0")
-				.replaceAll("_GUEST_NUMBER", "9042")
 		)
 	)
