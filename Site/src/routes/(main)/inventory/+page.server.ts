49068d115fcea588834a43fbe43f332ac538ddd3
Lewin Kelly <heli@odex.cf>
2024-08-30 14:38:39 +0100

    Move query filtering functions into initial query file

--- Site/src/routes/(main)/inventory/+page.server.ts
+++ Site/src/routes/(main)/inventory/+page.server.ts
@@ -3,20 +3,20 @@ import pageQuery from "$lib/server/pageQuery"
 import { Record, equery } from "$lib/server/surreal.ts"
 import inventoryQuery from "./inventory.surql"
 
-export type Asset = {
+type Asset = {
 	name: string
 	price: number
 	id: number
 	type: number
 }
 
-export type Q = [undefined, Asset[], number]
+export type Q = [Asset[], number]
 
 export async function load({ locals, url }) {
 	const { user } = await authorise(locals)
 	const { pq, checkPages } = pageQuery(url)
 
-	const [, assets, pages] = await equery<Q>(inventoryQuery, {
+	const [assets, pages] = await equery<Q>(inventoryQuery, {
 		user: Record("user", user.id),
 		...pq,
 	})
--- Site/src/routes/(main)/inventory/search/+server.ts
+++ Site/src/routes/(main)/inventory/search/+server.ts
@@ -2,14 +2,14 @@ import { authorise } from "$lib/server/lucia"
 import pageQuery from "$lib/server/pageQuery"
 import { Record, equery } from "$lib/server/surreal"
 import { json } from "@sveltejs/kit"
-import type { Asset, Q } from "../+page.server.ts"
+import type { Q } from "../+page.server.ts"
 import inventoryQuery from "../inventory.surql"
 
 export async function GET({ locals, url }) {
 	const { user } = await authorise(locals)
 	const { pq, checkPages } = pageQuery(url)
 
-	const [, assets, pages] = await equery<Q>(inventoryQuery, {
+	const [assets, pages] = await equery<Q>(inventoryQuery, {
 		user: Record("user", user.id),
 		...pq,
 	})
