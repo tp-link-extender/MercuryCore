a79fb79d8ffded6038cb58076862fad0ed78b033
Lewin Kelly <heli@odex.cf>
2025-09-13 01:13:51 +0100

    Fix graphic asset content name and asset URL

--- Site/src/lib/server/xmlAsset.ts
+++ Site/src/lib/server/xmlAsset.ts
@@ -23,12 +23,12 @@ export async function graphicAsset(
 ) {
 	const stringType = strings[type as keyof typeof strings]
 	const assetType = type === "Face" || type === "Decal" ? "Image" : "Other"
-	const assetUrl = `${config.Domain}/asset?id=${imageAssetId}`
+	const assetUrl = `http://${config.Domain}/asset?id=${imageAssetId}`
 
 	const assetFile = Bun.file(`xml/graphicAsset${assetType}.xml`)
 	const asset = (await assetFile.text())
 		.replaceAll("_CLASS", stringType[0])
-		// .replaceAll("_CONTENT_NAME", stringType[1]) // Unused at the moment
+		.replaceAll("_CONTENT_NAME", stringType[1]) // Unused at the moment
 		.replaceAll("_STRING_NAME", stringType[2])
 		.replaceAll("_ASSET_URL", assetUrl)
 
