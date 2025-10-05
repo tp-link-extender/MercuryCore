d985b6efbd8cc2f5d97c3329644420695307bebe
Lewin Kelly <heli@odex.cf>
2025-10-05 06:08:10 +0100

    Fix characterasset render URLs, allowing shirt renders to work correctly

--- Site/src/routes/api/render/characterasset/+server.ts
+++ Site/src/routes/api/render/characterasset/+server.ts
@@ -6,7 +6,7 @@ export async function GET({ url }) {
 	const id = url.searchParams.get("id")
 	if (!id || !assetRegex.test(id)) error(400, "Missing id parameter")
 
-	const res = `${config.Domain}/api/render/character;${config.Domain}/asset?id=${id}`
+	const res = `http://${config.Domain}/api/render/character;http://${config.Domain}/asset?id=${id}`
 	return new Response(res, {
 		headers: {
 			Pragma: "no-cache",
