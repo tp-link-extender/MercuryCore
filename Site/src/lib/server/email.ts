e461ed7ad994b917bcbb3d3bab50a32dba25eaaa
Lewin Kelly <heli@odex.cf>
2025-10-22 01:24:09 +0100

    Don't attempt to start email server during build

--- Site/src/lib/server/email.ts
+++ Site/src/lib/server/email.ts
@@ -1,40 +1,40 @@
 import { createTransport } from "nodemailer"
 import type Mail from "nodemailer/lib/mailer"
+import { building } from "$app/environment"
 import config from "./config"
 
-let transport: Mail
-try {
-	transport = createTransport({
-		host: config.Email.Host,
-		port: config.Email.Port,
-		auth: {
-			user: config.Email.Username,
-			pass: process.env.EMAIL_PASSWORD,
-		},
-	})
-} catch (error) {
-	console.error("Error creating SMTP transport:", error)
-	throw error
-}
-transport.on("error", err => console.error("SMTP Transport Error:", err))
-transport.on("idle", () => console.log("SMTP Transport is idle"))
-transport.on("token", () => console.log("SMTP Transport token event"))
+let transport = null as unknown as Mail
+if (!building)
+	try {
+		transport = createTransport({
+			host: config.Email.Host,
+			port: config.Email.Port,
+			auth: {
+				user: config.Email.Username,
+				pass: process.env.EMAIL_PASSWORD,
+			},
+		})
+		transport.on("error", err =>
+			console.error("SMTP Transport Error:", err)
+		)
+		transport.on("idle", () => console.log("SMTP Transport is idle"))
+		transport.on("token", () => console.log("SMTP Transport token event"))
 
-console.log("Verifying SMTP transport...")
-try {
-	await transport.verify()
-} catch (error) {
-	throw new Error(`SMTP transport verification failed: ${error}`)
-}
-console.log("SMTP transport verified successfully.")
+		console.log("Verifying SMTP transport...")
+		await transport.verify()
+		console.log("SMTP transport verified successfully.")
 
-transport.use("stream", (mail, callback) => {
-	console.log("Preparing to send email:")
-	console.log(`From: ${mail.data.from}`)
-	console.log(`To: ${mail.data.to}`)
-	console.log(`Subject: ${mail.data.subject}`)
-	callback()
-})
+		transport.use("stream", (mail, callback) => {
+			console.log("Preparing to send email:")
+			console.log(`From: ${mail.data.from}`)
+			console.log(`To: ${mail.data.to}`)
+			console.log(`Subject: ${mail.data.subject}`)
+			callback()
+		})
+	} catch (error) {
+		console.error("Error creating SMTP transport:", error)
+		throw error
+	}
 
 export async function sendEmail(to: string, subject: string, text: string) {
 	console.log(`Queuing email to ${to}`)
