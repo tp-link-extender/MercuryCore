1ade2f5c3b63e62ce2acfe11d1af7377d8f4aeca
Lewin Kelly <heli@odex.cf>
2025-11-03 23:51:28 +0000

    Use new melt-ui package with runes support

--- Site/src/components/forms/Select.svelte
+++ Site/src/components/forms/Select.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
 	// could make forms based on a js object or something instead of components but concerns with forms that require more interactivity/customisation
 
-	import { createSelect, melt } from "@melt-ui/svelte"
+	import { Select } from "melt/builders"
 	import NoScript from "$components/NoScript.svelte"
 	import YesScript from "$components/YesScript.svelte"
-	import fade from "$lib/fade"
 
 	const {
 		name,
 		label = "",
 		help = "",
 		disabled = false,
-		multiple = false, // messes up hugely if this is true, custom behaviour incoming
+		// multiple = false, // messes up hugely if this is true, custom behaviour incoming
 		// also messes up with superforms cuz it only submits the last selected value
-
 		options = [],
-		selected = null,
+		selected,
 		formData,
 		...rest
 	}: {
@@ -23,39 +21,34 @@
 		label?: string
 		help?: string
 		disabled?: boolean
-		multiple?: boolean
-		options: [string, string][]
-		selected?: string | null
+		// multiple?: boolean
+		options: string[]
+		selected?: string
 		formData: import("sveltekit-superforms").SuperForm<any>
 	} = $props()
 
 	const { form, errors, constraints } = formData
 
-	const {
-		elements: { trigger, menu, option },
-		states: { selectedLabel, open, selected: selectedValue },
-		helpers: { isSelected }
-	} = createSelect<string>({
-		forceVisible: true,
-		positioning: {
-			placement: "bottom",
-			fitViewport: true,
-			sameWidth: true
-		},
-		multiple: multiple as false // todo what the eff
-	})
+	let trigger = $state<HTMLElement>()
+	let dropdown = $state<HTMLElement>()
 
-	if (selected) {
-		$form[name] = selected
-		selectedValue.set({
-			value: selected,
-			label: Object.fromEntries(options)[selected] || selected
-		})
-	}
+	const select = new Select<string>({
+		value: selected,
+		onValueChange: val => {
+			$form[name] = val
+		}
+
+		// floatingConfig: {
+		// 	onCompute: a => {
+		// 		console.log(a)
+		// 		a.floatingApply(dropdown)
+		// 	}
+		// }
+	})
 </script>
 
 <div class="flex flex-wrap pb-8">
-	<label for={name} class="w-full md:w-1/4">
+	<label {...select.label} class="w-full md:w-1/4">
 		{label}
 	</label>
 	<div class="w-full md:w-3/4">
@@ -68,64 +61,36 @@
 				{name}
 				id={name}
 				class={{ "is-invalid": $errors[name] }}>
-				{#each options as [value, label]}
+				{#each options as opt}
 					<option
-						{value}
-						selected={selected
-							? value === selected
-							: $isSelected(value)}>
-						{label}
+						value={opt}
+						selected={opt === (selected ? selected : select.value)}>
+						{opt}
 					</option>
 				{/each}
 			</select>
 		</NoScript>
 		<YesScript>
-			{#if multiple}
-				<!-- We do not speak of this (progressive enhancement impossible) -->
-				<input
-					{name}
-					id={name}
-					type="hidden"
-					value={Array.isArray($selectedValue)
-						? $selectedValue.map(({ value }) => value)
-						: ""} />
-			{:else}
-				<select
-					bind:value={$form[name]}
-					{name}
-					id={name}
-					multiple
-					class="hidden">
-					{#each options as [value]}
-						<option {value} selected={$isSelected(value)}></option>
-					{/each}
-				</select>
-			{/if}
+			<input type="hidden" {name} value={select.value || options[0]} />
 
-			<button use:melt={$trigger} {disabled} class="fakeselect">
-				{$selectedLabel || options[0]?.[1] || "Select an option"}
+			<button
+				{...select.trigger}
+				{disabled}
+				class="fakeselect"
+				bind:this={trigger}>
+				{select.value || options[0] || "Select an option"}
 			</button>
-			{#if $open}
-				<div
-					class="dropdown-content bg-darker flex flex-col rounded-lg p-2"
-					use:melt={$menu}
-					transition:fade={{ duration: 150 }}>
-					{#each options as [value, label]}
-						<button
-							class="btn light-text"
-							use:melt={$option({ value, label })}>
-							<fa
-								fa-check
-								class={[
-									"pr-2",
-									{ hidden: !$isSelected(value) }
-								]}>
-							</fa>
-							{label}
-						</button>
-					{/each}
-				</div>
-			{/if}
+			<div {...select.content}>
+				{#each options as opt}
+					<button {...select.getOption(opt)} class="btn light-text">
+						<fa
+							fa-check
+							class={["pr-2", { hidden: opt !== select.value }]}>
+						</fa>
+						{opt}
+					</button>
+				{/each}
+			</div>
 		</YesScript>
 
 		{#if help}
--- Site/svelte.config.js
+++ Site/svelte.config.js
@@ -1,11 +1,10 @@
-import { preprocessMeltUI, sequence } from "@melt-ui/pp"
 import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
-/** @type {import("@sveltejs/kit").Config}*/
+/** @type {import("@sveltejs/kit").Config} */
 export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess for more information about preprocessors
-	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 		alias: { $components: "./src/components" },
