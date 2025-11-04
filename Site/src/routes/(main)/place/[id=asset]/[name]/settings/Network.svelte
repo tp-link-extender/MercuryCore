506ffaca393835e798288f4c27879d0febc92f29
Lewin Kelly <heli@odex.cf>
2025-11-04 00:54:49 +0000

    Dedicated server checkbox, disabling hosted server inputs when checked

--- Site/src/routes/(main)/place/[id=asset]/[name]/settings/Network.svelte
+++ Site/src/routes/(main)/place/[id=asset]/[name]/settings/Network.svelte
@@ -2,6 +2,7 @@
 	import { superForm } from "sveltekit-superforms/client"
 	import Form from "$components/forms/Form.svelte"
 	import Input from "$components/forms/Input.svelte"
+	import fade from "$lib/fade"
 
 	const {
 		data
@@ -21,12 +22,21 @@
 	{formData}
 	submit=" <fa fa-save></fa> Save changes"
 	action="?/network&tab=Network">
-	<Input {formData} name="serverAddress" label="Address" />
 	<Input
 		{formData}
-		name="serverPort"
-		label="Port"
-		type="number"
-		help="Using a port number lower than 49152 may not work correctly." />
+		name="dedicated"
+		label="Dedicated server"
+		type="checkbox" />
+	{#if $form.dedicated}
+		<div transition:fade>
+			<Input {formData} name="serverAddress" label="Address" />
+			<Input
+				{formData}
+				name="serverPort"
+				label="Port"
+				type="number"
+				help="Using a port number lower than 49152 may not work correctly." />
+		</div>
+	{/if}
 	<Input {formData} name="maxPlayers" label="Server limit" type="number" />
 </Form>
