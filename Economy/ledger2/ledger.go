83fe507b2b8a06b612a975e8440aa6918636f5f1
Lewin Kelly <heli@odex.cf>
2025-11-23 06:01:55 +0000

    Improved item ID system

--- Economy/ledger2/ledger.go
+++ Economy/ledger2/ledger.go
@@ -1,10 +1,78 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 
 	bolt "go.etcd.io/bbolt"
 )
+
+// a user can own:
+
+// multiple assets (fungible):
+// - currency (of a given ID), with a quantity
+// - assets (of a given ID), with a quantity
+
+// singular assets (non-fungible):
+// - place/game (of a given ID), only 1 of each
+// - group (of a given ID), only 1 of each
+
+
+type Item string
+
+const (
+	ItemCurrency Item = "currency"
+	ItemAsset    Item = "asset"
+	ItemPlace    Item = "place"
+	ItemGroup    Item = "group"
+)
+
+func (it Item) Fungible() bool {
+	return it == ItemCurrency || it == ItemAsset
+}
+
+type ID struct {
+	Item  Item
+	Value string
+}
+
+func (id ID) MarshalBinary() ([]byte, error) {
+	var buf bytes.Buffer
+	buf.WriteString(string(id.Item))
+	buf.WriteByte(':')
+	buf.WriteString(id.Value)
+	return buf.Bytes(), nil
+}
+
+func (id *ID) UnmarshalBinary(data []byte) error {
+	parts := bytes.SplitN(data, []byte(":"), 2)
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid ID format")
+	}
+	id.Item = Item(parts[0])
+	id.Value = string(parts[1])
+	return nil
+}
+
+func (id ID) Fungible() (bool, error) {
+	return id.Item.Fungible(), nil
+}
+
+func IDCurrency(currencyID uint) ID {
+	return ID{Item: ItemCurrency, Value: fmt.Sprint(currencyID)}
+}
+
+func IDAsset(assetID uint) ID {
+	return ID{Item: ItemAsset, Value: fmt.Sprint(assetID)}
+}
+
+func IDPlace(placeID uint) ID {
+	return ID{Item: ItemPlace, Value: fmt.Sprint(placeID)}
+}
+
+func IDGroup(groupID string) ID {
+	return ID{Item: ItemGroup, Value: groupID}
+}
 
 func main() {
 	db, err := bolt.Open("mydb.db", 0600, nil)
