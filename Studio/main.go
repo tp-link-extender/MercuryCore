d45795bd4eb4beb3d965377fc99c568215a8f187
Lewin Kelly <heli@odex.cf>
2026-02-26 23:35:12 +0000

    Improved component and template handling

--- Studio/main.go
+++ Studio/main.go
@@ -6,18 +6,65 @@ import (
 	"net/http"
 )
 
-func main() {
-	// test http
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// load index.tmpl
-		tmpl, err := template.ParseFiles("index.tmpl")
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
+const (
+	ext = ".tmpl"
+)
+
+type Data map[string]any
+
+func MergeData(maps ...Data) Data {
+	merged := make(Data)
+	for _, m := range maps {
+		for k, v := range m {
+			merged[k] = v
 		}
-		tmpl.Execute(w, nil)
+	}
+	return merged
+}
+
+type Component struct {
+	Name   string
+	Loader func(http.ResponseWriter, *http.Request) (Data, error)
+}
+
+func handle(Pages []Component) http.HandlerFunc {
+	lp := len(Pages)
+	files := make([]string, lp)
+	for i, p := range Pages {
+		files[i] = p.Name + ext
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		return func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "template error: "+err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		data := make(Data)
+		for _, p := range Pages {
+			nd, err := p.Loader(w, r)
+			if err != nil {
+				http.Error(w, "loader error: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+			data = MergeData(data, nd)
+		}
+
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, "template error: "+err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handle([]Component{root, loggedOut, pageIndex})(w, r)
 	})
 
 	fmt.Println("~ Studio is up on port 4444 ~")
-	http.ListenAndServe(":4444", nil)
+	if err := http.ListenAndServe(":4444", nil); err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
---
+++ Studio/pages.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+func loadnone(w http.ResponseWriter, r *http.Request) (Data, error) {
+	return Data{}, nil
+}
+
+var root = Component{
+	Name:   "root",
+	Loader: loadnone,
+}
+
+var loggedOut = Component{
+	Name:   "layouts/loggedout",
+	Loader: loadnone,
+}
+
+var pageIndex = Component{
+	Name:   "pages/index",
+	Loader: loadnone,
+}
