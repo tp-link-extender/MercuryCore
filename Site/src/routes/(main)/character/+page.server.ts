09055ee5802ad4f93648115f06d193663466b85e
Lewin Kelly <heli@odex.cf>
2026-02-28 20:17:21 +0000

    Await bodyColours database update

--- Site/src/routes/(main)/character/+page.server.ts
+++ Site/src/routes/(main)/character/+page.server.ts
@@ -102,7 +102,7 @@ async function paint({ fetch: f, locals, url }: RequestEvent) {
 
 	currentColours[bodyPart] = +bodyColour
 
-	db.update(Record("user", user.id)).merge({ bodyColours: currentColours })
+	await db.update(Record("user", user.id)).merge({ bodyColours: currentColours })
 
 	return await rerender(f, user)
 }
