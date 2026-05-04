7f349ee1689e31b2e1fdf81cf19e1f2096c3416b
Lewin Kelly <heli@odex.cf>
2026-05-04 20:41:53 +0100

    Add OwnsMany API function

--- Economy/client/api.ts
+++ Economy/client/api.ts
@@ -1,5 +1,5 @@
 import { SerialiseItem } from "./items"
-import type { Owner } from "./types"
+import type { CanOwnMany, CanOwnOne, Owner } from "./types"
 
 type Route =
 	| "ownsOne"
@@ -19,15 +19,19 @@ const url = "http://localhost:2009"
 export type ReturnValue<T> = Promise<{ ok: true; value: T } | { ok: false }>
 export type ReturnErr = { ok: true } | { ok: false; msg: string }
 
-const request = (route: Route, body: Bun.BodyInit): Promise<Response> =>
+export const request = (route: Route, body: Bun.BodyInit): Promise<Response> =>
 	fetch(`${url}/${route}`, {
 		method: "POST",
 		body,
 	})
 
-export async function ownsOne(o: Owner): Promise<ReturnValue<boolean>> {
-	const os = SerialiseItem(o)
-	const res = await request("ownsOne", os)
+export async function ownsOne(
+	o: Owner,
+	i: CanOwnOne
+): Promise<ReturnValue<boolean>> {
+	const body = Buffer.concat([SerialiseItem(o), SerialiseItem(i)])
+	const res = await request("ownsOne", body)
+
 	if (res.status !== 200)
 		// it won't be another 2xx status code
 		return { ok: false }
@@ -35,4 +39,16 @@ export async function ownsOne(o: Owner): Promise<ReturnValue<boolean>> {
 	const buf = await res.arrayBuffer()
 	const view = new DataView(buf)
 	return { ok: true, value: view.getUint8(0) === 1 }
+}
+
+export async function ownsMany(
+	o: Owner,
+	i: CanOwnMany
+): Promise<ReturnValue<number>> {
+	const body = Buffer.concat([SerialiseItem(o), SerialiseItem(i)])
+	const res = await request("ownsMany", body)
+	if (res.status !== 200) return { ok: false }
+
+	const text = await res.text()
+	return { ok: true, value: +text }
 }
