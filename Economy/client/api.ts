c9f31061e5ae79b8f75fe5ae8d2ac17d7cd59099
Lewin Kelly <heli@odex.cf>
2026-05-07 16:59:29 +0100

    Add client API functions for owners, inventory, balance, stipend

--- Economy/client/api.ts
+++ Economy/client/api.ts
@@ -1,4 +1,5 @@
-import { SerialiseItem } from "./items"
+import { OwnersMany, OwnersOne } from "./economy"
+import { BufReader, Items, SerialiseItem } from "./items"
 import type { CanOwnMany, CanOwnOne, Owner } from "./types"
 
 type Route =
@@ -19,36 +20,76 @@ const url = "http://localhost:2009"
 export type ReturnValue<T> = Promise<{ ok: true; value: T } | { ok: false }>
 export type ReturnErr = { ok: true } | { ok: false; msg: string }
 
-export const request = (route: Route, body: Bun.BodyInit): Promise<Response> =>
+export const request = (route: Route, body: Buffer): Promise<Response> =>
 	fetch(`${url}/${route}`, {
 		method: "POST",
-		body,
+		body: Uint8Array.from(body),
 	})
 
-export async function ownsOne(
-	o: Owner,
-	i: CanOwnOne
-): Promise<ReturnValue<boolean>> {
+export async function ownsOne(o: Owner, i: CanOwnOne): ReturnValue<boolean> {
 	const body = Buffer.concat([SerialiseItem(o), SerialiseItem(i)])
 	const res = await request("ownsOne", body)
-
-	if (res.status !== 200)
-		// it won't be another 2xx status code
-		return { ok: false }
+	if (res.status !== 200) return { ok: false } // it won't be another 2xx status code
 
 	const buf = await res.arrayBuffer()
 	const view = new DataView(buf)
 	return { ok: true, value: view.getUint8(0) === 1 }
 }
 
-export async function ownsMany(
-	o: Owner,
-	i: CanOwnMany
-): Promise<ReturnValue<number>> {
+export async function ownsMany(o: Owner, i: CanOwnMany): ReturnValue<number> {
 	const body = Buffer.concat([SerialiseItem(o), SerialiseItem(i)])
 	const res = await request("ownsMany", body)
 	if (res.status !== 200) return { ok: false }
 
 	const text = await res.text()
 	return { ok: true, value: +text }
+}
+
+export async function ownersOne(i: CanOwnOne): ReturnValue<OwnersOne> {
+	const body = SerialiseItem(i)
+	const res = await request("ownersOne", body)
+	if (res.status !== 200) return { ok: false }
+
+	const buf = Buffer.from(await res.arrayBuffer())
+	const r = new BufReader(buf)
+
+	return { ok: true, value: OwnersOne.Deserialise(r) }
+}
+
+export async function ownersMany(i: CanOwnMany): ReturnValue<OwnersMany> {
+	const body = SerialiseItem(i)
+	const res = await request("ownersMany", body)
+	if (res.status !== 200) return { ok: false }
+
+	const buf = Buffer.from(await res.arrayBuffer())
+	const r = new BufReader(buf)
+
+	return { ok: true, value: OwnersMany.Deserialise(r) }
+}
+
+export async function inventory(o: Owner): ReturnValue<Items> {
+	const body = SerialiseItem(o)
+	const res = await request("inventory", body)
+	if (res.status !== 200) return { ok: false }
+
+	const buf = Buffer.from(await res.arrayBuffer())
+	const r = new BufReader(buf)
+
+	return { ok: true, value: Items.Deserialise(r) }
+}
+
+export async function balance(o: Owner): ReturnValue<number> {
+	const body = SerialiseItem(o)
+	const res = await request("inventory", body)
+	if (res.status !== 200) return { ok: false }
+
+	const text = await res.text()
+	return { ok: true, value: +text }
+}
+
+export async function stipend(o: Owner): Promise<boolean> {
+	const body = SerialiseItem(o)
+	const res = await request("inventory", body)
+
+	return res.status === 200 || res.status === 429
 }
--- Economy/client/economy.ts
+++ Economy/client/economy.ts
@@ -1,5 +1,4 @@
 import {
-	BufBuilder,
 	BufReader,
 	DeserialiseItem,
 	Items,
