72e03210a8e82d6615df056ac15c7a2bcb4cca3f
Lewin Kelly <heli@odex.cf>
2026-07-22 04:43:56 +0100

    Remove unnecessary filter

--- Site/icons/search.ts
+++ Site/icons/search.ts
@@ -6,10 +6,7 @@ const out = await Bun.$`bunx unocss ./src/**/*.svelte --stdout`.quiet().text()
 
 const far = /fa-([a-zA-Z0-9-]+)/
 
-const classes = out
-	.split("\n")
-	.filter(s => s.match(far))
-	.map(s => s.match(far)?.[1])
+const classes = out.split("\n").map(s => s.match(far)?.[1])
 const deduped = [...new Set(classes)].filter(Boolean) as string[]
 
 const fal: { [_: string]: string } = fa
