ff01eda0276b5e1f7874d6f4045aac1e52771665
Lewin Kelly <heli@odex.cf>
2026-07-22 06:40:56 +0100

    Remove unused locals argument

--- Site/src/routes/(plain)/+page.server.ts
+++ Site/src/routes/(plain)/+page.server.ts
@@ -2,6 +2,6 @@ import { redirect } from "@sveltejs/kit"
 import { dev } from "$app/environment"
 
 // Redirect to check page if in development mode
-export async function load({ locals }) {
+export async function load() {
 	if (dev) redirect(302, "/check")
 }
