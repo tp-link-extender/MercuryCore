cab9fbbed9a39fe345c6350d135d54616be62fce
Lewin Kelly <heli@odex.cf>
2026-08-12 04:23:27 +0100

    Add more allowed security versions

--- Site/src/routes/api/getallowedsecurityversions/+server.ts
+++ Site/src/routes/api/getallowedsecurityversions/+server.ts
@@ -1,4 +1,11 @@
 import { json } from "@sveltejs/kit"
 
 export const GET = () =>
-	json({ data: ["0.75.0pcplayer", "0.75.0pcplayeraskljfLUZF"] })
+	json({
+		data: [
+			"0.75.0pcplayer",
+			"0.75.0pcplayeraskljfLUZF",
+			"0.235.0pcplayer",
+			"0.285.0pcplayer",
+		],
+	})
