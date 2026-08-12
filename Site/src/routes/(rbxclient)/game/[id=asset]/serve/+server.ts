2c5a15b7c355a3cff5759469ddefc576d7f32c61
Lewin Kelly <heli@odex.cf>
2026-08-12 04:59:31 +0100

    Add 2016 host loadscript endpoint and hardcode map location URL

--- Site/src/routes/(rbxclient)/game/[id=asset]/serve/+server.ts
+++ Site/src/routes/(rbxclient)/game/[id=asset]/serve/+server.ts
@@ -28,8 +28,7 @@ export async function GET({ params }) {
 	const scriptFile = Bun.file("../data/server/loadscripts/host.lua")
 	const script = (await scriptFile.text())
 		.replaceAll("_BASE_URL", `"${config.DomainInsecure}"`)
-		// .replaceAll("_MAP_LOCATION", `"http://${config.DomainInsecure}/game/${id}"`)
-		.replaceAll("_MAP_LOCATION", `"rbxasset://mbdtf.rbxl"`) // TODO: remove
+		.replaceAll("_MAP_LOCATION", `"http://${config.DomainInsecure}/game/${id}"`)
 		.replaceAll("_SERVER_PORT", idToPort(id).toString())
 		.replaceAll("_SERVER_PRESENCE_URL", `"${serverPresenceUrl}"`)
 
---
+++ Site/src/routes/(rbxclient)/game/host/2016/+server.ts
@@ -0,0 +1,38 @@
+import { error } from "@sveltejs/kit"
+import config from "$lib/server/config"
+import { SignScript } from "$lib/server/sign"
+import { db } from "$lib/server/surreal"
+import placeQuery from "./place.surql"
+
+export async function GET({ params, url }) {
+	if (config.Gameservers.Hosting === "Dedicated")
+		error(400, "Dedicated servers are not supported")
+
+	const ticket = url.searchParams.get("ticket")
+	if (!ticket) error(400, "Invalid Request")
+
+	const [[place]] = await db.query<{ serverPort: number }[][]>(placeQuery, {
+		ticket,
+	})
+	if (!place) error(400, "Invalid Server ticket")
+
+	const port = place.serverPort
+	// const serverId = placeData.id.toString()
+
+	let mapLocation = url.searchParams.get("autopilot")
+	if (mapLocation) {
+		mapLocation = Buffer.from(mapLocation, "base64").toString()
+		if (mapLocation.slice(-5) !== ".rbxl") mapLocation = null
+		else if (mapLocation) mapLocation = `rbxasset://maps/${mapLocation}`
+	}
+
+	const scriptFile = Bun.file("../data/server/loadscripts/host2016.lua")
+	const script = (await scriptFile.text())
+		.replaceAll("_PLACE_ID", params.id)
+		.replaceAll("_BASE_URL", `"${config.Domain}"`)
+		// .replaceAll("_MAP_LOCATION", `"${mapLocation || ""}"`)
+		.replaceAll("_MAP_LOCATION", `"rbxasset://mbdtf.rbxl"`) // TODO: remove again
+		.replaceAll("_SERVER_PORT", port.toString())
+
+	return new Response(await SignScript(script))
+}
