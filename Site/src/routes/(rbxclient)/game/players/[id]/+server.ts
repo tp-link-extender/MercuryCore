ba7b5e1202c183b23a4be7baef67ad53b3c8d865
tskmgr222 <51055281+Mantaraix@users.noreply.github.com>
2026-08-13 03:03:31 +0100

    Add game\players\{id} endpoint

---
+++ Site/src/routes/(rbxclient)/game/players/[id]/+server.ts
@@ -0,0 +1,14 @@
+import { json } from "@sveltejs/kit"
+
+export async function GET({ params }) {
+  // get bearer token from header instead of insecure url (temporarily disabled)
+  // const auth = request.headers.get("Authorization")
+  // if (!auth || !auth.startsWith("Bearer ") || auth.slice(7) !== GAMESERVER_KEY)
+  // 	throw error(403, "Unauthorised")
+
+  const id = +params.id;
+
+  json({
+    "ChatFilter":"whitelist"
+  });
+}
