0e6a8fcf6710883cbdfb61f9717f0d5b8339e4e0
tskmgr222 <51055281+Mantaraix@users.noreply.github.com>
2026-08-14 18:30:13 +0100

    Update +server.ts

--- Site/src/routes/api/getallowedmd5hashes/+server.ts
+++ Site/src/routes/api/getallowedmd5hashes/+server.ts
@@ -1,3 +1,3 @@
 import { json } from "@sveltejs/kit"
 
-export const GET = () => json({ data: ["1d301329ddf133104869d7d938e606dd"] }) // TODO: make configurable from mercury.core.ts
+export const GET = () => json({ data: ["53d7c853c80ecbcd220fc6875485a718"] }) // TODO: make configurable from mercury.core.ts
