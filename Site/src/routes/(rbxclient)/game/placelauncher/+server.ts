547f33aa42b984a163a61044332add03f01ece67
tskmgr222 <51055281+Mantaraix@users.noreply.github.com>
2026-08-26 23:33:44 +0100

    Create +server.ts

---
+++ Site/src/routes/(rbxclient)/game/placelauncher/+server.ts
@@ -0,0 +1,8 @@
+import { SignScript } from "$lib/server/sign"
+
+export async function GET() {
+    const scriptFile = Bun.file("../data/server/loadscripts/visit.lua")
+    const script = (await scriptFile.text()).replaceAll("_PLACE_ID", "0")
+
+    return new Response(await SignScript(script))
+}
